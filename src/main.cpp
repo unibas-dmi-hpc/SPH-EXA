0d3207d1c02ff1fcb52d6082dd9defcca65279fb
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2018-05-08 15:32:37 +0200

    add main outline

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,31 @@
 #include <iostream>
-
 #include "Simulation.hpp"
-
-
 
 int main(int argc, char** argv)
 {
-    int N;            //number of particles
-    int nsteps;         //number ot integration steps
-    
     Simulation sim;
-    
-    if(argc>1)
-    {
-        N=atoi(argv[1]);
-        sim.set_number_of_particles(N);
-        if(argc==3)
-        {
-            nsteps=atoi(argv[2]);
-            sim.set_number_of_steps(nsteps);
-        }
-    }
-    
-    
-    
-    sim.start();
-    
-    return 0;
-}
 
-int newmain(int argc, char** argv)
-{
-    int N;            //number of particles
-    int nsteps;         //number ot integration steps
-    
-    Simulation sim;
-    
-    if(argc>1)
+    sim.init("squarepatch3D");
+    //sim.readdata();
+    //sim.init_scenario();
+
+    do
     {
-        N=atoi(argv[1]);
-        sim.set_number_of_particles(N);
-        if(argc==3)
-        {
-            nsteps=atoi(argv[2]);
-            sim.set_number_of_steps(nsteps);
-        }
-    }
-    
-//call testparameters
-//    call init
-//
-//    call readdata
-//    call init_scenario
-//
-//    timepass=0.d0
-//
-//
-//    !.....Start iterations.
-//    do 100 l=iterini,nnl
-//        call buildtree
-//        call findneighbors
-//
-//        call calculate_density !Density
-//
-//        call calculate_omega      !Omega
-//
-//        call calculate_IAD       !6 - IAD terms
-//
-//        call eostot
-//
-//        call calculate_divv    !9 - Div-v
-//
-//
-//        call momeqn            !11 - Mom + Energy eqs
-//
-//
-//
-//
-//        if (gravity) then
-//            call treewalk          !13 - Gravity calculation
-//
-//
-//        call update            !15 - Update
-//
-//        call timectrl          !16 - Timestep
-//
-//
-//        if(conserv)call conservation      !17 - Conservation laws
-    
-                
+       //sim.buildtree();
+       sim.find_neighbors();
+       //sim.calculate_density();
+       //sim.calculate_omega();
+       //sim.calculate_IAD();
+       //sim.eostot();
+       //sim.calculate_divv();
+       //sim.momeqn();
+
+       //if(gravity)
+           //sim.treewalk();
+
+       //sim.update();
+       //sim.timectrl();
+    } while(sim.advance());
     
     return 0;
 }
