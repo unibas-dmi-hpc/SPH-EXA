9a064d3a88f48a9dd312198ce5cfa1c9ec1ad329
Danilo Guerrera <danilo.guerrera@unibas.ch>
2018-05-08 17:05:15 +0200

    implementation of the find neighbours via list

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <algorithm>
 #include "Simulation.hpp"
+#include <cmath>
+
+#define NEIGHBOURS_SIZE 100
 
 using namespace std;
 
 Simulation::Simulation() : 
-    n(100), simTime(0.0), targetTime(1.0),
+    n(1e5), simTime(0.0), targetTime(1.0),
     x(n, 0.0), y(n, 0.0), z(n, 0.0),
     vx(n, 0.0), vy(n, 0.0), vz(n, 0.0),
     grad_x(n, 0.0), grad_y(n, 0.0), grad_z(n, 0.0),
@@ -28,8 +32,8 @@ void Simulation::init(const string &filename)
     for(int i=0; i<n; i++)
     {
         inFile >> x[i] >> y[i] >> z[i] >> vx[i] >> vy[i] >> vz[i] >> pressure[i] >> h[i] >> volume[i] >> mass[i];
-        cout << x[i] << " " << y[i] << " " << z[i] << " " << vx[i] << " " << vy[i] << " " << vz[i] << " ";
-        cout << pressure[i] << " " << h[i] << " " << volume[i] << mass[i] << endl;
+//        cout << x[i] << " " << y[i] << " " << z[i] << " " << vx[i] << " " << vy[i] << " " << vz[i] << " ";
+//        cout << pressure[i] << " " << h[i] << " " << volume[i] << mass[i] << endl;
     }
     
     inFile.close();
@@ -45,4 +49,33 @@ bool Simulation::advance()
 void Simulation::find_neighbors()
 {
     printf("::find_neighbors()\n");
+    real d;
+    vector<vector<real> > neighbours(n, vector<real>(NEIGHBOURS_SIZE, std::numeric_limits<real>::max()));
+    vector<vector<real> > neighbours_x(n, vector<real>(NEIGHBOURS_SIZE, std::numeric_limits<real>::max()));
+    vector<vector<real> > neighbours_y(n, vector<real>(NEIGHBOURS_SIZE, std::numeric_limits<real>::max()));
+    vector<vector<real> > neighbours_z(n, vector<real>(NEIGHBOURS_SIZE, std::numeric_limits<real>::max()));
+    int index;
+
+    for(int i=0; i<n; ++i)
+    {
+        for(int j=0; j<n; ++j){
+            if (i != j) {
+                d = sqrt(pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2) + pow((z[i] - z[j]), 2));
+                
+                //based on https://stackoverflow.com/questions/47982127/c-how-to-insert-a-new-element-in-a-sorted-vector
+                auto pos = std::find_if(neighbours[i].begin(), neighbours[i].end(), [d](auto s) {
+                    return s < d;
+                });
+                if(pos != neighbours[i].end()){
+                    neighbours[i].insert(pos, d);
+                    index = distance(neighbours[i].begin(),pos);
+                
+                    neighbours_x[i][index] = x[i];
+                    neighbours_y[i][index] = y[i];
+                    neighbours_z[i][index] = z[i];
+                }
+                
+            }
+        }
+    }
 }
