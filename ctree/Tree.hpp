6a39a27daa7081014244ad21d0741a8aadbe7189
David Imbert <david.imbert@nextflow-software.com>
2018-10-16 08:41:33 +0200

    New namespace SPHExa for every public functions belonging to SPHExa mini-app; replace free functions by static member functions and replace deprecated '#define' by C++14 'constexpr'.

--- ctree/Tree.hpp
+++ ctree/Tree.hpp
@@ -1,10 +1,14 @@
-#ifndef TREE_HPP
-#define TREE_HPP
+#ifndef SPHEXA_TREE_HPP
+#define SPHEXA_TREE_HPP
 
 #include <vector>
 
-#define MAXP 64
-#define PLANCK 1e-15
+
+namespace SPHExa
+{
+
+constexpr int const MAXP = 64;
+constexpr double const PLANCK = 1e-15;
 
 class Tree
 {
@@ -14,15 +18,15 @@ public:
 
 	void clean();
 
-	int cellCount();
+	int cellCount() const;
 	
 	void init(const double minx, const double maxx, const double miny, const double maxy, const double minz, const double maxz);
 
 	void build(const int n, const double *x, const double *y, const double *z);
 	
 	void findNeighbors(const int i, const double *x, const double *y, const double *z, const double r, const int ngmax, int *ng, int &nvi, 
-		const bool PBCx = false, const bool PBCy = false, const bool PBCz = false);
-
+		const bool PBCx = false, const bool PBCy = false, const bool PBCz = false) const;
+ 
 private:
 	double _minx, _maxx;
 	double _miny, _maxy;
@@ -37,7 +41,15 @@ private:
 	void buildRec(const std::vector<int> &list, const double *x, const double *y, const double *z);
 
 	void findNeighborsRec(const double *x, const double *y, const double *z, 
-		const double xi, const double yi, const double zi, const double r, const int ngmax, int *ng, int &nvi);
+		const double xi, const double yi, const double zi, const double r, const int ngmax, int *ng, int &nvi) const;
+
+	static inline double normalize(double d, double min, double max);
+
+	static inline double distance(const double x1, const double y1, const double z1, const double x2, const double y2, const double z2);
+
+	static inline void check_add_list(const std::vector<int> &list, const double *x, const double *y, const double *z, const double xi, const double yi, const double zi, const double r, const int ngmax, int *ng, int &nvi);
 };
 
-#endif
+}
+
+#endif // SPHEXA_TREE_HPP
