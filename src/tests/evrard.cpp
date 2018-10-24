b7e35db55d4d407a1ef38b3c8f15a36a1476812e
Aurelien Cavelan <aurelien.cavelan@unibas.ch>
2018-10-24 18:37:32 +0200

    move trees to src, add KdTree and rudimentary tests

--- src/tests/evrard.cpp
+++ src/tests/evrard.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <omp.h>
 
-#include "Tree.hpp"
+#include "../tree/BroadTree.hpp"
+#include "../tree/KdTree.hpp"
 
 using namespace sphexa;
 
@@ -12,6 +13,24 @@ using namespace sphexa;
 
 using namespace std;
 
+void computeBox(double &xmin, double &xmax, double &ymin, double &ymax, double &zmin, double &zmax, const double *x, const double *y, const double *z, const int n)
+{
+	xmin = INFINITY;
+	xmax = -INFINITY;
+	ymin = INFINITY;
+	ymax = -INFINITY;
+	zmin = INFINITY;
+	zmax = -INFINITY;
+	for(int i=0; i<n; i++)
+	{
+		if(x[i] < xmin) xmin = x[i];
+		if(x[i] > xmax) xmax = x[i];
+		if(y[i] < ymin) ymin = y[i];
+		if(y[i] > ymax) ymax = y[i];
+		if(z[i] < zmin) zmin = z[i];
+		if(z[i] > zmax) zmax = z[i];
+	}
+}
 void readfileEvrard(const char *filename, int n, double *x, double *y, double *z, double *h)
 {
 	FILE *f = fopen(filename, "rb");
@@ -24,31 +43,6 @@ void readfileEvrard(const char *filename, int n, double *x, double *y, double *z
 		fclose(f);
 	}
 	else
-		printf("Error opening file.\n");
-}
-
-void readfileSquarePatch(const char *filename, int n, double *x, double *y, double *z, double *h)
-{
-	FILE *f = fopen(filename, "rb");
-	if(f)
-	{
-		int u1;
-		double gm, gh, gd;
-		fread(&u1, sizeof(int), 1, f);
-		fread(&gm, sizeof(double), 1, f);
-		fread(&gh, sizeof(double), 1, f);
-		fread(&gd, sizeof(double), 1, f);
-
-		fread(x, sizeof(double), n, f);
-		fread(y, sizeof(double), n, f);
-		fread(z, sizeof(double), n, f);
-
-		for(int i=0; i<n; i++)
-			h[i] = gh*1.5916455;
-
-		fclose(f);
-	}
-	else
 	{
 		printf("Error opening file.\n");
 		exit(1);
@@ -58,7 +52,6 @@ void readfileSquarePatch(const char *filename, int n, double *x, double *y, doub
 int main()
 {
 	int n = 1000000;
-	//int n = 10077696;
 	int ngmax = 150;
 
 	double *x = new double[n];
@@ -73,32 +66,23 @@ int main()
 	
 	double start, tbuild, tfind;
 
-	readfileEvrard("../../../bigfiles/evrard_1M.bin", n, x, y, z, h);
-	//readfileSquarePatch("../bigfiles/squarepatch3D.bin", n, x, y, z, h);
+	readfileEvrard("../../bigfiles/evrard_1M.bin", n, x, y, z, h);
 
-	double xmin = 1000, xmax = -1000, ymin = 1000, ymax = -1000, zmin = 1000, zmax = -1000;
-	for(int i=0; i<n; i++)
-	{
-		if(x[i] < xmin) xmin = x[i];
-		if(x[i] > xmax) xmax = x[i];
-		if(y[i] < ymin) ymin = y[i];
-		if(y[i] > ymax) ymax = y[i];
-		if(z[i] < zmin) zmin = z[i];
-		if(z[i] > zmax) zmax = z[i];
-	}
+	double xmin, xmax, ymin, ymax, zmin, zmax;
+	computeBox(xmin, xmax, ymin, ymax, zmin, zmax, x, y, z, n);
 
 	printf("Domain x[%f %f]\n", xmin, xmax);
 	printf("Domain y[%f %f]\n", ymin, ymax);
 	printf("Domain z[%f %f]\n", zmin, zmax);
-  
-	Tree tree;
+
+	TREEINTERFACE tree;
 
 	start = START;
 	tree.setBox(xmin, xmax, ymin, ymax, zmin, zmax);
 	tree.build(n, x, y, z);
 	tbuild = STOP;
 
-	printf("CELLS: %d\n", tree.cellCount());
+	//printf("CELLS: %d\n", tree.cellCount());
 	printf("BUILD TIME: %f\n", tbuild);
 
 	start = START;
@@ -112,6 +96,7 @@ int main()
 	long int sum = 0;
 	for(int i=0; i<n; i++)
 		sum += nvi[i];
+
 	printf("Total neighbors found: %lu\n", sum);
 
 	tree.clean();
