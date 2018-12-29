f0ee085fadb9ac130d57c72dc1666f9aaed78811
Aurelien Cavelan <aurelien.cavelan@unibas.ch>
2018-12-29 22:46:56 +0100

    sqpatch mpi ready

--- src/SqPatch.hpp
+++ src/SqPatch.hpp
@@ -11,32 +11,109 @@ template<typename T>
 class SqPatch
 {
 public:
-    SqPatch(int n, const char *filename) : 
-    	n(n), x(n), y(n), z(n), x_m1(n), y_m1(n), z_m1(n), vx(n), vy(n), vz(n), 
-    	ro(n), ro_0(n), u(n), p(n), p_0(n), h(n), m(n), c(n), temp(n), 
-    	grad_P_x(n), grad_P_y(n), grad_P_z(n), 
-    	du(n), du_m1(n), dt(n), dt_m1(n), neighbors(n)
-    {
-        for(auto i : neighbors)
-            i.reserve(ngmax);
+    #ifdef USE_MPI
+        SqPatch(int n, const std::string &filename, MPI_Comm comm) : 
+            n(n), count(n), comm(comm),data({&x, &y, &z, &x_m1, &y_m1, &z_m1, &vx, &vy, &vz, 
+                &ro, &ro_0, &u, &p, &p_0, &h, &m, &c, &temp, 
+                &grad_P_x, &grad_P_y, &grad_P_z, &du, &du_m1, &dt, &dt_m1})
+        {
+            MPI_Comm_size(comm, &nrank);
+            MPI_Comm_rank(comm, &rank);
+            MPI_Get_processor_name(pname, &pnamelen);
+            loadMPI(filename);
+            init();
+        }
+    #else
+         SqPatch(int n, const std::string &filename) : 
+            n(n), count(n), data({&x, &y, &z, &x_m1, &y_m1, &z_m1, &vx, &vy, &vz, 
+                &ro, &ro_0, &u, &p, &p_0, &h, &m, &c, &temp, 
+                &grad_P_x, &grad_P_y, &grad_P_z, &du, &du_m1, &dt, &dt_m1})
+        {
+            resize(n);
+            load(filename);
+            init();
+        }
+    #endif
 
+    inline void resize(unsigned int size)
+    {
+        for(unsigned int i=0; i<data.size(); i++)
+            data[i]->resize(size);
+        neighbors.resize(size);
+    }
+
+    void load(const std::string &filename)
+    {
         // input file stream
         std::ifstream inputfile(filename, std::ios::binary);
-        if(!inputfile)
+
+        if(inputfile.is_open())
         {
-            std::cout << "Couldn't open file " << filename << std::endl;
-            exit(1);
+            // read the contents of the file into the vectors
+            inputfile.read(reinterpret_cast<char*>(x.data()), sizeof(T)*x.size());
+            inputfile.read(reinterpret_cast<char*>(y.data()), sizeof(T)*y.size());
+            inputfile.read(reinterpret_cast<char*>(z.data()), sizeof(T)*z.size());
+            inputfile.read(reinterpret_cast<char*>(vx.data()), sizeof(T)*vx.size());
+            inputfile.read(reinterpret_cast<char*>(vy.data()), sizeof(T)*vy.size());
+            inputfile.read(reinterpret_cast<char*>(vz.data()), sizeof(T)*vz.size());
+            inputfile.read(reinterpret_cast<char*>(p_0.data()), sizeof(T)*p_0.size());
+        }
+        else
+            std::cout << "ERROR: " << "in opening file " << filename << std::endl;
+    }
+
+    #ifdef USE_MPI
+    void loadMPI(const std::string &filename)
+    {
+        count = n / nrank;
+        int offset = n % count;
+        
+        workload.resize(nrank);
+        std::vector<int> displs(nrank);
+
+        workload[0] = count+offset;
+        displs[0] = 0;
+
+        for(int i=1; i<nrank; i++)
+        {
+            workload[i] = count;
+            displs[i] = displs[i-1] + workload[i-1];
         }
 
-        // read the contents of the file into the vectors
-        inputfile.read(reinterpret_cast<char*>(x.data()), sizeof(T)*x.size());
-        inputfile.read(reinterpret_cast<char*>(y.data()), sizeof(T)*y.size());
-        inputfile.read(reinterpret_cast<char*>(z.data()), sizeof(T)*z.size());
-        inputfile.read(reinterpret_cast<char*>(vx.data()), sizeof(T)*vx.size());
-        inputfile.read(reinterpret_cast<char*>(vy.data()), sizeof(T)*vy.size());
-        inputfile.read(reinterpret_cast<char*>(vz.data()), sizeof(T)*vz.size());
-        inputfile.read(reinterpret_cast<char*>(p_0.data()), sizeof(T)*p_0.size());
+        if(rank == 0)
+        {
+            count += offset;
 
+            resize(n);
+            load(filename);
+
+            MPI_Scatterv(&x[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&y[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&z[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&vx[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&vy[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&vz[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(&p_0[0], &workload[0], &displs[0], MPI_DOUBLE, MPI_IN_PLACE, count, MPI_DOUBLE, 0, comm);
+
+            resize(count);
+        }
+        else
+        {
+            resize(count);
+
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &x[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &y[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &z[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &vx[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &vy[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &vz[0], count, MPI_DOUBLE, 0, comm);
+            MPI_Scatterv(NULL, &workload[0], &displs[0], MPI_DOUBLE, &p_0[0], count, MPI_DOUBLE, 0, comm);
+        }
+    }
+    #endif
+
+    void init()
+    {
         std::fill(h.begin(), h.end(), 2.0);
         std::fill(temp.begin(), temp.end(), 1.0);
         std::fill(ro_0.begin(), ro_0.end(), 1.0);
@@ -44,7 +121,7 @@ public:
         std::fill(c.begin(), c.end(), 3500.0);
         std::fill(m.begin(), m.end(), 1.0);
 
-        for(int i=0; i<n; i++)
+        for(unsigned int i=0; i<count; i++)
         {
             p_0[i] = p_0[i] * 10.0;
             x[i] = x[i] * 100.0;
@@ -65,7 +142,7 @@ public:
         std::fill(dt.begin(), dt.end(), 1e-6);
         std::fill(dt_m1.begin(), dt_m1.end(), 1e-6);
 
-        for(int i=0; i<n; i++)
+        for(unsigned int i=0; i<count; i++)
         {
             x_m1[i] = x[i] - vx[i] * dt[0];
             y_m1[i] = y[i] - vy[i] * dt[0];
@@ -76,58 +153,74 @@ public:
         bbox.zmax = 50;
     }
 
-    void reorderSwap(const std::vector<int> &ordering, std::vector<T> &data)
+    void writeFile(const std::vector<int> &clist, std::ofstream &outputFile)
     {
-        std::vector<T> tmp(ordering.size());
-        for(unsigned int i=0; i<ordering.size(); i++)
-            tmp[i] = data[ordering[i]];
-        tmp.swap(data);
-    }
+        #ifdef USE_MPI
+            std::vector<int> workload(nrank);
 
-    void reorder(const std::vector<int> &ordering)
-    {
-        reorderSwap(ordering, x);
-        reorderSwap(ordering, y);
-        reorderSwap(ordering, z);
-        reorderSwap(ordering, x_m1);
-        reorderSwap(ordering, y_m1);
-        reorderSwap(ordering, z_m1);
-        reorderSwap(ordering, vx);
-        reorderSwap(ordering, vy);
-        reorderSwap(ordering, vz);
-        reorderSwap(ordering, ro);
-        reorderSwap(ordering, ro_0);
-        reorderSwap(ordering, u);
-        reorderSwap(ordering, p);
-        reorderSwap(ordering, p_0);
-        reorderSwap(ordering, h);
-        reorderSwap(ordering, m);
-        reorderSwap(ordering, c);
-        reorderSwap(ordering, temp);
-        reorderSwap(ordering, grad_P_x);
-        reorderSwap(ordering, grad_P_y);
-        reorderSwap(ordering, grad_P_z);
-        reorderSwap(ordering, du);
-        reorderSwap(ordering, du_m1);
-        reorderSwap(ordering, dt);
-        reorderSwap(ordering, dt_m1);
-    }
+            int load = (int)clist.size();
+            MPI_Allgather(&load, 1, MPI_INT, &workload[0], 1, MPI_INT, MPI_COMM_WORLD);
 
-    void writeFile(std::ofstream &outputFile)
-    {
-        for(int i=0; i<n; i++)
+            std::vector<int> displs(nrank);
+
+            displs[0] = 0;
+            for(int i=1; i<nrank; i++)
+                displs[i] = displs[i-1]+workload[i-1];
+
+            if(rank == 0)
+            {
+                resize(n);
+
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &x[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &y[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &z[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &vx[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &vy[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &vz[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &h[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &ro[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &u[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &p[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &c[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &grad_P_x[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &grad_P_y[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(MPI_IN_PLACE, (int)clist.size(), MPI_DOUBLE, &grad_P_z[0], &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            }
+            else
+            {
+                MPI_Gatherv(&x[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&y[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&z[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&vx[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&vy[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&vz[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&h[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&ro[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&u[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&p[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&c[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&grad_P_x[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&grad_P_y[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                MPI_Gatherv(&grad_P_z[0], (int)clist.size(), MPI_DOUBLE, NULL, &workload[0], &displs[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            }
+        #endif
+
+        if(rank == 0)
         {
-            outputFile << x[i] << ' ' << y[i] << ' ' << z[i] << ' ';
-            outputFile << vx[i] << ' ' << vy[i] << ' ' << vz[i] << ' ';
-            outputFile << h[i] << ' ' << ro[i] << ' ' << u[i] << ' ' << p[i] << ' ' << c[i] << ' ';
-            outputFile << grad_P_x[i] << ' ' << grad_P_y[i] << ' ' << grad_P_z[i] << ' ';
-            T rad = sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
-            T vrad = (vx[i] *  x[i] + vy[i] * y[i] + vz[i] * z[i]) / rad;
-            outputFile << rad << ' ' << vrad << std::endl;  
-        }
+            for(unsigned int i=0; i<n; i++)
+            {
+                outputFile << x[i] << ' ' << y[i] << ' ' << z[i] << ' ';
+                outputFile << vx[i] << ' ' << vy[i] << ' ' << vz[i] << ' ';
+                outputFile << h[i] << ' ' << ro[i] << ' ' << u[i] << ' ' << p[i] << ' ' << c[i] << ' ';
+                outputFile << grad_P_x[i] << ' ' << grad_P_y[i] << ' ' << grad_P_z[i] << ' ';
+                T rad = sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
+                T vrad = (vx[i] *  x[i] + vy[i] * y[i] + vz[i] * z[i]) / rad;
+                outputFile << rad << ' ' << vrad << std::endl;  
+            }  
+        } 
     }
 
-    int n; // Number of particles
+    unsigned int n, count; // Number of particles
     std::vector<T> x, y, z, x_m1, y_m1, z_m1; // Positions
     std::vector<T> vx, vy, vz; // Velocities
     std::vector<T> ro, ro_0; // Density
@@ -147,6 +240,16 @@ public:
     sphexa::BBox<T> bbox;
     std::vector<std::vector<int>> neighbors; // List of neighbor indices per particle.
 
+    #ifdef USE_MPI
+        MPI_Comm comm;
+        int nrank = 0, pnamelen = 0;
+        char pname[MPI_MAX_PROCESSOR_NAME];
+        std::vector<int> workload;
+    #endif
+    
+    int rank = 0;
+
+    std::vector<std::vector<T>*> data;
     const T K = sphexa::compute_3d_k(6.0);
     const T maxDtIncrease = 1.1;
     const int stabilizationTimesteps = 15;
--- src/sqpatch.cpp
+++ src/sqpatch.cpp
@@ -8,15 +8,37 @@
 using namespace std;
 using namespace sphexa;
 
+#define REPORT_TIME(rank, expr, name) (rank == 0)? sphexa::timer::report_time([&](){ expr; }, name) : expr
+
+int neighbors_sum(const std::vector<std::vector<int>> &neighbors)
+{
+    int sum = 0;
+    #pragma omp parallel for reduction (+:sum)
+    for(unsigned int i=0; i<neighbors.size(); i++)
+        sum += neighbors[i].size();
+
+    #ifdef USE_MPI
+        MPI_Allreduce(MPI_IN_PLACE, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    #endif
+
+    return sum;
+}
+
 int main()
 {
     typedef double Real;
-    typedef Octree<Real> Tree;
+    typedef HTree<Real> Tree;
     typedef SqPatch<Real> Dataset;
 
-    int n = 1e6;
-    Dataset d(n, "bigfiles/squarepatch3D_1M.bin");
-    Domain<Real, Tree> domain(d.ngmin, d.ng0, d.ngmax, d.PBCx, d.PBCy, d.PBCz);
+    #ifdef USE_MPI
+        MPI_Init(NULL, NULL);
+        Dataset d(1e6, "bigfiles/squarepatch3D_1M.bin", MPI_COMM_WORLD);
+        DistributedDomain<Real> mpi(MPI_COMM_WORLD);
+    #else
+        Dataset d(1e6, "bigfiles/squarepatch3D_1M.bin");
+    #endif
+
+    Domain<Real, Tree> domain(d.ngmin, d.ng0, d.ngmax);
     Density<Real> density(d.K);
     EquationOfStateSqPatch<Real> equationOfState(d.stabilizationTimesteps);
     MomentumEnergySqPatch<Real> momentumEnergy(d.stabilizationTimesteps, d.K);
@@ -24,45 +46,84 @@ int main()
     UpdateQuantities<Real> updateQuantities(d.stabilizationTimesteps);
     EnergyConservation<Real> energyConservation;
 
+    std::vector<int> clist(d.count);
+    for(unsigned int i=0; i<d.count; i++)
+        clist[i] = i;
+
     for(int iteration = 0; iteration < 200; iteration++)
     {
         timer::TimePoint start = timer::Clock::now();
 
-        cout << "Iteration: " << iteration << endl;
+        if(d.rank == 0) cout << "Iteration: " << iteration << endl;
 
-        REPORT_TIME(domain.buildTree(d.x, d.y, d.z, d.h, d.bbox), "BuildTree");
-        REPORT_TIME(domain.reorderParticles(d), "ReorderParticles");
-        REPORT_TIME(domain.findNeighbors(d.x, d.y, d.z, d.h, d.neighbors), "FindNeighbors");
-        REPORT_TIME(density.compute(d.neighbors, d.x, d.y, d.z, d.h, d.m, d.ro), "Density");
-        REPORT_TIME(equationOfState.compute(iteration, d.ro_0, d.p_0, d.ro, d.p, d.u, d.c), "EquationOfState");
-        REPORT_TIME(momentumEnergy.compute(iteration, d.neighbors, d.x, d.y, d.z, d.h, d.vx, d.vy, d.vz, d.ro, d.p, d.c, d.m, d.grad_P_x, d.grad_P_y, d.grad_P_z, d.du), "MomentumEnergy");
-        REPORT_TIME(timestep.compute(d.h, d.c, d.dt_m1, d.dt), "Timestep");
-        REPORT_TIME(updateQuantities.compute(iteration, d.grad_P_x, d.grad_P_y, d.grad_P_z, d.dt, d.du, d.bbox, d.x, d.y, d.z, d.vx, d.vy, d.vz, d.x_m1, d.y_m1, d.z_m1, d.u, d.du_m1, d.dt_m1), "UpdateQuantities");
-        REPORT_TIME(energyConservation.compute(d.u, d.vx, d.vy, d.vz, d.m, d.etot, d.ecin, d.eint), "EnergyConservation");
+        #ifdef USE_MPI
+            d.resize(d.count);
+            REPORT_TIME(d.rank, mpi.build(clist, d.workload, d.x, d.y, d.z, d.h), "mpi::build");
+            REPORT_TIME(d.rank, mpi.synchronize(d.data), "mpi::synchronize");
+            REPORT_TIME(d.rank, mpi.discard(d.data), "mpi::discard");
+            
+            d.count = d.data[0]->size();
+            d.neighbors.resize(d.count);
+            clist.resize(d.count);
 
-        int totalNeighbors = 0;
-        #pragma omp parallel for reduction (+:totalNeighbors)
-        for(unsigned int i=0; i<d.neighbors.size(); i++)
-            totalNeighbors += d.neighbors[i].size();
+            for(unsigned int i=0; i<clist.size(); i++)
+                clist[i] = i;
 
-        cout << "### Check ### Computational domain: ";
-        cout << d.bbox.xmin << " " << d.bbox.xmax << " ";
-        cout << d.bbox.ymin << " " << d.bbox.ymax << " ";
-        cout << d.bbox.zmin << " " << d.bbox.zmax << endl;
-        cout << "### Check ### Avg. number of neighbours: " << totalNeighbors/n << "/" << d.ng0 << endl;
-        cout << "### Check ### New Time-step: " << d.dt[0] << endl;
-        cout << "### Check ### Total energy: " << d.etot << ", (internal: " << d.eint << ", cinetic: " << d.ecin << ")" << endl;
+            REPORT_TIME(d.rank, mpi.build(clist, d.workload, d.x, d.y, d.z, d.h), "mpi::build");
+            REPORT_TIME(d.rank, mpi.synchronizeHalos(clist, d.x, d.y, d.z, d.h, d.data, true), "mpi::synchronizeHalos");
+
+            if(d.rank == 0) cout << "ComputeList.size: " << clist.size() << ", Halos: " << mpi.haloCount << endl;
+        #endif
+
+        REPORT_TIME(d.rank, domain.buildTree(d.x, d.y, d.z, d.h, d.bbox), "BuildTree");
+        // REPORT_TIME(d.rank, mpi.reorder(d.data), "ReorderParticles");
+        REPORT_TIME(d.rank, domain.findNeighbors(clist, d.x, d.y, d.z, d.h, d.neighbors), "FindNeighbors");
+        REPORT_TIME(d.rank, density.compute(clist, d.neighbors, d.x, d.y, d.z, d.h, d.m, d.ro), "Density");
+        REPORT_TIME(d.rank, equationOfState.compute(clist, iteration, d.ro_0, d.p_0, d.ro, d.p, d.u, d.c), "EquationOfState");
+        
+        #ifdef USE_MPI
+            d.resize(d.count);
+            mpi.synchronizeHalos(clist, d.x, d.y, d.z, d.h, d.data);
+        #endif
+
+        REPORT_TIME(d.rank, momentumEnergy.compute(clist, iteration, d.neighbors, d.x, d.y, d.z, d.h, d.vx, d.vy, d.vz, d.ro, d.p, d.c, d.m, d.grad_P_x, d.grad_P_y, d.grad_P_z, d.du), "MomentumEnergy");
+        REPORT_TIME(d.rank, timestep.compute(clist, d.h, d.c, d.dt_m1, d.dt), "Timestep");
+        REPORT_TIME(d.rank, updateQuantities.compute(clist, iteration, d.grad_P_x, d.grad_P_y, d.grad_P_z, d.dt, d.du, d.bbox, d.x, d.y, d.z, d.vx, d.vy, d.vz, d.x_m1, d.y_m1, d.z_m1, d.u, d.du_m1, d.dt_m1), "UpdateQuantities");
+        REPORT_TIME(d.rank, energyConservation.compute(clist, d.u, d.vx, d.vy, d.vz, d.m, d.etot, d.ecin, d.eint), "EnergyConservation");
+        
+        #ifdef USE_MPI
+            d.resize(d.count);
+            mpi.synchronizeHalos(clist, d.x, d.y, d.z, d.h, d.data);
+        #endif
+
+        int totalNeighbors = neighbors_sum(d.neighbors);
+
+        if(d.rank == 0)
+        {
+            cout << "### Check ### Computational domain: ";
+            cout << d.bbox.xmin << " " << d.bbox.xmax << " ";
+            cout << d.bbox.ymin << " " << d.bbox.ymax << " ";
+            cout << d.bbox.zmin << " " << d.bbox.zmax << endl;
+            cout << "### Check ### Avg. number of neighbours: " << totalNeighbors/d.n << "/" << d.ng0 << endl;
+            cout << "### Check ### New Time-step: " << d.dt[0] << endl;
+            cout << "### Check ### Total energy: " << d.etot << ", (internal: " << d.eint << ", cinetic: " << d.ecin << ")" << endl;
+        }
 
         if(iteration % 10 == 0)
         {
             std::ofstream outputFile("output" + to_string(iteration) + ".txt");
-            REPORT_TIME(d.writeFile(outputFile), "writeFile");
+            REPORT_TIME(d.rank, d.writeFile(clist, outputFile), "writeFile");
             outputFile.close();
         }
 
         timer::TimePoint stop = timer::Clock::now();
-        cout << "=== Total time for iteration " << timer::duration(start, stop) << "s" << endl << endl;
+        
+        if(d.rank == 0) cout << "=== Total time for iteration " << timer::duration(start, stop) << "s" << endl << endl;
     }
+
+    #ifdef USE_MPI
+        MPI_Finalize();
+    #endif
 
     return 0;
 }
