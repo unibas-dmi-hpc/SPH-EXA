150aa9440128c7bd0bddb3b258bf90a325297b17
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2019-06-20 11:28:50 +0200

    correctness test

--- test/test_correctness.sh
+++ test/test_correctness.sh
@@ -5,6 +5,7 @@ GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 BOLD='\e[1m'
 
+<<<<<<< HEAD
 EXPECTED_OUTPUT_FILE=$(mktemp /tmp/sph-exa-correctness-test-tmp.XXXXXXXXXX)
 exec 3>"$EXPECTED_OUTPUT_FILE"
 echo "0 1.1e-06 1.1e-06 2.07823e+10 2.07813e+10 1e+06 1734080 
@@ -46,10 +47,38 @@ run_test() {
         return $EXIT_CODE_PASSED
     else
         printf "${RED}${BOLD} $binary binary does not exist. Skipping test\n"
+=======
+EXPECTED_OUTPUT_FILE=expected_constants_n25_s0.txt
+OUTPUT_FILE=constants.txt
+
+BIN_PATH=../bin/
+OMP_BIN=$BIN_PATH/omp.app
+MPIOMP_BIN=$BIN_PATH/mpi+omp.app
+BIN_PARAMS="-n 25 -s 0"
+
+EXIT_CODE_PASSED=1
+EXIT_CODE_FAILED=0
+EXIT_CODE_BINARY_MISSING=2
+
+run_test() {
+    name=$1
+    binary=$2
+    printf "${NOCOLOR}\n"
+
+    if test -f $binary; then
+        $binary $BIN_PARAMS
+
+        cmp --silent $EXPECTED_OUTPUT_FILE $OUTPUT_FILE && printf "${GREEN}${BOLD}$name Correctness test PASSED\n" ||
+                { printf "${RED}${BOLD}Correctness test FAILED\nOutput file diff:\n"; diff $EXPECTED_OUTPUT_FILE $OUTPUT_FILE; return $EXIT_CODE_FAILED; }
+        return $EXIT_CODE_PASSED
+    else
+        printf "${RED}${BOLD}$name binary does not exist in Path: $BIN_PATH, skipping test\n"
+>>>>>>> correctness test
         return $EXIT_CODE_BINARY_MISSING
     fi
 }
 
+<<<<<<< HEAD
 if test -f $OUTPUT_FILE; then rm $OUTPUT_FILE; fi
 
 run_test $BIN "-n 20 -s 10 --quiet"; ret_code=$?
@@ -65,3 +94,21 @@ else
 fi
 
 
+=======
+print_verdict() {
+    result_code=$1
+
+    if [ $result_code -eq $EXIT_CODE_PASSED ]; then printf "${GREEN}${BOLD}PASSED\n";
+    elif [ $result_code -eq $EXIT_CODE_FAILED ]; then printf "${RED}${BOLD}FAILED\n";
+    else printf "${RED}${BOLD}BINARY MISSING\n";
+    fi
+}
+
+run_test "OpenMP" $OMP_BIN; omp_result=$?
+run_test "MPI+OpenMP" $MPIOMP_BIN; mpiomp_result=$?
+
+printf "\n${NOCOLOR}CORRECTNESS TEST SUMMARY\n"
+printf "${NOCOLOR}OpenMP "; print_verdict $omp_result 
+printf "${NOCOLOR}MPI+OpenMP "; print_verdict $mpiomp_result
+printf "\n"
+>>>>>>> correctness test
