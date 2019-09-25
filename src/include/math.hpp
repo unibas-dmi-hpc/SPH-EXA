fafa5aedab927b6ee6bfa97e0a60a379b2451853
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2019-09-25 15:36:19 +0200

    remove sin and cos lookup table

--- src/include/math.hpp
+++ src/include/math.hpp
@@ -14,8 +14,6 @@ namespace sphexa
 {
 namespace math
 {
-/* Small powers, such as the ones used inside the SPH kernel
- * are transformed into straight multiplications. */
 template <typename T>
 CUDA_DEVICE_HOST_FUN inline T pow(T a, int b)
 {
@@ -37,30 +35,6 @@ CUDA_DEVICE_HOST_FUN inline T pow(T a, int b)
         return a * a * a * a * a * a * a;
     else
         return std::pow(a, b);
-}
-
-template <typename T>
-CUDA_DEVICE_FUN inline T cos(T n)
-{
-    namespace lt = sphexa::lookup_tables;
-
-    const T f = n * lt::sinCosLTMaxCircleAngle / PI;
-    const int i = static_cast<int>(f);
-
-    return i < 0 ? lt::fast_cossin_table[((-i) + lt::sinCosLTQuarterMaxCircleAngle) & lt::sinCosLTMaskMaxCircleAngle]
-                 : lt::fast_cossin_table[(i + lt::sinCosLTQuarterMaxCircleAngle) & lt::sinCosLTMaskMaxCircleAngle];
-}
-
-template <typename T>
-CUDA_DEVICE_FUN inline T sin(T n)
-{
-    namespace lt = sphexa::lookup_tables;
-
-    const T f = n * lt::sinCosLTHalfMaxCircleAngle / PI;
-    const int i = static_cast<int>(f);
-
-    return i < 0 ? lt::fast_cossin_table[(-((-i) & lt::sinCosLTMaskMaxCircleAngle)) + lt::sinCosLTMaxCircleAngle]
-                 : lt::fast_cossin_table[i & lt::sinCosLTMaskMaxCircleAngle];
 }
 
 } // namespace math
--- src/include/sph/lookupTables.hpp
+++ src/include/sph/lookupTables.hpp
@@ -48,34 +48,14 @@ std::array<T, N> createWharmonicDerivativeLookupTable()
     return lt;
 }
 
-template <typename T, std::size_t N>
-std::array<T, N> createCosSinLookupTable()
-{
-    std::array<T, N> lt;
-
-    const auto halfSize = N / 2;
-    for (size_t i = 0; i < N; ++i)
-        lt[i] = (T)std::sin((T)i * PI / halfSize);
-
-    return lt;
-}
-
-constexpr int sinCosLTMaxCircleAngle = 131072;
-constexpr int sinCosLTHalfMaxCircleAngle = sinCosLTMaxCircleAngle / 2;
-constexpr int sinCosLTQuarterMaxCircleAngle = sinCosLTMaxCircleAngle / 4;
-constexpr int sinCosLTMaskMaxCircleAngle = sinCosLTMaxCircleAngle - 1;
-constexpr int sinCosLTSize = sinCosLTMaxCircleAngle;
-
 constexpr size_t wharmonicLookupTableSize = 20000;
 
 #if defined(__NVCC__)
 __device__ extern double wharmonicLookupTable[wharmonicLookupTableSize];
 __device__ extern double wharmonicDerivativeLookupTable[wharmonicLookupTableSize];
-__device__ extern double fast_cossin_table[sinCosLTSize];
 #else
 static auto wharmonicLookupTable = createWharmonicLookupTable<double, wharmonicLookupTableSize>();
 static auto wharmonicDerivativeLookupTable = createWharmonicDerivativeLookupTable<double, wharmonicLookupTableSize>();
-static auto fast_cossin_table = createCosSinLookupTable<double, sinCosLTSize>();
 #endif
 
 template <typename T>
@@ -84,14 +64,11 @@ struct GpuLookupTableInitializer
     GpuLookupTableInitializer()
     {
 #if defined(USE_OMP_TARGET)
-#pragma omp target enter data map(to : fast_cossin_table [0:sinCosLTSize])
 #pragma omp target enter data map(to : wharmonicLookupTable [0:wharmonicLookupTableSize])
 #pragma omp target enter data map(to : wharmonicDerivativeLookupTable [0:wharmonicLookupTableSize])
 #elif defined(__NVCC__)
         namespace cuda = sphexa::sph::cuda;
 
-        cuda::CHECK_CUDA_ERR(
-            cudaMemcpyToSymbol(fast_cossin_table, createCosSinLookupTable<T, sinCosLTSize>().data(), sinCosLTSize * sizeof(T)));
         cuda::CHECK_CUDA_ERR(cudaMemcpyToSymbol(wharmonicLookupTable, createWharmonicLookupTable<T, wharmonicLookupTableSize>().data(),
                                                 wharmonicLookupTableSize * sizeof(T)));
         cuda::CHECK_CUDA_ERR(cudaMemcpyToSymbol(wharmonicDerivativeLookupTable,
@@ -103,11 +80,9 @@ struct GpuLookupTableInitializer
     ~GpuLookupTableInitializer()
     {
 #if defined(USE_OMP_TARGET)
-#pragma omp target exit data map(delete : fast_cossin_table [0:sinCosLTSize])
 #pragma omp target exit data map(delete : wharmonicLookupTable [0:wharmonicLookupTableSize])
 #pragma omp target exit data map(delete : wharmonicDerivativeLookupTable [0:wharmonicLookupTableSize])
 #elif defined(__NVCC__)
-        cudaFree(fast_cossin_table);
         cudaFree(wharmonicLookupTable);
         cudaFree(wharmonicDerivativeLookupTable);
 #endif
