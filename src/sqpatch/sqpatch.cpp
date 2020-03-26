d3edd731efe746664847e5f55c9431688fef154e
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2020-03-26 14:48:03 +0100

    print usage with list of arguments

--- src/sqpatch/sqpatch.cpp
+++ src/sqpatch/sqpatch.cpp
@@ -7,16 +7,27 @@
 #include "SqPatchDataGenerator.hpp"
 #include "SqPatchFileWriter.hpp"
 
-using namespace std;
 using namespace sphexa;
+
+void printHelp(char *binName, int rank);
 
 int main(int argc, char **argv)
 {
-    ArgParser parser(argc, argv);
+    const int rank = initAndGetRankId();
+
+    const ArgParser parser(argc, argv);
+
+    if (parser.exists("-h") || parser.exists("--h") || parser.exists("-help") || parser.exists("--help"))
+    {
+        printHelp(argv[0], rank);
+        return exitSuccess();
+    }
+
     const size_t cubeSide = parser.getInt("-n", 50);
     const size_t maxStep = parser.getInt("-s", 10);
     const int writeFrequency = parser.getInt("-w", -1);
     const bool quiet = parser.exists("--quiet");
+    const std::string outDirectory = parser.getString("--outDir");
 
     std::ofstream nullOutput("/dev/null");
     std::ostream &output = quiet ? nullOutput : std::cout;
@@ -26,7 +37,6 @@ int main(int argc, char **argv)
     using Tree = Octree<Real>;
 
 #ifdef USE_MPI
-    MPI_Init(NULL, NULL);
     DistributedDomain<Real, Dataset, Tree> domain;
     const IFileWriter<Dataset> &fileWriter = SqPatchMPIFileWriter<Dataset>();
 #else
@@ -39,7 +49,7 @@ int main(int argc, char **argv)
 
     MasterProcessTimer timer(output, d.rank), totalTimer(output, d.rank);
 
-    std::ofstream constantsFile("constants.txt");
+    std::ofstream constantsFile(outDirectory + "constants.txt");
 
     Tree::bucketSize = 64;
     Tree::minGlobalBucketSize = 512;
@@ -98,8 +108,8 @@ int main(int argc, char **argv)
 
         if ((writeFrequency > 0 && d.iteration % writeFrequency == 0) || writeFrequency == 0)
         {
-            fileWriter.dumpParticleDataToAsciiFile(d, domain.clist, "dump_sqpatch" + to_string(d.iteration) + ".txt");
-            fileWriter.dumpParticleDataToBinFile(d, "dump_sqpatch" + to_string(d.iteration) + ".bin");
+            fileWriter.dumpParticleDataToAsciiFile(d, domain.clist, outDirectory + "dump_sqpatch" + std::to_string(d.iteration) + ".txt");
+            // fileWriter.dumpParticleDataToBinFile(d, outDirectory + "dump_sqpatch" + std::to_string(d.iteration) + ".bin");
             timer.step("writeFile");
         }
 
@@ -112,9 +122,24 @@ int main(int argc, char **argv)
 
     constantsFile.close();
 
-#ifdef USE_MPI
-    MPI_Finalize();
-#endif
+    return exitSuccess();
+}
 
-    return 0;
+void printHelp(char *name, int rank)
+{
+    if (rank == 0)
+    {
+        printf("\nUsage:\n\n");
+        printf("%s [OPTIONS]\n", name);
+        printf("\nWhere possible options are:\n");
+        printf("\t-n NUM \t\t\t NUM^3 Number of particles\n");
+        printf("\t-s NUM \t\t\t NUM Number of iterations (time-steps)\n");
+        printf("\t-w NUM \t\t\t Dump particles data every NUM iterations (time-steps)\n\n");
+
+        printf("\t--quiet \t\t Don't print anything to stdout\n\n");
+
+        printf("\t--outDir PATH \t\t Path to directory where output will be saved.\
+                    \n\t\t\t\t Note that directory must exist and be provided with ending slash.\
+                    \n\t\t\t\t Example: --outDir /home/user/folderToSaveOutputFiles/\n");
+    }
 }
