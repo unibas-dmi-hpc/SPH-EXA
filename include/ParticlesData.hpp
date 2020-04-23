4474be08d1f41a33add547060220eb0c86e3c416
Gabriel Zihlmann <gabriel.zihlmann@unibas.ch>
2020-04-23 11:53:35 +0200

    extended hint what the problem is between task data and particle data

--- include/ParticlesData.hpp
+++ include/ParticlesData.hpp
@@ -34,13 +34,16 @@ struct ParticlesData
     std::vector<T> m;                            // Mass
     std::vector<T> c;                            // Speed of sound
     std::vector<T> grad_P_x, grad_P_y, grad_P_z; // gradient of the pressure
-    std::vector<T> du, du_m1;                    // variation of the energy
+    std::vector<T> du, du_m1;                    // variation of the energy without AV contribution
     std::vector<T> du_av, du_av_m1;              // artificial viscosity contribution of the variation of the energy
     std::vector<T> dt, dt_m1;
     std::vector<T> c11, c12, c13, c22, c23, c33; // IAD components
     std::vector<T> maxvsignal;
     // todo: the nn ones should be reconciled with the neighborscount in the task struct.
     //       problem is that like this here, we can dump it easily for debug, but they're actually uints, not doubles!
+    //       challenge for the dump: I'm not sure how the particle index from the task clist maps to the index in the
+    //       particle data arrays (basically the i = clist[pi] mapping). If we want to dump data from the task struct,
+    //       we need to tell the writeparticledata function which index to use... (whether to apply the mapping or not)
     std::vector<T> nn;                           // number of neighbors
     std::vector<T> nn_actual;                    // actual number of neighbors (not capped by ngmax)
     std::vector<T> gradh;                        // gradh terms (omega)
