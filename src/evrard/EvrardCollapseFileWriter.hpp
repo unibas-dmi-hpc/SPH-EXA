274a05bfdec966d123084d6b81f33b574ebb6c17
gabuzi <15203081+gabuzi@users.noreply.github.com>
2020-05-07 16:16:43 +0200

    Fixed error where ascii dump file only contained last rank's data (#33)

--- src/evrard/EvrardCollapseFileWriter.hpp
+++ src/evrard/EvrardCollapseFileWriter.hpp
@@ -86,16 +86,9 @@ struct EvrardCollapseMPIFileWriter : IFileWriter<Dataset>
         {
             if (turn == d.rank)
             {
-                std::ofstream dump;
-
-                if (d.rank == 0)
-                    dump.open(path);
-                else
-                    dump.open(path, std::ios_base::app);
-
                 try
                 {
-                    fileutils::writeParticleDataToAsciiFile(clist, path, separator, d.x, d.y, d.z, d.vx, d.vy, d.vz, d.h, d.ro, d.u, d.p,
+                    fileutils::writeParticleDataToAsciiFile(clist, path, d.rank != 0, separator, d.x, d.y, d.z, d.vx, d.vy, d.vz, d.h, d.ro, d.u, d.p,
                                                             d.c, d.grad_P_x, d.grad_P_y, d.grad_P_z /*, d.radius*/, d.fx, d.fy, d.fz,
                                                             d.ugrav);
                 }
@@ -104,8 +97,6 @@ struct EvrardCollapseMPIFileWriter : IFileWriter<Dataset>
                     if (d.rank == 0) fprintf(stderr, "ERROR: %s. Terminating\n", ex.what());
                     MPI_Abort(d.comm, ex.mpierr);
                 }
-
-                dump.close();
 
                 MPI_Barrier(MPI_COMM_WORLD);
             }
