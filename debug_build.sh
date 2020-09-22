8734e8f6fc6e8064a696554d9c0c25ba964c4f68
Hck Course <hck01@daint105.login.cscs.ch>
2020-09-22 19:06:43 +0200

    added debug build

--- debug_build.sh
+++ debug_build.sh
@@ -16,6 +16,5 @@ module load craype-accel-nvidia60
 export CRAYPE_LINK_TYPE=dynamic
 export OMP_NUM_THREADS=1
 module rm xalt
-make clean
 make -f Makefile -j 2 CC="cc" CXX="CC" FC="ftn" NVCC="nvcc -g -G" CXXFLAGS="-std=c++14 -g -O0 -DNDEBUG" mpi+omp+cuda SRCDIR=. BUILDDIR=build BINDIR=bin NVCCFLAGS="-std=c++14 -rdc=true -arch=sm_60 -g -G --expt-relaxed-constexpr" NVCCARCH=sm_60 CUDA_PATH=$CUDA_PATH MPICXX=CC
 mv bin/mpi+omp+cuda.app bin/mpi+omp+cuda
