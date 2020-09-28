c1b157d84275c96810656925b15006f8d5ce9551
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2020-09-28 17:20:05 +0200

    use smaller Particlesdata for Sedov

--- include/ParticlesData.hpp
+++ include/ParticlesData.hpp
@@ -82,6 +82,61 @@ struct ParticlesDataSqPatch
     size_t n, side, count;                       // Number of particles
     std::vector<T> x, y, z, x_m1, y_m1, z_m1;    // Positions
     std::vector<T> vx, vy, vz;                   // Velocities
+    std::vector<T> ro, ro_0;                           // Density
+    std::vector<T> u;                            // Internal Energy
+    std::vector<T> p, p_0;                            // Pressure
+    std::vector<T> h;                            // Smoothing Length
+    std::vector<T> m;                            // Mass
+    std::vector<T> c;                            // Speed of sound
+    std::vector<T> grad_P_x, grad_P_y, grad_P_z; // gradient of the pressure
+    std::vector<T> du, du_m1;                    // variation of the energy
+    std::vector<T> dt, dt_m1;
+    std::vector<T> c11, c12, c13, c22, c23, c33; // IAD components
+    std::vector<T> maxvsignal;
+
+    // For Sedov
+    std::vector<T> mui, temp, cv;
+
+    T ttot, etot, ecin, eint;
+    T minDt;
+
+    BBox<T> bbox;
+
+    std::vector<std::vector<T> *> data{&x,   &y,   &z,   &x_m1, &y_m1, &z_m1,     &vx,         &vy,       &vz,   &ro, &ro_0,   &u,
+                                       &p,   &p_0, &h,   &m,    &c,    &grad_P_x, &grad_P_y,   &grad_P_z, &du,   &du_m1, &dt,   &dt_m1,
+                                       &c11, &c12, &c13, &c22,  &c23,  &c33,      &maxvsignal, &mui,      &temp, &cv};
+
+    const std::array<double, lt::size> wh = lt::createWharmonicLookupTable<double, lt::size>();
+    const std::array<double, lt::size> whd = lt::createWharmonicDerivativeLookupTable<double, lt::size>();
+
+#ifdef USE_MPI
+    MPI_Comm comm;
+    int pnamelen = 0;
+    char pname[MPI_MAX_PROCESSOR_NAME];
+#endif
+
+    int rank = 0;
+    int nrank = 1;
+
+    constexpr static T sincIndex = 6.0;
+    constexpr static T Kcour = 0.2;
+    constexpr static T maxDtIncrease = 1.1;
+    const static T K;
+};
+
+template <typename T>
+struct ParticlesData
+{
+    inline void resize(const size_t size)
+    {
+        for (unsigned int i = 0; i < data.size(); ++i)
+            data[i]->resize(size);
+    }
+
+    size_t iteration;                            // Current iteration
+    size_t n, side, count;                       // Number of particles
+    std::vector<T> x, y, z, x_m1, y_m1, z_m1;    // Positions
+    std::vector<T> vx, vy, vz;                   // Velocities
     std::vector<T> ro;                           // Density
     std::vector<T> u;                            // Internal Energy
     std::vector<T> p;                            // Pressure
@@ -104,61 +159,6 @@ struct ParticlesDataSqPatch
 
     std::vector<std::vector<T> *> data{&x,   &y,   &z,   &x_m1, &y_m1, &z_m1,     &vx,         &vy,       &vz,   &ro,    &u,
                                        &p,   &h,   &m,    &c,    &grad_P_x, &grad_P_y,   &grad_P_z, &du,   &du_m1, &dt,   &dt_m1,
-                                       &c11, &c12, &c13, &c22,  &c23,  &c33,      &maxvsignal, &mui,      &temp, &cv};
-
-    const std::array<double, lt::size> wh = lt::createWharmonicLookupTable<double, lt::size>();
-    const std::array<double, lt::size> whd = lt::createWharmonicDerivativeLookupTable<double, lt::size>();
-
-#ifdef USE_MPI
-    MPI_Comm comm;
-    int pnamelen = 0;
-    char pname[MPI_MAX_PROCESSOR_NAME];
-#endif
-
-    int rank = 0;
-    int nrank = 1;
-
-    constexpr static T sincIndex = 6.0;
-    constexpr static T Kcour = 0.2;
-    constexpr static T maxDtIncrease = 1.1;
-    const static T K;
-};
-
-template <typename T>
-struct ParticlesDataSqPatch
-{
-    inline void resize(const size_t size)
-    {
-        for (unsigned int i = 0; i < data.size(); ++i)
-            data[i]->resize(size);
-    }
-
-    size_t iteration;                            // Current iteration
-    size_t n, side, count;                       // Number of particles
-    std::vector<T> x, y, z, x_m1, y_m1, z_m1;    // Positions
-    std::vector<T> vx, vy, vz;                   // Velocities
-    std::vector<T> ro, ro_0;                     // Density
-    std::vector<T> u;                            // Internal Energy
-    std::vector<T> p, p_0;                            // Pressure
-    std::vector<T> h;                            // Smoothing Length
-    std::vector<T> m;                            // Mass
-    std::vector<T> c;                            // Speed of sound
-    std::vector<T> grad_P_x, grad_P_y, grad_P_z; // gradient of the pressure
-    std::vector<T> du, du_m1;                    // variation of the energy
-    std::vector<T> dt, dt_m1;
-    std::vector<T> c11, c12, c13, c22, c23, c33; // IAD components
-    std::vector<T> maxvsignal;
-
-    // For Sedov
-    std::vector<T> mui, temp, cv;
-
-    T ttot, etot, ecin, eint;
-    T minDt;
-
-    BBox<T> bbox;
-
-    std::vector<std::vector<T> *> data{&x,   &y,   &z,   &x_m1, &y_m1, &z_m1,     &vx,         &vy,       &vz,   &ro, &ro_0,   &u,
-                                       &p,   &p_0, &h,   &m,    &c,    &grad_P_x, &grad_P_y,   &grad_P_z, &du,   &du_m1, &dt,   &dt_m1,
                                        &c11, &c12, &c13, &c22,  &c23,  &c33,      &maxvsignal, &mui,      &temp, &cv};
 
     const std::array<double, lt::size> wh = lt::createWharmonicLookupTable<double, lt::size>();
