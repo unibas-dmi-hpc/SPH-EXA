0ceec7ef88cb7ef8812af0060bd25aebe8f7f77f
Sebastian Keller <keller@cscs.ch>
2020-09-28 11:30:45 +0200

    morton3D code tests pass

--- include/sfc.hpp
+++ include/sfc.hpp
@@ -20,14 +20,19 @@ unsigned int expandBits(unsigned int v)
 
 // Calculates a 30-bit Morton code for the
 // given 3D point located within the unit cube [0,1].
-unsigned int morton3D(double x, double y, double z)
+template<class T>
+unsigned int morton3D(T x, T y, T z)
 {
+    assert( x >= 0.0 && x <= 1.0);
+    assert( y >= 0.0 && y <= 1.0);
+    assert( z >= 0.0 && z <= 1.0);
+
     // normalize floating point numbers
-    // 1024 = 2^10, so we can take the
-    // 10 most significant bits from the mantissa
-    x = std::min(std::max(x * 1024.0, 0.0), 1023.0);
-    y = std::min(std::max(y * 1024.0, 0.0), 1023.0);
-    z = std::min(std::max(z * 1024.0, 0.0), 1023.0);
+    // 1024 = 2^10, so we map the floating point numbers
+    // in [0,1] to [0,1023] and convert to integers
+    x = std::min(std::max(x * T(1024.0), T(0.0)), T(1023.0));
+    y = std::min(std::max(y * T(1024.0), T(0.0)), T(1023.0));
+    z = std::min(std::max(z * T(1024.0), T(0.0)), T(1023.0));
     unsigned int xx = detail::expandBits((unsigned int)x);
     unsigned int yy = detail::expandBits((unsigned int)y);
     unsigned int zz = detail::expandBits((unsigned int)z);
--- test/unit/sfc_tests.cpp
+++ test/unit/sfc_tests.cpp
@@ -10,11 +10,18 @@
 
 TEST(SFC, mortonIndex) {
 
-    double x = 0.1;
-    double y = 0.2;
-    double z = 0.3;
+    double x = 0.00489; // 5 on the scale 0-1023
+    double y = 0.00196; // 2
+    double z = 0.00391; // 4
 
-    EXPECT_EQ(0, sphexa::morton3D(x, y, z));
+    // binary representation:
+    // x = 101
+    // y = 010
+    // z = 100
+    // Morton code is 101010100 = 340
+
+    EXPECT_EQ(340, sphexa::morton3D(float(x), float(y), float(z)));
+    EXPECT_EQ(340, sphexa::morton3D(x, y, z));
 }
 
 int main(int argc, char **argv) {
