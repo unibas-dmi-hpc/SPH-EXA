11f7ce3206775ecee50a1344f9d1f68eb8552acc
Sebastian Keller <44800242+sebkelle1@users.noreply.github.com>
2020-10-01 13:47:44 +0200

    SFC tests (#47)
    * added SFC test harness
    
    * morton3D code tests pass
    
    * SFC implementation and tests
    
    * link tests against MPI
    
    * enabled CTest
    
    * reorganized files
    
    * reorganized test files
    
    * added 64-bit morton code
    
    * added 32bit morton decode
    
    * added 64-bit morton decode
    
    * sfc neighbor discovery
    
    Co-authored-by: Sebastian Keller <sebkelle@ethz.ch>

---
+++ include/sfc/zorder.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <numeric>
+#include <tuple>
+#include <vector>
+
+#include "mortoncode.hpp"
+
+namespace sphexa
+{
+/*! \brief calculate the sortKey that sortes the input sequence
+ *
+ * \param[in] inBegin input sequence start 
+ * \param[in] inEnd input sequence end
+ * \param[out] outBegin integer sortKey output
+ *
+ * upon completion of this routine, the output sequence containes the sort keys
+ * that will access the input sequence in a sorted manner, i.e.
+ * the sequence inBegin[outBegin[k]], k=0,1,2,...,n is sorted
+ */
+template <class InputIterator, class OutputIterator>
+void sort_invert(InputIterator inBegin, InputIterator inEnd, OutputIterator outBegin)
+{
+    using ValueType = std::decay_t<decltype(*inBegin)>;
+    using Integer   = std::decay_t<decltype(*outBegin)>;
+    std::size_t n   = std::distance(inBegin, inEnd);
+
+    OutputIterator outEnd = outBegin + n;
+    // create index sequence 0,1,2,...,n
+    std::iota(outBegin, outEnd, 0);
+
+    // zip the input integer array together with the index sequence
+    std::vector<std::tuple<ValueType, Integer>> keyIndexPairs(n);
+    std::transform(inBegin, inEnd, outBegin, begin(keyIndexPairs),
+                   [](ValueType i1, Integer i2) { return std::make_tuple(i1, i2); });
+
+    // sort, comparing only the first tuple element
+    std::sort(begin(keyIndexPairs), end(keyIndexPairs),
+              [](const auto& t1, const auto& t2){ return std::get<0>(t1) < std::get<0>(t2); });
+
+    // extract the resulting ordering
+    std::transform(begin(keyIndexPairs), end(keyIndexPairs), outBegin,
+                   [](const auto& tuple) { return std::get<1>(tuple); } );
+}
+
+/*! \brief compute the Morton z-order for the input coordinate arrays
+ *
+ * \param[in]  [x,y,z][Begin, End] (const) input iterators for coordinate arrays
+ * \param[out] order[Begin, End]  output for z-order
+ * \param[in]  [x,y,z][min, max]  coordinate bounding box
+ */
+template<class InputIterator, class OutputIterator, class T>
+void computeZorder(InputIterator  xBegin,
+                   InputIterator  xEnd,
+                   InputIterator  yBegin,
+                   InputIterator  zBegin,
+                   OutputIterator orderBegin,
+                   T xmin, T xmax, T ymin, T ymax, T zmin, T zmax)
+{
+    std::size_t n = std::distance(xBegin, xEnd);
+
+    std::vector<unsigned> mortonCodes(n); 
+    computeMortonCodes(xBegin, xEnd, yBegin, zBegin, begin(mortonCodes), xmin, xmax, ymin, ymax, zmin, xmax);
+
+    sort_invert(begin(mortonCodes), end(mortonCodes), orderBegin);
+}
+
+} // namespace sphexa
