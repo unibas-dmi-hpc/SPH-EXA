89d5b86bb55c8572ce3275401af3b5990a0437ad
Sebastian Keller <sebkelle@ethz.ch>
2020-10-16 18:26:36 +0200

    test octree aggregation with random gaussian normal coordinates

--- test/unit/sfc/randombox.hpp
+++ test/unit/sfc/randombox.hpp
@@ -35,10 +35,10 @@ public:
         std::vector<I> mortonOrder(n);
         sphexa::sort_invert(cbegin(codes_), cend(codes_), begin(mortonOrder));
 
-        reorder(mortonOrder, codes_);
-        reorder(mortonOrder, x_);
-        reorder(mortonOrder, y_);
-        reorder(mortonOrder, z_);
+        sphexa::reorder(mortonOrder, codes_);
+        sphexa::reorder(mortonOrder, x_);
+        sphexa::reorder(mortonOrder, y_);
+        sphexa::reorder(mortonOrder, z_);
     }
 
     const std::vector<T>& x() const { return x_; }
@@ -48,19 +48,54 @@ public:
 
 private:
 
-    template<class ValueType>
-    void reorder(const std::vector<I>& ordering, std::vector<ValueType>& array)
-    {
-        std::vector<ValueType> tmp(array.size());
-        for (std::size_t i = 0; i < array.size(); ++i)
-        {
-            tmp[i] = array[ordering[i]];
-        }
-        std::swap(tmp, array);
-    }
-
     sphexa::Box<T> box_;
     std::vector<T> x_, y_, z_;
     std::vector<I> codes_;
 };
 
+template<class T, class I>
+class RandomGaussianCoordinates
+{
+public:
+
+    RandomGaussianCoordinates(unsigned n, sphexa::Box<T> box)
+        : box_(std::move(box)), x_(n), y_(n), z_(n), codes_(n)
+    {
+        //std::random_device rd;
+        std::mt19937 gen(42);
+        // random gaussian distribution at the center
+        std::normal_distribution<T> disX((box_.xmax() - box_.xmin())/2, (box_.xmax() - box_.xmin())/5);
+        std::normal_distribution<T> disY((box_.ymax() - box_.ymin())/2, (box_.ymax() - box_.ymin())/5);
+        std::normal_distribution<T> disZ((box_.zmax() - box_.zmin())/2, (box_.zmax() - box_.zmin())/5);
+
+        auto randX = [cmin=box_.xmin(), cmax=box_.xmax(), &disX, &gen]() { return std::max(std::min(disX(gen), cmax), cmin); };
+        auto randY = [cmin=box_.ymin(), cmax=box_.ymax(), &disY, &gen]() { return std::max(std::min(disY(gen), cmax), cmin); };
+        auto randZ = [cmin=box_.zmin(), cmax=box_.zmax(), &disZ, &gen]() { return std::max(std::min(disZ(gen), cmax), cmin); };
+
+        std::generate(begin(x_), end(x_), randX);
+        std::generate(begin(y_), end(y_), randY);
+        std::generate(begin(z_), end(z_), randZ);
+
+        sphexa::computeMortonCodes(begin(x_), end(x_), begin(y_), begin(z_),
+                                   begin(codes_), box);
+
+        std::vector<I> mortonOrder(n);
+        sphexa::sort_invert(cbegin(codes_), cend(codes_), begin(mortonOrder));
+
+        sphexa::reorder(mortonOrder, codes_);
+        sphexa::reorder(mortonOrder, x_);
+        sphexa::reorder(mortonOrder, y_);
+        sphexa::reorder(mortonOrder, z_);
+    }
+
+    const std::vector<T>& x() const { return x_; }
+    const std::vector<T>& y() const { return y_; }
+    const std::vector<T>& z() const { return z_; }
+    const std::vector<I>& mortonCodes() const { return codes_; }
+
+private:
+
+    sphexa::Box<T> box_;
+    std::vector<T> x_, y_, z_;
+    std::vector<I> codes_;
+};
