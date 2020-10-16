10defdf564820001464c45107f5cdf555e6d6b23
Sebastian Keller <sebkelle@ethz.ch>
2020-10-16 16:29:28 +0200

    sfc octree working prototype

--- include/sfc/octree.hpp
+++ include/sfc/octree.hpp
@@ -48,66 +48,54 @@ inline bool operator==(const SfcNode<I>& lhs, const SfcNode<I>& rhs)
 
 }
 
-/*! \brief aggregate mortonCodes into octree leaf nodes
+/*! \brief aggregate mortonCodes into octree leaf nodes of increased size
  *
  * \tparam I              32- or 64-bit unsigned integer
  * \param[in] mortonCodes input mortonCode array
  * \param[in] bucketSize  determine size of octree nodes such that
- *                        (leaf node).count < bucketSize
+ *                        (leaf node).count <= bucketSize
  *                        and for their parents (<=> internal nodes)
- *                        (parent node).count >= bucketSize
+ *                        (parent node).count > bucketSize
  *
  * \return vector with the sorted octree leaf nodes
  */
 template<class I>
-std::vector<SfcNode<I>> generateOctree(const std::vector<I>& mortonCodes, unsigned bucketSize)
+std::vector<SfcNode<I>> trimZCurve(const std::vector<I>& mortonCodes, unsigned bucketSize)
 {
     std::vector<SfcNode<I>> ret;
 
     unsigned n = mortonCodes.size();
     unsigned i = 0;
-    //while (i < n)
+
+    I previousBoxEnd = 0;
+
+    while (i < n)
     {
         I code = mortonCodes[i];
-        unsigned upperIndex = std::min(n, i + bucketSize - 1);
 
+        // the smallest code more than bucketSize away
+        // need to find a box that stays below it
+        I codeLimit = (i + bucketSize < n) ? mortonCodes[i + bucketSize] : nodeRange<I>(0);
 
+        // find smallest j in [i, i + bucketSize], such that codeLimit < get<1>(smallestCommonBox(mCodes[i], mCodes[j]))
+        auto isInBox = [code](I c1_, I c2_){ return c1_ < std::get<1>(smallestCommonBox(code, c2_)); };
+        auto jIt = std::upper_bound(cbegin(mortonCodes) + i, cbegin(mortonCodes) + std::min(n, i + bucketSize), codeLimit, isInBox);
+        unsigned j = jIt - cbegin(mortonCodes);
 
-        I upperCode = mortonCodes[upperIndex];
+        // find smallest k in [i, i + bucketSize], such that not(get<0>(smallestCommonBox(mCodes[i], mCodes[k])) < previousBoxEnd)
+        auto isBelowBox = [code](I c1_, I c2_){ return !(std::get<0>(smallestCommonBox(code, c1_)) < c2_); };
+        auto kIt = std::lower_bound(cbegin(mortonCodes) + i, cbegin(mortonCodes) + std::min(n, i + bucketSize), previousBoxEnd, isBelowBox);
+        unsigned k = kIt - cbegin(mortonCodes);
 
-        // if the resolution of the SFC is not enough
-        // to tell bucketSize particles apart at the lowest level
-        if (code == upperCode)
-        {
-            upperIndex = std::upper_bound(cbegin(mortonCodes) + i, cend(mortonCodes), code)
-                           - cbegin(mortonCodes);
-            if (upperIndex == n)
-            {
-                ret.push_back(SfcNode<I>{code, upperCode, i, upperIndex});
-                return ret;
-            }
-            else
-            {
-                upperCode = mortonCodes[upperIndex];
-            }
-        }
+        // the smaller of the two indices is the one that produces a range of codes
+        // with an enclosing octree box that both doesn't overlap with the previous one
+        // and does not include more than bucketSize particles
+        j = std::min(j, k);
 
-        // guaranteed to be > 0
-        I codeDifference = upperCode - code;
-        unsigned containingTreeLevel = (countLeadingZeros(codeDifference) - unusedBits<I>{})/3 + 1;
-        I range = 1u << (3*(maxTreeLevel<I>{} - containingTreeLevel));
-
-        I nodeStart = detail::enclosingBoxCode(code, containingTreeLevel);
-        I nodeEnd   = nodeStart + range;
-
-        // count the number of codes in the range [nodeStart, nodeEnd)
-        upperIndex = std::lower_bound(cbegin(mortonCodes) + i, cbegin(mortonCodes) + upperIndex, nodeEnd)
-                       - cbegin(mortonCodes);
-
-        unsigned count = upperIndex - i;
-        ret.push_back(SfcNode<I>{nodeStart, nodeEnd, i, count});
-
-        i += count;
+        std::tuple<I, I> box = smallestCommonBox(code, mortonCodes[j-1]);
+        ret.push_back(SfcNode<I>{std::get<0>(box), std::get<1>(box), i, j-i});
+        i = j;
+        previousBoxEnd = std::get<1>(box);
     }
 
     return ret;
--- test/unit/sfc/octree.cpp
+++ test/unit/sfc/octree.cpp
@@ -58,28 +58,21 @@ public:
         },
         nodes{
             sphexa::SfcNode<I>{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 7},
-            sphexa::SfcNode<I>{codeFromIndices<I>({1}), codeFromIndices<I>({2}), 7, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,0}), codeFromIndices<I>({2,1,1}), 8, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,1}), codeFromIndices<I>({2,1,2}), 9, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,2}), codeFromIndices<I>({2,1,3}), 10, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,3}), codeFromIndices<I>({2,1,4}), 11, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,4}), codeFromIndices<I>({2,1,5}), 12, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,5}), codeFromIndices<I>({2,1,6}), 13, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,6}), codeFromIndices<I>({2,1,7}), 14, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({2,1,7}), codeFromIndices<I>({2,2}), 15, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({3}), codeFromIndices<I>({4}), 16, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({4}), codeFromIndices<I>({5}), 17, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({5}), codeFromIndices<I>({6}), 18, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({6}), codeFromIndices<I>({7}), 19, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({1}), codeFromIndices<I>({1})+1, 7, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({2,1}), codeFromIndices<I>({2,2}), 8, 8},
+            sphexa::SfcNode<I>{codeFromIndices<I>({3}), codeFromIndices<I>({3})+1, 16, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({4}), codeFromIndices<I>({4})+1, 17, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({5}), codeFromIndices<I>({5})+1, 18, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({6}), codeFromIndices<I>({6})+1, 19, 1},
 
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,0}), codeFromIndices<I>({7,1}), 20, 2},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,1}), codeFromIndices<I>({7,2}), 22, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,2}), codeFromIndices<I>({7,3}), 23, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,3}), codeFromIndices<I>({7,4}), 24, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,4}), codeFromIndices<I>({7,5}), 25, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,5}), codeFromIndices<I>({7,6}), 25, 1},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,6}), codeFromIndices<I>({7,7}), 26, 7},
-            sphexa::SfcNode<I>{codeFromIndices<I>({7,7}), 8*codeFromIndices<I>({1}), 33, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,0}), codeFromIndices<I>({7,0})+8, 20, 2},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,1}), codeFromIndices<I>({7,1})+1, 22, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,2}), codeFromIndices<I>({7,2})+1, 23, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,3}), codeFromIndices<I>({7,3})+1, 24, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,4}), codeFromIndices<I>({7,4})+1, 25, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,5}), codeFromIndices<I>({7,5})+1, 26, 1},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,6}), codeFromIndices<I>({7,7}),   27, 7},
+            sphexa::SfcNode<I>{codeFromIndices<I>({7,7}), codeFromIndices<I>({7,7})+1, 34, 1},
         }
     {
     }
@@ -89,14 +82,177 @@ public:
     std::vector<sphexa::SfcNode<I>> nodes;
 };
 
-TEST(Octree, construct)
+TEST(Octree, trimExample32)
 {
-    using CodeType = unsigned;
+    using I = unsigned;
     unsigned bucketSize = 8;
 
-    ExampleOctree<CodeType> tree;
+    ExampleOctree<I> tree;
 
-    //auto leafs = sphexa::generateOctree(tree.codes, bucketSize);
+    auto leaves = sphexa::trimZCurve(tree.codes, bucketSize);
+    EXPECT_EQ(leaves, tree.nodes);
+}
 
-    //EXPECT_EQ(leafs[0], tree.nodes[0]);
+TEST(Octree, trimExample64)
+{
+    using I = uint64_t;
+    unsigned bucketSize = 8;
+
+    ExampleOctree<I> tree;
+
+    auto leaves = sphexa::trimZCurve(tree.codes, bucketSize);
+    EXPECT_EQ(leaves, tree.nodes);
+}
+
+TEST(Octree, trim8)
+{
+    using I = unsigned;
+    unsigned bucketSize = 8;
+
+    std::vector<I> codes
+    {
+        codeFromIndices<I>({0, 1}),
+        codeFromIndices<I>({0, 2}),
+        codeFromIndices<I>({0, 3}),
+        codeFromIndices<I>({0, 4}),
+        codeFromIndices<I>({0, 5}),
+        codeFromIndices<I>({0, 6}),
+        codeFromIndices<I>({0, 7}),
+        codeFromIndices<I>({1}),
+        codeFromIndices<I>({2,1}),
+    };
+
+    {
+        I code = codes[0];
+        I codeLimit = codes[8];
+        auto isInBox = [code](I c1_, I c2_){ return std::get<1>(sphexa::smallestCommonBox(code, c1_)) < c2_; };
+        //auto ub = std::upper_bound(cbegin(codes), cbegin(codes) + 8, codeLimit, isInBox);
+        //unsigned j = ub - cbegin(codes);
+        EXPECT_EQ(true, isInBox(codes[6], codeLimit));
+        EXPECT_EQ(false, isInBox(codes[7], codeLimit));
+    }
+
+    auto leaves = sphexa::trimZCurve(codes, bucketSize);
+
+    sphexa::SfcNode<I> node0{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 7};
+    sphexa::SfcNode<I> node1{codeFromIndices<I>({1}), codeFromIndices<I>({1})+1, 7, 1};
+    sphexa::SfcNode<I> node2{codeFromIndices<I>({2,1}), codeFromIndices<I>({2,1})+1, 8, 1};
+
+    EXPECT_EQ(leaves[0], node0);
+    EXPECT_EQ(leaves[1], node1);
+    EXPECT_EQ(leaves[2], node2);
+}
+
+TEST(Octree, trim9)
+{
+    using I = unsigned;
+    unsigned bucketSize = 8;
+
+    std::vector<I> codes
+    {
+        codeFromIndices<I>({0, 0}),
+        codeFromIndices<I>({0, 1}),
+        codeFromIndices<I>({0, 2}),
+        codeFromIndices<I>({0, 3}),
+        codeFromIndices<I>({0, 4}),
+        codeFromIndices<I>({0, 5}),
+        codeFromIndices<I>({0, 6}),
+        codeFromIndices<I>({0, 7}),
+        codeFromIndices<I>({1}),
+    };
+
+    auto leaves = sphexa::trimZCurve(codes, bucketSize);
+
+    sphexa::SfcNode<I> node0{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 8};
+    sphexa::SfcNode<I> node1{codeFromIndices<I>({1}), codeFromIndices<I>({1}) + 1, 8, 1};
+
+    EXPECT_EQ(leaves[0], node0);
+    EXPECT_EQ(leaves[1], node1);
+}
+
+TEST(Octree, trim10)
+{
+    using I = unsigned;
+    unsigned bucketSize = 8;
+
+    std::vector<I> codes
+    {
+        codeFromIndices<I>({0, 0}),
+        codeFromIndices<I>({0, 1}),
+        codeFromIndices<I>({0, 2}),
+        codeFromIndices<I>({0, 3}),
+        codeFromIndices<I>({0, 4}),
+        codeFromIndices<I>({0, 5}),
+        codeFromIndices<I>({0, 6}),
+        codeFromIndices<I>({0, 7}),
+        codeFromIndices<I>({1}),
+        codeFromIndices<I>({1,0,0,5}),
+    };
+
+    auto leaves = sphexa::trimZCurve(codes, bucketSize);
+
+    sphexa::SfcNode<I> node0{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 8};
+    sphexa::SfcNode<I> node1{codeFromIndices<I>({1}), codeFromIndices<I>({1, 0, 1}), 8, 2};
+
+    EXPECT_EQ(leaves[0], node0);
+    EXPECT_EQ(leaves[1], node1);
+}
+
+TEST(Octree, trim10a)
+{
+    using I = unsigned;
+    unsigned bucketSize = 8;
+
+    std::vector<I> codes
+    {
+        codeFromIndices<I>({0, 0}),
+        codeFromIndices<I>({0, 1}),
+        codeFromIndices<I>({0, 2}),
+        codeFromIndices<I>({0, 3}),
+        codeFromIndices<I>({0, 4}),
+        codeFromIndices<I>({0, 5}),
+        codeFromIndices<I>({0, 6}),
+        codeFromIndices<I>({0, 7}),
+        codeFromIndices<I>({1,0,0,0,3}),
+        codeFromIndices<I>({1,0,0,5}),
+    };
+
+    auto leaves = sphexa::trimZCurve(codes, bucketSize);
+
+    sphexa::SfcNode<I> node0{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 8};
+    sphexa::SfcNode<I> node1{codeFromIndices<I>({1}), codeFromIndices<I>({1, 0, 1}), 8, 2};
+
+    EXPECT_EQ(leaves[0], node0);
+    EXPECT_EQ(leaves[1], node1);
+}
+
+TEST(Octree, trim10b)
+{
+    using I = unsigned;
+    unsigned bucketSize = 8;
+
+    std::vector<I> codes
+    {
+        codeFromIndices<I>({0, 0}),
+        codeFromIndices<I>({0, 1}),
+        codeFromIndices<I>({0, 2}),
+        codeFromIndices<I>({0, 3}),
+        codeFromIndices<I>({0, 4}),
+        codeFromIndices<I>({0, 5}),
+        codeFromIndices<I>({0, 6}),
+        codeFromIndices<I>({0, 7}),
+        codeFromIndices<I>({1}),
+        codeFromIndices<I>({2}),
+    };
+
+    auto leaves = sphexa::trimZCurve(codes, bucketSize);
+
+    sphexa::SfcNode<I> node0{codeFromIndices<I>({0}), codeFromIndices<I>({1}), 0, 8};
+    sphexa::SfcNode<I> node1{codeFromIndices<I>({1}), codeFromIndices<I>({1})+1, 8, 1};
+    sphexa::SfcNode<I> node2{codeFromIndices<I>({2}), codeFromIndices<I>({2})+1, 9, 1};
+
+    ASSERT_EQ(leaves.size(), 3);
+    EXPECT_EQ(leaves[0], node0);
+    EXPECT_EQ(leaves[1], node1);
+    EXPECT_EQ(leaves[2], node2);
 }
