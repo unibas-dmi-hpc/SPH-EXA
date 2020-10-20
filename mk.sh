f982fde247060db4e3bbb23562f7fa21c7ec58a5
Cypox <am_neggaz@esi.dz>
2020-10-20 16:38:49 +0200

    removed script files

--- mk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CRAYPE_LINK_TYPE=dynamic
-export OMP_NUM_THREADS=1
-
-make clean
-make -f Makefile -j 2 mpi+omp+cuda SRCDIR=. BUILDDIR=build BINDIR=bin NVCC="nvcc" NVCCARCH=sm_52
