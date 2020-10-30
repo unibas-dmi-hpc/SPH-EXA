6e9e71fc9b285ac4c865d5066df7a6daf17ae688
Sebastian Keller <sebkelle@ethz.ch>
2020-10-30 19:25:13 +0100

    moved coordinate test out of findNeighbors

--- test/unit/sfc_components/findneighbors.cpp
+++ test/unit/sfc_components/findneighbors.cpp
@@ -44,27 +44,6 @@ TEST(FindNeighbors, treeLevel)
     EXPECT_EQ(2, sphexa::radiusToTreeLevel(0.126, 1.));
 }
 
-TEST(FindNeighbors, coordinateContainerIsSorted)
-{
-    using real = double;
-    using CodeType = unsigned;
-    int n = 10;
-
-    sphexa::Box<real> box{ 0, 1, -1, 2, 0, 5 };
-    RandomCoordinates<real, CodeType> c(n, box);
-
-    std::vector<CodeType> testCodes(n);
-    sphexa::computeMortonCodes(begin(c.x()), end(c.x()), begin(c.y()), begin(c.z()),
-                               begin(testCodes), box);
-
-    EXPECT_EQ(testCodes, c.mortonCodes());
-
-    std::vector<CodeType> testCodesSorted = testCodes;
-    std::sort(begin(testCodesSorted), end(testCodesSorted));
-
-    EXPECT_EQ(testCodes, testCodesSorted);
-}
-
 
 template<class T, class I>
 class NeighborCheck
