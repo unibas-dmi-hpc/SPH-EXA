87d085832be1643ee658c2b30b5af4761b39ca76
Sebastian Keller <sebkelle@ethz.ch>
2020-12-07 20:50:34 +0100

    added test harness for findHalos

--- include/sfc/halos.hpp
+++ include/sfc/halos.hpp
@@ -17,15 +17,25 @@ struct IncomingHaloRange
     int sourceRank;
 };
 
+
+
 template <class I, class T>
-std::vector<IncomingHaloRange<I>> findIncomingHalos(std::vector<I>&                 globalTree,
-                                                    const std::vector<std::size_t>& globalCounts,
+std::vector<IncomingHaloRange<I>> findIncomingHalos(const std::vector<I>&           tree,
+                                                    const std::vector<std::size_t>& counts,
                                                     const std::vector<T>&           interactionRadii,
-                                                    const Box<T>&                   globalBox,
-                                                    const SpaceCurveAssignment<I>&  assignment)
+                                                    const Box<T>&                   box,
+                                                    const SpaceCurveAssignment<I>&  assignment,
+                                                    int                             rank)
 {
+    std::vector<IncomingHaloRange<I>> ret;
 
+    for (int node = 0; node < nNodes(tree); ++node)
+    {
+        T h  = interactionRadii[node]; // smoothing length
+        T hn = normalize(h, box.xmin(), box.xmax());
+    }
 
+    return ret;
 }
 
 } // namespace sphexa
