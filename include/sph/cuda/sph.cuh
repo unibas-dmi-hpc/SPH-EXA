2fecfebde79d14d3689a47c7781b1726f86a6fef
cypox <am_neggaz@esi.dz>
2020-12-08 13:22:55 +0100

    fixed non cuda build errors

--- include/sph/cuda/sph.cuh
+++ include/sph/cuda/sph.cuh
@@ -4,7 +4,9 @@
 
 #include "Task.hpp"
 #include "LinearOctree.hpp"
+#if defined(USE_CUDA)
 #include "cudaParticlesData.cuh"
+#endif
 
 namespace sphexa
 {
@@ -12,12 +14,14 @@ namespace sph
 {
 namespace cuda
 {
+#if defined(USE_CUDA)
 namespace kernels
 {
 template <typename T>
 __global__ void findNeighbors(const DeviceLinearOctree<T> o, const int *clist, const int n, const T *x, const T *y, const T *z, const T *h, const T displx,
                               const T disply, const T displz, const int max, const int may, const int maz, const int ngmax, int *neighbors, int *neighborsCount);
 } // namespace kernels
+#endif
 
 template <typename T, class Dataset>
 extern void computeFindNeighbors2(const LinearOctree<T> &o, std::vector<Task> &taskList, Dataset &d);
