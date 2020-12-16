de25b3d023c78d03b29d80359dc9c8fc2ee3150f
Sebastian Keller <sebkelle@ethz.ch>
2020-12-16 09:50:06 +0100

    test 4x4x4 grid internal binary tree

--- test/unit/sfc_components/binarytree.cpp
+++ test/unit/sfc_components/binarytree.cpp
@@ -3,13 +3,14 @@
 #include "gtest/gtest.h"
 
 #include "sfc/binarytree.hpp"
+#include "sfc/octree.hpp"
 
 namespace sphexa
 {
 
 /*! Create a set of example octree leaves
  *
- * This example also provided in the original paper referenced in sfc/binarytree.hpp.
+ * This example is illustrated in the original paper referenced in sfc/binarytree.hpp.
  * Please refer to the publication for a graphical illustration of the resulting
  * node connectivity.
  */
@@ -77,13 +78,12 @@ TEST(BinaryTree, findSplit)
 }
 
 template<class I>
-void findChildrenTest()
+void paperExampleTest()
 {
     using CodeType = I;
 
     std::vector<CodeType> example = makeExample<CodeType>();
-
-    std::vector<BinaryNode<CodeType>> internalNodes(example.size() - 1);
+    std::vector<BinaryNode<CodeType>> internalNodes = createInternalTree(example);
 
     std::vector<BinaryNode<CodeType>*> refLeft
         {
@@ -114,11 +114,6 @@ void findChildrenTest()
 
     for (int idx = 0; idx < internalNodes.size(); ++idx)
     {
-        constructInternalNode(example.data(), example.size(), internalNodes.data(), idx);
-    }
-
-    for (int idx = 0; idx < internalNodes.size(); ++idx)
-    {
         EXPECT_EQ(internalNodes[idx].leftChild,      refLeft[idx]);
         EXPECT_EQ(internalNodes[idx].leftLeafIndex,  refLeftIndices[idx]);
         EXPECT_EQ(internalNodes[idx].rightChild,     refRight[idx]);
@@ -129,8 +124,58 @@ void findChildrenTest()
 
 } // namespace sphexa
 
-TEST(BinaryTree, findChildren)
+TEST(BinaryTree, internalIrregular)
 {
-    sphexa::findChildrenTest<unsigned>();
-    sphexa::findChildrenTest<uint64_t>();
+    sphexa::paperExampleTest<unsigned>();
+    sphexa::paperExampleTest<uint64_t>();
+}
+
+/*! \brief add (binary) zeros behind a prefix
+ *
+ * Allows comparisons, such as number of leading common bits (cpr)
+ * of the prefix with Morton codes.
+ *
+ * @tparam I      32- or 64-bit unsigned integer type
+ * @param prefix  the bit pattern
+ * @param length  number of bits in the prefix
+ * @return        prefix padded out with zeros
+ *
+ */
+template <class I>
+constexpr I pad(I prefix, int length)
+{
+    return prefix << (3*sphexa::maxTreeLevel<I>{} - length);
+}
+
+TEST(BinaryTree, padUtility)
+{
+    EXPECT_EQ(pad(0b011,   3), 0b00011 << 27);
+    EXPECT_EQ(pad(0b011ul, 3), 0b0011ul << 60);
+}
+
+namespace sphexa
+{
+
+template <class I>
+void internal4x4x4()
+{
+    // a tree with 4 subdivisions along each dimension, 64 nodes
+    std::vector<I> tree = detail::makeUniformNLevelTree<I>(64, 1);
+
+    auto internalTree = createInternalTree(tree);
+    EXPECT_EQ(internalTree[0].prefixLength, 0);
+
+    EXPECT_EQ(internalTree[31].prefixLength, 1);
+    EXPECT_GE(cpr(internalTree[31].prefix, pad(I(0b011), 3)), 3);
+
+    EXPECT_EQ(internalTree[32].prefixLength, 1);
+    EXPECT_GE(cpr(internalTree[32].prefix, pad(I(0b100), 3)), 3);
+}
+
+} // namespace sphexa
+
+TEST(BinaryTree, internalTreeRegular4x4x4)
+{
+    sphexa::internal4x4x4<unsigned>();
+    sphexa::internal4x4x4<uint64_t>();
 }
