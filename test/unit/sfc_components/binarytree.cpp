80a1785b62c08d2c6ddf85abc97b6eb1f00a76e9
Sebastian Keller <sebkelle@ethz.ch>
2020-12-18 09:55:23 +0100

    expand documentation

--- test/unit/sfc_components/binarytree.cpp
+++ test/unit/sfc_components/binarytree.cpp
@@ -5,130 +5,6 @@
 #include "sfc/binarytree.hpp"
 #include "sfc/octree.hpp"
 
-namespace sphexa
-{
-
-/*! Create a set of example octree leaves
- *
- * This example is illustrated in the original paper referenced in sfc/binarytree.hpp.
- * Please refer to the publication for a graphical illustration of the resulting
- * node connectivity.
- */
-
-template<class I>
-std::vector<I> makeExample();
-
-template<>
-std::vector<unsigned> makeExample()
-{
-    std::vector<unsigned> ret
-        {
-            0b0000001u << 25u,
-            0b0000010u << 25u,
-            0b0000100u << 25u,
-            0b0000101u << 25u,
-            0b0010011u << 25u,
-            0b0011000u << 25u,
-            0b0011001u << 25u,
-            0b0011110u << 25u
-        };
-   return ret;
-}
-
-template<>
-std::vector<uint64_t> makeExample()
-{
-    std::vector<uint64_t> ret
-        {
-            0b000001ul << 58u,
-            0b000010ul << 58u,
-            0b000100ul << 58u,
-            0b000101ul << 58u,
-            0b010011ul << 58u,
-            0b011000ul << 58u,
-            0b011001ul << 58u,
-            0b011110ul << 58u
-        };
-    return ret;
-}
-
-template<class I>
-void findSplitTest()
-{
-    std::vector<I> example = makeExample<I>();
-
-    {
-        int split = findSplit(example.data(), 0, 7);
-        EXPECT_EQ(split, 3);
-    }
-    {
-        int split = findSplit(example.data(), 0, 3);
-        EXPECT_EQ(split, 1);
-    }
-    {
-        int split = findSplit(example.data(), 4, 7);
-        EXPECT_EQ(split, 4);
-    }
-}
-
-TEST(BinaryTree, findSplit)
-{
-    findSplitTest<unsigned>();
-    findSplitTest<uint64_t>();
-}
-
-template<class I>
-void paperExampleTest()
-{
-    using CodeType = I;
-
-    std::vector<CodeType> example = makeExample<CodeType>();
-    std::vector<BinaryNode<CodeType>> internalNodes = createInternalTree(example);
-
-    std::vector<BinaryNode<CodeType>*> refLeft
-        {
-            internalNodes.data() + 3,
-            nullptr,
-            nullptr,
-            internalNodes.data() + 1,
-            nullptr,
-            internalNodes.data() + 6,
-            nullptr
-        };
-
-    std::vector<BinaryNode<CodeType>*> refRight
-        {
-            internalNodes.data() + 4,
-            nullptr,
-            nullptr,
-            internalNodes.data() + 2,
-            internalNodes.data() + 5,
-            nullptr,
-            nullptr
-        };
-
-    std::vector<int> refLeftIndices {-1, 0, 2, -1, 4, -1, 5};
-    std::vector<int> refRightIndices{-1, 1, 3, -1, -1, 7, 6};
-
-    std::vector<int> refPrefixLengths{0, 3, 4, 2, 1, 2, 4};
-
-    for (int idx = 0; idx < internalNodes.size(); ++idx)
-    {
-        EXPECT_EQ(internalNodes[idx].leftChild,      refLeft[idx]);
-        EXPECT_EQ(internalNodes[idx].leftLeafIndex,  refLeftIndices[idx]);
-        EXPECT_EQ(internalNodes[idx].rightChild,     refRight[idx]);
-        EXPECT_EQ(internalNodes[idx].rightLeafIndex, refRightIndices[idx]);
-        EXPECT_EQ(internalNodes[idx].prefixLength,   refPrefixLengths[idx]);
-    }
-}
-
-} // namespace sphexa
-
-TEST(BinaryTree, internalIrregular)
-{
-    sphexa::paperExampleTest<unsigned>();
-    sphexa::paperExampleTest<uint64_t>();
-}
 
 /*! \brief add (binary) zeros behind a prefix
  *
@@ -262,13 +138,13 @@ namespace sphexa
  * 2. For each leaf enlarged by the halo range, find collisions
  *    between all the other leaves.
  *
- * 3. a) For each leaf, compute x,y,z coordinate range of the leaf + halo radius
+ * 3. a) For each leaf, compute x,y,z coordinate ranges of the leaf + halo radius
  *    b) Test all the other leaves for overlap with the ranges of part a)
  *       If a collision between the node pair was reported in 2., there has to be overlap,
  *       if no collision was reported, there must not be any overlap.
  */
 template <class I>
-void internal4x4x4traversalTest()
+void regular4x4x4traversalTest()
 {
     /// 1.
     // a tree with 4 subdivisions along each dimension, 64 nodes
@@ -351,8 +227,144 @@ void internal4x4x4traversalTest()
 
 } // namespace sphexa
 
-TEST(BinaryTree, internalTree4x4x4FullTraversal)
+TEST(BinaryTree, regularTree4x4x4FullTraversal)
 {
-    sphexa::internal4x4x4traversalTest<unsigned>();
+    sphexa::regular4x4x4traversalTest<unsigned>();
     sphexa::overlapTest<uint64_t>();
+}
+
+
+namespace sphexa
+{
+
+}
+
+TEST(BinaryTree, irregularTreeTraversal)
+{
+
+}
+
+namespace sphexa
+{
+
+/*! Create a set of example octree leaves
+ *
+ * This example is illustrated in the original paper referenced in sfc/binarytree.hpp.
+ * Please refer to the publication for a graphical illustration of the resulting
+ * node connectivity.
+ */
+
+template<class I>
+std::vector<I> makeExample();
+
+template<>
+std::vector<unsigned> makeExample()
+{
+    std::vector<unsigned> ret
+        {
+            0b0000001u << 25u,
+            0b0000010u << 25u,
+            0b0000100u << 25u,
+            0b0000101u << 25u,
+            0b0010011u << 25u,
+            0b0011000u << 25u,
+            0b0011001u << 25u,
+            0b0011110u << 25u
+        };
+    return ret;
+}
+
+template<>
+std::vector<uint64_t> makeExample()
+{
+    std::vector<uint64_t> ret
+        {
+            0b000001ul << 58u,
+            0b000010ul << 58u,
+            0b000100ul << 58u,
+            0b000101ul << 58u,
+            0b010011ul << 58u,
+            0b011000ul << 58u,
+            0b011001ul << 58u,
+            0b011110ul << 58u
+        };
+    return ret;
+}
+
+template<class I>
+void findSplitTest()
+{
+    std::vector<I> example = makeExample<I>();
+
+    {
+        int split = findSplit(example.data(), 0, 7);
+        EXPECT_EQ(split, 3);
+    }
+    {
+        int split = findSplit(example.data(), 0, 3);
+        EXPECT_EQ(split, 1);
+    }
+    {
+        int split = findSplit(example.data(), 4, 7);
+        EXPECT_EQ(split, 4);
+    }
+}
+
+TEST(BinaryTree, findSplit)
+{
+    findSplitTest<unsigned>();
+    findSplitTest<uint64_t>();
+}
+
+template<class I>
+void paperExampleTest()
+{
+    using CodeType = I;
+
+    std::vector<CodeType> example = makeExample<CodeType>();
+    std::vector<BinaryNode<CodeType>> internalNodes = createInternalTree(example);
+
+    std::vector<BinaryNode<CodeType>*> refLeft
+        {
+            internalNodes.data() + 3,
+            nullptr,
+            nullptr,
+            internalNodes.data() + 1,
+            nullptr,
+            internalNodes.data() + 6,
+            nullptr
+        };
+
+    std::vector<BinaryNode<CodeType>*> refRight
+        {
+            internalNodes.data() + 4,
+            nullptr,
+            nullptr,
+            internalNodes.data() + 2,
+            internalNodes.data() + 5,
+            nullptr,
+            nullptr
+        };
+
+    std::vector<int> refLeftIndices {-1, 0, 2, -1, 4, -1, 5};
+    std::vector<int> refRightIndices{-1, 1, 3, -1, -1, 7, 6};
+
+    std::vector<int> refPrefixLengths{0, 3, 4, 2, 1, 2, 4};
+
+    for (int idx = 0; idx < internalNodes.size(); ++idx)
+    {
+        EXPECT_EQ(internalNodes[idx].leftChild,      refLeft[idx]);
+        EXPECT_EQ(internalNodes[idx].leftLeafIndex,  refLeftIndices[idx]);
+        EXPECT_EQ(internalNodes[idx].rightChild,     refRight[idx]);
+        EXPECT_EQ(internalNodes[idx].rightLeafIndex, refRightIndices[idx]);
+        EXPECT_EQ(internalNodes[idx].prefixLength,   refPrefixLengths[idx]);
+    }
+}
+
+} // namespace sphexa
+
+TEST(BinaryTree, internalIrregular)
+{
+    sphexa::paperExampleTest<unsigned>();
+    sphexa::paperExampleTest<uint64_t>();
 }
