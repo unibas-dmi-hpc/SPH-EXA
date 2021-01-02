49a3ffb85a6eeac14265b8db18c80aac56e78665
Sebastian Keller <sebkelle@ethz.ch>
2021-01-02 12:41:36 +0100

    test for prepareHaloExchange

--- include/sfc/halos.hpp
+++ include/sfc/halos.hpp
@@ -54,7 +54,7 @@ struct HaloRange
  * The source rank of the halo is also the destination where the internal node referenced in the first
  * pair element must be sent to.
  */
-template <class I, class T>
+template<class I, class T>
 void findHalos(const std::vector<I>&           tree,
                const std::vector<T>&           interactionRadii,
                const Box<T>&                   box,
@@ -79,6 +79,8 @@ void findHalos(const std::vector<I>&           tree,
             // find out with which other nodes in the octree that the node at nodeIdx
             // enlarged by the halo radius collides with
             Box<int> haloBox = makeHaloBox(tree[nodeIdx], tree[nodeIdx + 1], radius, box);
+
+            // TODO skip collision detection if is the halo box is fully contained in the assigned range
             findCollisions(internalTree.data(), tree.data(), collisions, haloBox);
 
             // Go through all colliding nodes to determine which of them fall into a part of the SFC
@@ -114,6 +116,56 @@ void findHalos(const std::vector<I>&           tree,
                 }
             }
         }
+    }
+}
+
+/*! \brief Compute send/receive node lists from halo pair node indices
+ *
+ * @tparam     I                32- or 64-bit unsigned integer
+ * @param[in]  tree             cornerstone octree
+ * @param[in]  assignment       stores which rank owns which part of the SFC
+ * @param[in]  haloPairs        list of mutually overlapping pairs of local/remote nodes
+ * @param[out] incomingNodes    sorted list of halo nodes to be received,
+ *                              grouped by source rank
+ * @param[out] outgoingNodes    sorted list of internal nodes to be sent,
+ *                              grouped by destination rank
+ */
+template<class I>
+void prepareHaloExchange(const std::vector<I>& tree,
+                         const SpaceCurveAssignment<I>& assignment,
+                         const std::vector<pair<int>>& haloPairs,
+                         std::vector<std::vector<int>>& incomingNodes,
+                         std::vector<std::vector<int>>& outgoingNodes)
+{
+    // needed to efficiently look up the assigned rank of a given octree node
+    SfcLookupKey<I> sfcLookup(assignment);
+
+    for (auto& p : haloPairs)
+    {
+        // as defined in findHalos, the internal node index is stored first
+        int internalNodeIdx = p[0];
+        int remoteNodeIdx   = p[1];
+
+        int remoteRank = sfcLookup.findRank(tree[remoteNodeIdx]);
+
+        incomingNodes[remoteRank].push_back(remoteNodeIdx);
+        outgoingNodes[remoteRank].push_back(internalNodeIdx);
+    }
+
+    // remove duplicates in receiver list
+    for (auto& v : incomingNodes)
+    {
+        std::sort(begin(v), end(v));
+        auto unique_end = std::unique(begin(v), end(v));
+        v.erase(unique_end, end(v));
+    }
+
+    // remove duplicates in sender list
+    for (auto& v : outgoingNodes)
+    {
+        std::sort(begin(v), end(v));
+        auto unique_end = std::unique(begin(v), end(v));
+        v.erase(unique_end, end(v));
     }
 }
 
--- test/unit/sfc_components/halos.cpp
+++ test/unit/sfc_components/halos.cpp
@@ -3,8 +3,7 @@
 
 #include "sfc/halos.hpp"
 
-namespace sphexa
-{
+using namespace sphexa;
 
 /*! \brief find halo test
  *
@@ -23,7 +22,6 @@ namespace sphexa
 template <class I>
 void findHalos()
 {
-
     // a tree with 4 subdivisions along each dimension, 64 nodes
     std::vector<I> tree = makeUniformNLevelTree<I>(64, 1);
 
@@ -72,10 +70,63 @@ void findHalos()
     }
 }
 
-} // namespace sphexa
-
-TEST(Halos, findIncomingHalos)
+TEST(Halos, findHalos)
 {
-    sphexa::findHalos<unsigned>();
-    sphexa::findHalos<uint64_t>();
+    findHalos<unsigned>();
+    findHalos<uint64_t>();
+}
+
+
+//! \brief test processing of halo pair nodes into send/receive node lists
+template <class I>
+void prepareHaloExchange()
+{
+    // a tree with 4 subdivisions along each dimension, 64 nodes
+    std::vector<I> tree = makeUniformNLevelTree<I>(64, 1);
+
+    // two domains
+    SpaceCurveAssignment<I> assignment(2);
+    assignment.addRange(Rank(0), tree[0], tree[32], 64);
+    assignment.addRange(Rank(1), tree[32], tree[64], 64);
+
+    Box<double> box(0, 1);
+
+    // size of one node is 0.25^3
+    std::vector<double> interactionRadii(nNodes(tree), 0.1);
+
+    std::vector<pair<int>> haloPairs;
+    for (int i = 0; i < nNodes(tree) / 2; ++i)
+        for (int j = nNodes(tree) / 2; j < nNodes(tree); ++j)
+        {
+            if (overlap(tree[i], tree[i + 1], makeHaloBox(tree[j], tree[j + 1], interactionRadii[j], box)))
+            {
+                haloPairs.emplace_back(i, j);
+            }
+        }
+    std::sort(begin(haloPairs), end(haloPairs));
+
+    {
+        std::vector<std::vector<int>> incomingHalos(assignment.nRanks());
+        std::vector<std::vector<int>> outgoingHalos(assignment.nRanks());
+
+        prepareHaloExchange<I>(tree, assignment, haloPairs, incomingHalos, outgoingHalos);
+
+        std::vector<std::vector<int>> refIncomingHalos(assignment.nRanks());
+        std::vector<std::vector<int>> refOutgoingHalos(assignment.nRanks());
+
+        std::vector<int> frontier0{4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31};
+        std::vector<int> frontier1{32,33,34,35,40,41,42,43,48,49,50,51,56,57,58,59};
+
+        refIncomingHalos[1] = frontier1;
+        refOutgoingHalos[1] = frontier0;
+
+        EXPECT_EQ(incomingHalos, refIncomingHalos);
+        EXPECT_EQ(outgoingHalos, refOutgoingHalos);
+    }
+}
+
+TEST(Halos, prepareHaloExchange)
+{
+    prepareHaloExchange<unsigned>();
+    prepareHaloExchange<uint64_t>();
 }
