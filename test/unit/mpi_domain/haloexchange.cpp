7dcbd56636801b388dc5a7406c12cb8a9e9bf193
Sebastian Keller <sebkelle@ethz.ch>
2021-01-07 13:53:44 +0100

    converted halo exchange to purely range-based input

--- test/unit/mpi_domain/haloexchange.cpp
+++ test/unit/mpi_domain/haloexchange.cpp
@@ -12,7 +12,7 @@ void simpleTest(int thisRank)
 {
     int nRanks = 2;
     std::vector<int> nodeList{0,1,10,11};
-    std::vector<int> offsets{0,1,3,6,10};
+    std::vector<int>  offsets{0,1,3,6,10};
     ArrayLayout layout(std::move(nodeList), std::move(offsets));
 
     if (thisRank == 0)
@@ -20,22 +20,22 @@ void simpleTest(int thisRank)
     if (thisRank == 1)
         layout.addLocalRange(10,12);
 
-    std::vector<std::vector<int>> incomingHalos(nRanks);
-    std::vector<std::vector<int>> outgoingHalos(nRanks);
+    SendList incomingHalos(nRanks);
+    SendList outgoingHalos(nRanks);
 
     if (thisRank == 0)
     {
-        incomingHalos[1].push_back(10);
-        incomingHalos[1].push_back(11);
-        outgoingHalos[1].push_back(0);
-        outgoingHalos[1].push_back(1);
+        incomingHalos[1].addRange(3,6);
+        incomingHalos[1].addRange(6,10);
+        outgoingHalos[1].addRange(0,1);
+        outgoingHalos[1].addRange(1,3);
     }
     if (thisRank == 1)
     {
-        incomingHalos[0].push_back(0);
-        incomingHalos[0].push_back(1);
-        outgoingHalos[0].push_back(10);
-        outgoingHalos[0].push_back(11);
+        incomingHalos[0].addRange(0,1);
+        incomingHalos[0].addRange(1,3);
+        outgoingHalos[0].addRange(3,6);
+        outgoingHalos[0].addRange(6,10);
     }
 
     EXPECT_EQ(layout.totalSize(), 10);
@@ -66,7 +66,7 @@ void simpleTest(int thisRank)
         EXPECT_EQ(yOrig, y);
     }
 
-    haloexchange<T>(layout, incomingHalos, outgoingHalos, thisRank, x.data(), y.data());
+    haloexchange<T>(incomingHalos, outgoingHalos, x.data(), y.data());
 
     std::vector<T> xRef{20,21,22,23,24,25,26,27,28,29};
     std::vector<T> yRef{30,31,32,33,34,35,36,37,38,39};
