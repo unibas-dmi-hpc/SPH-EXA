02eb4371f94f5e5f0d412e60069ef014d56049a0
Sebastian Keller <sebkelle@ethz.ch>
2021-01-09 16:20:20 +0100

    fixed halo radius normalization and added regression test

--- test/unit/collision_reference/collisions_a2a.hpp
+++ test/unit/collision_reference/collisions_a2a.hpp
@@ -39,9 +39,9 @@ std::vector<CollisionList> findCollisionsAll2all(const std::vector<I>& tree, con
     {
         T radius = haloRadii[leafIdx];
 
-        int dx = detail::toNBitInt<I>(abs(normalize(radius, globalBox.xmin(), globalBox.xmax())));
-        int dy = detail::toNBitInt<I>(abs(normalize(radius, globalBox.ymin(), globalBox.ymax())));
-        int dz = detail::toNBitInt<I>(abs(normalize(radius, globalBox.zmin(), globalBox.zmax())));
+        int dx = detail::toNBitInt<I>(radius / (globalBox.xmax() - globalBox.xmin()));
+        int dy = detail::toNBitInt<I>(radius / (globalBox.ymax() - globalBox.ymin()));
+        int dz = detail::toNBitInt<I>(radius / (globalBox.zmax() - globalBox.zmin()));
 
         Box<int> haloBox = makeHaloBox(tree[leafIdx], tree[leafIdx + 1], dx, dy, dz);
         findCollisions2All(tree, collisions[leafIdx], haloBox);
