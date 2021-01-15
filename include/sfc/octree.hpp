3ab3726c083fc60f1128dc7204a09e0edd010dd0
Sebastian Keller <sebkelle@ethz.ch>
2021-01-15 16:44:43 +0100

    completed file level doxygen and added MIT license

--- include/sfc/octree.hpp
+++ include/sfc/octree.hpp
@@ -1,13 +1,32 @@
-#pragma once
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-#include <vector>
-
-#include "sfc/mortoncode.hpp"
-
-/*! \brief \file Generation of local and global octrees in cornerstone format
+/*! \file
+ * \brief Generation of local and global octrees in cornerstone format
+ *
+ * \author Sebastian Keller <sebastian.f.keller@gmail.com>
  *
  * In the cornerstone format, the octree is stored as sequence of Morton codes
  * fulfilling three invariants. Each code in the sequence both signifies the
@@ -29,6 +48,15 @@
  *        in the vector
  *      - a vector of length N represents N-1 leaf nodes
  */
+
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <vector>
+
+#include "sfc/mortoncode.hpp"
 
 namespace sphexa
 {
--- test/unit/mpi_domain/domain_nranks.cpp
+++ test/unit/mpi_domain/domain_nranks.cpp
@@ -1,3 +1,44 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! \file
+ * \brief Domain tests with n-ranks
+ *
+ * \author Sebastian Keller <sebastian.f.keller@gmail.com>
+ *
+ * Each rank creates identical random gaussian distributed particles.
+ * Then each ranks grabs 1/n-th of those particles and uses them
+ * to build the global domain, rejoining the same set of particles, but
+ * distributed. Neighbors are then calculated for each local particle on each rank
+ * and the total number of neighbors is summed up across all all ranks.
+ *
+ * This neighbor sum is then compared again the neighbor sum obtained from the original
+ * array that has all the global particles and tests that they match.
+ *
+ * This tests that the domain halo exchange finds all halos needed for a correct neighbor count.
+ */
 
 #include "gtest/gtest.h"
 
@@ -143,7 +184,7 @@ void randomGaussianDomain(int rank, int nRanks)
     }
 }
 
-TEST(Domain, moreHalos)
+TEST(Domain, randomGaussianNeighborSum)
 {
     int rank = 0, nRanks = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
