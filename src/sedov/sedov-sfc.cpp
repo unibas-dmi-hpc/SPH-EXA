94c8578f7a3c273887807ba01f86e847d3fddf96
Sebastian Keller <sebkelle@ethz.ch>
2021-01-18 11:13:02 +0100

    added prototype sedov-sfc

--- src/sedov/sedov-sfc.cpp
+++ src/sedov/sedov-sfc.cpp
@@ -3,11 +3,18 @@
 #include <string>
 #include <vector>
 
+// hard code MPI for now
+#define USE_MPI
+
+#include "cstone/domain.hpp"
+
 #include "sphexa.hpp"
 #include "SedovDataGenerator.hpp"
 #include "SedovDataFileWriter.hpp"
 
+
 using namespace sphexa;
+using namespace cstone;
 
 void printHelp(char *binName, int rank);
 
@@ -33,16 +40,10 @@ int main(int argc, char **argv)
     std::ostream &output = quiet ? nullOutput : std::cout;
 
     using Real = double;
+    using CodeType = unsigned;
     using Dataset = ParticlesData<Real>;
-    using Tree = Octree<Real>;
 
-#ifdef USE_MPI
-    DistributedDomain<Real, Dataset, Tree> domain;
-    const IFileWriter<Dataset> &fileWriter = SedovMPIFileWriter<Dataset>();
-#else
-    Domain<Real, Dataset, Tree> domain;
-    const IFileWriter<Dataset> &fileWriter = SedovFileWriter<Dataset>();
-#endif
+    //const IFileWriter<Dataset> &fileWriter = SedovMPIFileWriter<Dataset>();
 
     auto d = SedovDataGenerator<Real>::generate(cubeSide);
     const Printer<Dataset> printer(d);
@@ -54,75 +55,78 @@ int main(int argc, char **argv)
     std::ofstream constantsFile(outDirectory + "constants.txt");
 
     // -n 350, 42M per node
-    Tree::bucketSize = 64;
-    Tree::minGlobalBucketSize = 512;//std::pow(2,12);
-    Tree::maxGlobalBucketSize = 2048;//std::pow(2,14);//1048576;
-    domain.create(d);
+    const int bucketSize = 512;
+    cstone::Domain<CodeType, Real> domain(rank, d.nrank, bucketSize);
 
     if(d.rank == 0) std::cout << "Domain created." << std::endl;
+
+    std::vector<CodeType> codes;
+    domain.sync(d.x, d.y, d.z, d.y, codes);
+
+    if(d.rank == 0) std::cout << "Domain synchronized, nLocalParticles " << d.x.size() << std::endl;
 
     const size_t nTasks = 64;
     const size_t ngmax = 150;
     const size_t ng0 = 100;
-    TaskList taskList = TaskList(domain.clist, nTasks, ngmax, ng0);
+    //TaskList taskList = TaskList(domain.clist, nTasks, ngmax, ng0);
 
     if(d.rank == 0) std::cout << "Starting main loop." << std::endl;
 
     totalTimer.start();
-    for (d.iteration = 0; d.iteration <= maxStep; d.iteration++)
-    {
-        timer.start();
+    //for (d.iteration = 0; d.iteration <= maxStep; d.iteration++)
+    //{
+    //    timer.start();
 
-        domain.update(d);
-        timer.step("domain::distribute");
-        domain.synchronizeHalos(&d.x, &d.y, &d.z, &d.h);
-        timer.step("mpi::synchronizeHalos");
-        domain.buildTree(d);
-        timer.step("domain::buildTree");
-        taskList.update(domain.clist);
-        timer.step("updateTasks");
-        sph::findNeighbors(domain.octree, taskList.tasks, d);
-        timer.step("FindNeighbors");
-        if(domain.clist.size() > 0) sph::computeDensity<Real>(taskList.tasks, d);
-        timer.step("Density");
-        sph::computeEquationOfStateEvrard<Real>(taskList.tasks, d);
-        timer.step("EquationOfState");
-        domain.synchronizeHalos(&d.vx, &d.vy, &d.vz, &d.ro, &d.p, &d.c);
-        timer.step("mpi::synchronizeHalos");
-        if(domain.clist.size() > 0) sph::computeIAD<Real>(taskList.tasks, d);
-        timer.step("IAD");
-        domain.synchronizeHalos(&d.c11, &d.c12, &d.c13, &d.c22, &d.c23, &d.c33);
-        timer.step("mpi::synchronizeHalos");
-        if(domain.clist.size() > 0) sph::computeMomentumAndEnergyIAD<Real>(taskList.tasks, d);
-        timer.step("MomentumEnergyIAD");
-        sph::computeTimestep<Real, sph::TimestepPress2ndOrder<Real, Dataset>>(taskList.tasks, d);
-        timer.step("Timestep"); // AllReduce(min:dt)
-        sph::computePositions<Real, sph::computeAcceleration<Real, Dataset>>(taskList.tasks, d);
-        timer.step("UpdateQuantities");
-        sph::computeTotalEnergy<Real>(taskList.tasks, d);
-        timer.step("EnergyConservation"); // AllReduce(sum:ecin,ein)
-        sph::updateSmoothingLength<Real>(taskList.tasks, d);
-        timer.step("UpdateSmoothingLength");
+    //    domain.update(d);
+    //    timer.step("domain::distribute");
+    //    domain.synchronizeHalos(&d.x, &d.y, &d.z, &d.h);
+    //    timer.step("mpi::synchronizeHalos");
+    //    domain.buildTree(d);
+    //    timer.step("domain::buildTree");
+    //    taskList.update(domain.clist);
+    //    timer.step("updateTasks");
+    //    sph::findNeighbors(domain.octree, taskList.tasks, d);
+    //    timer.step("FindNeighbors");
+    //    if(domain.clist.size() > 0) sph::computeDensity<Real>(taskList.tasks, d);
+    //    timer.step("Density");
+    //    sph::computeEquationOfStateEvrard<Real>(taskList.tasks, d);
+    //    timer.step("EquationOfState");
+    //    domain.synchronizeHalos(&d.vx, &d.vy, &d.vz, &d.ro, &d.p, &d.c);
+    //    timer.step("mpi::synchronizeHalos");
+    //    if(domain.clist.size() > 0) sph::computeIAD<Real>(taskList.tasks, d);
+    //    timer.step("IAD");
+    //    domain.synchronizeHalos(&d.c11, &d.c12, &d.c13, &d.c22, &d.c23, &d.c33);
+    //    timer.step("mpi::synchronizeHalos");
+    //    if(domain.clist.size() > 0) sph::computeMomentumAndEnergyIAD<Real>(taskList.tasks, d);
+    //    timer.step("MomentumEnergyIAD");
+    //    sph::computeTimestep<Real, sph::TimestepPress2ndOrder<Real, Dataset>>(taskList.tasks, d);
+    //    timer.step("Timestep"); // AllReduce(min:dt)
+    //    sph::computePositions<Real, sph::computeAcceleration<Real, Dataset>>(taskList.tasks, d);
+    //    timer.step("UpdateQuantities");
+    //    sph::computeTotalEnergy<Real>(taskList.tasks, d);
+    //    timer.step("EnergyConservation"); // AllReduce(sum:ecin,ein)
+    //    sph::updateSmoothingLength<Real>(taskList.tasks, d);
+    //    timer.step("UpdateSmoothingLength");
 
-        const size_t totalNeighbors = sph::neighborsSum(taskList.tasks);
+    //    const size_t totalNeighbors = sph::neighborsSum(taskList.tasks);
 
-        if (d.rank == 0)
-        {
-            printer.printCheck(d.count, domain.octree.globalNodeCount, d.x.size() - d.count, totalNeighbors, output);
-            printer.printConstants(d.iteration, totalNeighbors, constantsFile);
-        }
+    //    if (d.rank == 0)
+    //    {
+    //        printer.printCheck(d.count, domain.octree.globalNodeCount, d.x.size() - d.count, totalNeighbors, output);
+    //        printer.printConstants(d.iteration, totalNeighbors, constantsFile);
+    //    }
 
-        if ((writeFrequency > 0 && d.iteration % writeFrequency == 0) || writeFrequency == 0)
-        {
-            //fileWriter.dumpParticleDataToAsciiFile(d, domain.clist, outDirectory + "dump_Sedov" + std::to_string(d.iteration) + ".txt");
-            fileWriter.dumpParticleDataToBinFile(d, outDirectory + "dump_Sedov" + std::to_string(d.iteration) + ".bin");
-            timer.step("writeFile");
-        }
+    //    if ((writeFrequency > 0 && d.iteration % writeFrequency == 0) || writeFrequency == 0)
+    //    {
+    //        //fileWriter.dumpParticleDataToAsciiFile(d, domain.clist, outDirectory + "dump_Sedov" + std::to_string(d.iteration) + ".txt");
+    //        fileWriter.dumpParticleDataToBinFile(d, outDirectory + "dump_Sedov" + std::to_string(d.iteration) + ".bin");
+    //        timer.step("writeFile");
+    //    }
 
-        timer.stop();
+    //    timer.stop();
 
-        if (d.rank == 0) printer.printTotalIterationTime(timer.duration(), output);
-    }
+    //    if (d.rank == 0) printer.printTotalIterationTime(timer.duration(), output);
+    //}
 
     totalTimer.step("Total execution time of " + std::to_string(maxStep) + " iterations of Sedov");
 
