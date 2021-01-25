c86a8aef2e93d99c8631302188e962a941f1cb7d
Sebastian Keller <44800242+sebkelle1@users.noreply.github.com>
2021-01-25 10:26:09 +0100

    shared densityJLoop between CPU and GPU (#65)

--- include/sph/kernels.hpp
+++ include/sph/kernels.hpp
@@ -2,6 +2,7 @@
 
 #include "math.hpp"
 #include "cudaFunctionAnnotation.hpp"
+#include "lookupTables.hpp"
 
 namespace sphexa
 {
@@ -62,6 +63,27 @@ CUDA_DEVICE_FUN inline T artificial_viscosity(const T ro_i, const T ro_j, const 
     }
 
     return viscosity_ij;
+}
+
+template <typename T>
+CUDA_DEVICE_HOST_FUN inline
+void densityJLoop(int i, T sincIndex, T K, int ngmax, const BBox<T> *bbox, const int *neighbors, const int *neighborsCount,
+                  const T *x, const T *y, const T *z, const T *h, const T *m, const T *wh, const T *whd, size_t ltsize, T *ro)
+{
+    T roloc = 0.0;
+    const int nn = neighborsCount[i];
+
+    for (int pj = 0; pj < nn; ++pj)
+    {
+        const int j = neighbors[i * ngmax + pj];
+        const T dist = distancePBC(*bbox, h[i], x[i], y[i], z[i], x[j], y[j], z[j]);
+        const T vloc = dist / h[i];
+        const T w = K * math_namespace::pow(lt::wharmonic_lt_with_derivative(wh, whd, ltsize, vloc), (int)sincIndex);
+        const T value = w / (h[i] * h[i] * h[i]);
+        roloc += value * m[j];
+    }
+
+    ro[i] = roloc + m[i] * K / (h[i] * h[i] * h[i]);
 }
 
 } // namespace sphexa
