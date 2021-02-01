a46e8e3af0ba7c93edbf57f96bda8cab58a2ae67
Sebastian Keller <sebkelle@ethz.ch>
2021-02-01 09:04:24 +0100

    added conversion operators to MpiType

--- include/cstone/mpi_wrappers.hpp
+++ include/cstone/mpi_wrappers.hpp
@@ -39,30 +39,34 @@ struct MpiType
 };
 
 template<>
-struct MpiType<double> : std::integral_constant<MPI_Datatype, MPI_DOUBLE>
+struct MpiType<double>
 {
+    constexpr operator MPI_Datatype() const noexcept { return MPI_DOUBLE; }
 };
 
 template<>
-struct MpiType<float> : std::integral_constant<MPI_Datatype, MPI_FLOAT>
+struct MpiType<float>
 {
+    constexpr operator MPI_Datatype() const noexcept { return MPI_FLOAT; }
 };
 
 template<>
-struct MpiType<int> : std::integral_constant<MPI_Datatype, MPI_INT>
+struct MpiType<int>
 {
+    constexpr operator MPI_Datatype() const noexcept { return MPI_INT; }
 };
 
 template<>
-struct MpiType<unsigned> : std::integral_constant<MPI_Datatype, MPI_UNSIGNED>
+struct MpiType<unsigned>
 {
+    constexpr operator MPI_Datatype() const noexcept { return MPI_UNSIGNED; }
 };
 
 template<>
-struct MpiType<unsigned long> : std::integral_constant<MPI_Datatype, MPI_UNSIGNED_LONG>
+struct MpiType<unsigned long>
 {
+    constexpr operator MPI_Datatype() const noexcept { return MPI_UNSIGNED_LONG; }
 };
-
 
 template<class T>
 std::enable_if_t<std::is_same<double, std::decay_t<T>>{}>
@@ -140,4 +144,37 @@ mpiRecvSync(T* data, int count, int rank, int tag, MPI_Status* status)
 }
 
 
-
+//template<class T>
+//std::enable_if_t<std::is_same<double, std::decay_t<T>>{}>
+//mpiAllreduce(void* output, T* data, int count, MPI_Op operation, MPI_Comm communicator)
+//{
+//    MPI_Allreduce(output, data, count, MPI_DOUBLE, operation, communicator);
+//}
+//
+//template<class T>
+//std::enable_if_t<std::is_same<float, std::decay_t<T>>{}>
+//mpiAllreduce(void* output, T* data, int count, MPI_Op operation, MPI_Comm communicator)
+//{
+//    MPI_Allreduce(output, data, count, MPI_FLOAT, operation, communicator);
+//}
+//
+//template<class T>
+//std::enable_if_t<std::is_same<int, std::decay_t<T>>{}>
+//mpiAllreduce(void* output, T* data, int count, MPI_Op operation, MPI_Comm communicator)
+//{
+//    MPI_Allreduce(output, data, count, MPI_INT, operation, communicator);
+//}
+//
+//template<class T>
+//std::enable_if_t<std::is_same<unsigned, std::decay_t<T>>{}>
+//mpiAllreduce(void* output, T* data, int count, MPI_Op operation, MPI_Comm communicator)
+//{
+//    MPI_Allreduce(output, data, count, MPI_UNSIGNED, operation, communicator);
+//}
+//
+//template<class T>
+//std::enable_if_t<std::is_same<unsigned long, std::decay_t<T>>{}>
+//mpiAllreduce(void* output, T* data, int count, MPI_Op operation, MPI_Comm communicator)
+//{
+//    MPI_Allreduce(output, data, count, MPI_UNSIGNED_LONG, operation, communicator);
+//}
