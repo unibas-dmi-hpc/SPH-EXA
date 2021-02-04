3db5d2a6884afb8f255137c80b0eed8f48b0c723
Sebastian Keller <sebkelle@ethz.ch>
2021-02-04 19:10:13 +0100

    added gather performance test

---
+++ test/performance/gather_driver.cpp
@@ -0,0 +1,104 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! \file
+ * \brief  Tests a gather (reordering) operation on arrays
+ *
+ * \author Sebastian Keller <sebastian.f.keller@gmail.com>
+ */
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include <random>
+#include <vector>
+
+#include "cstone/cuda/gather.cuh"
+
+template<class I>
+std::vector<I> makeRandomPermutation(std::size_t nElements)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::vector<I> map(nElements);
+    std::iota(begin(map), end(map), 0);
+    std::shuffle(begin(map), end(map), gen);
+    return map;
+}
+
+template<class T, class I>
+void cpuGather(const std::vector<I>& map, std::vector<T>& values)
+{
+    std::vector<T> tmp(values.size());
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < map.size(); ++i)
+        tmp[i] = values[map[i]];
+
+    using std::swap;
+    swap(values, tmp);
+}
+
+void check()
+{
+    using I = unsigned;
+    using T = double;
+
+    std::vector<I> map{0,2,4,6,8,1,3,5,7,9};
+    std::vector<T> values{0,1,2,3,4,5,6,7,8,9};
+
+    DeviceGather<T, I> dg(map.data(), map.data() + map.size());
+    dg.gather(values.data());
+
+    for (auto v : values)
+        std::cout << v << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    using I = unsigned;
+    using T = double;
+
+    check();
+
+    int nElements = 32000000;
+
+    std::vector<I> map = makeRandomPermutation<I>(nElements);
+    std::vector<T> values(nElements);
+
+    auto tcpu0 = std::chrono::high_resolution_clock::now();
+    cpuGather(map, values);
+    auto tcpu1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "cpu gather Melements/s: " << T(nElements)/(1e6 * std::chrono::duration<double>(tcpu1 - tcpu0).count()) << std::endl;
+
+    DeviceGather<T, I> dg(map.data(), map.data() + map.size());
+
+    auto tgpu0 = std::chrono::high_resolution_clock::now();
+    dg.gather(values.data());
+    auto tgpu1 = std::chrono::high_resolution_clock::now();
+    std::cout << "gpu gather Melements/s: " << T(nElements)/(1e6*std::chrono::duration<double>(tgpu1 - tgpu0).count()) << std::endl;
+}
