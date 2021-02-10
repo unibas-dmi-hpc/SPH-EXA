0810d52e1d6161a9436fc520ec480ba872300398
Sebastian Keller <keller@cscs.ch>
2021-02-10 16:41:48 +0100

    fix std::vector reserve performance bug

--- include/cstone/domaindecomp_mpi.hpp
+++ include/cstone/domaindecomp_mpi.hpp
@@ -121,9 +121,20 @@ template<class... Arrays>
 void reallocate(std::size_t size, Arrays&... arrays)
 {
     std::array data{ (&arrays)... };
+
+    size_t current_capacity = data[0]->capacity();
+    if (size > current_capacity)
+    {
+        // limit reallocation growth to 5% instead of 200%
+        size_t reserve_size = double(size) * 1.05;
+        for (auto array : data)
+        {
+            array->reserve(reserve_size);
+        }
+    }
+
     for (auto array : data)
     {
-        array->reserve(size);
         array->resize(size);
     }
 }
