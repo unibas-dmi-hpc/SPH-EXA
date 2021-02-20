0c3b0d21460e8ff48b6a3f062ce1575289dc3930
Sebastian Keller <sebkelle@ethz.ch>
2021-02-20 17:59:58 +0100

    small tweaks

--- test/performance/scan.cpp
+++ test/performance/scan.cpp
@@ -91,7 +91,7 @@ void test_scan(const std::vector<T>& input, std::vector<T>& output, const std::v
 
 int main()
 {
-    std::size_t numElements = 10000000;
+    std::size_t numElements = 40000000;
     //std::size_t numElements = 100;
     std::vector<unsigned> input(numElements, 1);
     std::vector<unsigned> output(numElements);
