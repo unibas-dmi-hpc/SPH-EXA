44e23c092ea10abd86934913d5ef6df574bf00d6
Sebastian Keller <sebkelle@ethz.ch>
2021-03-16 11:47:23 +0100

    finished generic upsweep

--- test/unit/tree/upsweep.cpp
+++ test/unit/tree/upsweep.cpp
@@ -36,3 +36,107 @@
 #include "cstone/tree/octree_util.hpp"
 
 using namespace cstone;
+
+/*! \brief test upsweep sum with a regular 4x4x4 grid of leaves
+ *
+ * This creates 64 level-2 leaf nodes. The resulting internal tree should
+ * have 9 nodes, the root node and the 8 level-1 nodes.
+ */
+template<class I>
+void upsweepSum4x4x4()
+{
+    std::vector<I> cstoneTree = makeUniformNLevelTree<I>(64, 1);
+
+    Octree<I> octree;
+    octree.update(cstoneTree.data(), cstoneTree.data() + cstoneTree.size());
+
+    std::vector<unsigned> nodeCounts(octree.nTreeNodes(), 0);
+    for (int i = octree.nInternalNodes(); i < octree.nTreeNodes(); ++i)
+    {
+        nodeCounts[i] = 1;
+    }
+
+    auto sumFunction = [](auto a, auto b, auto c, auto d, auto e, auto f, auto g, auto h) { return a+b+c+d+e+f+g+h; };
+    upsweep(octree, nodeCounts.data() + octree.nInternalNodes(), nodeCounts.data(), sumFunction);
+
+    std::vector<unsigned> refNodeCounts{64, 8, 8, 8, 8, 8, 8, 8, 8,
+                                        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+    EXPECT_EQ(nodeCounts, refNodeCounts);
+}
+
+TEST(Upsweep, sum4x4x4)
+{
+    upsweepSum4x4x4<unsigned>();
+    upsweepSum4x4x4<uint64_t>();
+}
+
+/*! \brief test upsweep sum with an irregular leaf tree
+ *
+ * The leaf tree is the result of subdividing the root node, then further
+ * subdividing octant 0. This results in 15 leaves, so the internal tree
+ * should have two nodes: the root and the one internal level-1 node for the
+ * first octant. The root points to the one internal node and to leaves [8:15].
+ * The internal level-1 nodes points to leaves [0:8].
+ */
+template<class I>
+void upsweepSumIrregularL2()
+{
+    std::vector<I> cstoneTree = OctreeMaker<I>{}.divide().divide(0).makeTree();
+    Octree<I> octree;
+    octree.update(cstoneTree.data(), cstoneTree.data() + cstoneTree.size());
+
+    std::vector<unsigned> nodeCounts(octree.nTreeNodes(), 0);
+    for (int i = 0; i < octree.nLeafNodes(); ++i)
+    {
+        nodeCounts[i + octree.nInternalNodes()] = i;
+    }
+
+    auto sumFunction = [](auto a, auto b, auto c, auto d, auto e, auto f, auto g, auto h) { return a+b+c+d+e+f+g+h; };
+    upsweep(octree, nodeCounts.data() + octree.nInternalNodes(), nodeCounts.data(), sumFunction);
+
+    std::vector<unsigned> refNodeCounts{105,28,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+    EXPECT_EQ(nodeCounts, refNodeCounts);
+}
+
+TEST(Upsweep, sumIrregularL2)
+{
+    upsweepSumIrregularL2<unsigned>();
+    upsweepSumIrregularL2<uint64_t>();
+}
+
+/*! \brief an irregular tree with a max depth of 3
+ *
+ *     - total nodes: 33
+ *     - leaf nodes: 29
+ *     - internal nodes: 4
+ */
+template<class I>
+void upsweepSumIrregularL3()
+{
+    std::vector<I> cstoneTree = OctreeMaker<I>{}.divide().divide(0).divide(0, 2).divide(3).makeTree();
+    Octree<I> octree;
+    octree.update(cstoneTree.data(), cstoneTree.data() + cstoneTree.size());
+
+    std::vector<unsigned> nodeCounts(octree.nTreeNodes(), 0);
+    for (int i = octree.nInternalNodes(); i < octree.nTreeNodes(); ++i)
+    {
+        nodeCounts[i] = 1;
+    }
+
+    auto sumFunction = [](auto a, auto b, auto c, auto d, auto e, auto f, auto g, auto h) { return a+b+c+d+e+f+g+h; };
+    upsweep(octree, nodeCounts.data() + octree.nInternalNodes(), nodeCounts.data(), sumFunction);
+
+    std::vector<unsigned> refNodeCounts{29,15,8,8,
+                                        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+    EXPECT_EQ(nodeCounts, refNodeCounts);
+}
+
+TEST(Upsweep, sumIrregularL3)
+{
+    upsweepSumIrregularL3<unsigned>();
+    upsweepSumIrregularL3<uint64_t>();
+}
