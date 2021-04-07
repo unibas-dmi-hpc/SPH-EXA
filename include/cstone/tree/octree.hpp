295b054b832d7b335da90c1773d47d2b53055dea
Sebastian Keller <sebkelle@ethz.ch>
2021-04-07 10:57:17 +0200

    handle out-of-bounds guess in findSearchBounds

--- include/cstone/tree/octree.hpp
+++ include/cstone/tree/octree.hpp
@@ -100,7 +100,7 @@ unsigned calculateNodeCount(const I* tree, TreeNodeIndex nodeIdx, const I* codes
 /*! @brief determine search bound for @p targetCode in an array of sorted particle SFC codes
  *
  * @tparam I          32- or 64-bit unsigned integer type
- * @param firstIdx    first (of two) search boundary, permissible range: [0:codesEnd-codesStart-1]
+ * @param firstIdx    first (of two) search boundary, must be non-negative, but can exceed the codes range
  *                    (the guess for the location of @p targetCode in [codesStart:codesEnd]
  * @param targetCode  code in [codesStart:codesEnd] to look for
  * @param codesStart  particle SFC code array start
@@ -114,6 +114,9 @@ pair<const I*> findSearchBounds(std::make_signed_t<I> firstIdx, I targetCode,
 {
     using SI = std::make_signed_t<I>;
     SI nCodes = codesEnd - codesStart;
+
+    // firstIdx must be an accessible index
+    firstIdx = stl::min(nCodes-1, firstIdx);
 
     I firstCode = codesStart[firstIdx];
     if (firstCode == targetCode)
