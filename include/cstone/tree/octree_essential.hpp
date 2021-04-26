6405cc5d12fe39ca8a625e20e49150edbd5059fe
Sebastian Keller <sebkelle@ethz.ch>
2021-04-26 20:13:15 +0200

    moved findHalos to halo directory

---
+++ include/cstone/tree/octree_essential.hpp
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! @file
+ * @brief Generation of locally essential global octrees in cornerstone format
+ *
+ * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ *
+ * A locally essential octree has a certain global resolution specified by a maximum
+ * particle count per leaf node. In addition, it features a focus area defined as a
+ * sub-range of the global space filling curve. In this focus sub-range, the resolution
+ * can be higher, expressed through a smaller maximum particle count per leaf node.
+ * Crucially, the resolution is also higher in the halo-areas of the focus sub-range.
+ * These halo-areas can be defined as the overlap with the smoothing-length spheres around
+ * the contained particles in the focus sub-range (SPH) or as the nodes whose opening angle
+ * is too big to satisfy a multipole acceptance criterion from any perspective within the
+ * focus subrange (N-body).
+ */
+
+#pragma once
+
+
+
+namespace cstone
+{
+
+} // namespace cstone
