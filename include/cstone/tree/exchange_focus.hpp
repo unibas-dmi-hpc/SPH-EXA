d56e5de9edab476c4788bae43b9d1ce4d36d04de
Sebastian Keller <sebastian.f.keller@gmail.com>
2021-06-11 17:34:40 +0200

    reuse countRequestParticles for the global counts and added documentation

--- include/cstone/tree/exchange_focus.hpp
+++ include/cstone/tree/exchange_focus.hpp
@@ -28,8 +28,8 @@
  *
  * @author Sebastian Keller <sebastian.f.keller@gmail.com>
  *
- * Overall procedure:
- *      1.  rank1 sends node structure (vector of SFC keys) to rank2. The node structure sent by rank1
+ * Overall procedure for each pair for peer ranks (rank1, rank2):
+ *      1.  rank1 sends a node structure (vector of SFC keys) to rank2. The node structure sent by rank1
  *          covers the assigned domain of rank2. The node structure cannot exceed the resolution of
  *          the local tree of rank2, this is guaranteed by the tree-build process, as long as all ranks
  *          use the same bucket size for the locally focused tree. Usually, rank1 requests the full resolution
@@ -37,6 +37,8 @@
  *
  *      2.  rank2 receives the the node structure, counts particles for each received node and sends back
  *          an answer with the particle counts per node.
+ *
+ *      3. rank1 receives the counts for the requested SFC keys from rank2
  */
 
 #pragma once
@@ -50,9 +52,17 @@
 namespace cstone
 {
 
+/*! @brief count particles inside specified ranges of a cornerstone leaf tree
+ *
+ * @tparam KeyType             32- or 64-bit unsigned integer
+ * @param[in]  leaves          cornerstone SFC key sequence,
+ * @param[in]  counts          particle counts of @p leaves, length = length(leaves) - 1
+ * @param[in]  requestLeaves   query cornerstone SFC key sequence
+ * @param[out] requestCounts   output counts for @p requestLeaves, length = length(requestLeaves) - 1
+ */
 template<class KeyType>
 void countRequestParticles(gsl::span<const KeyType> leaves, gsl::span<const unsigned> counts,
-                         gsl::span<const KeyType> requestLeaves, gsl::span<unsigned> requestCounts)
+                           gsl::span<const KeyType> requestLeaves, gsl::span<unsigned> requestCounts)
 {
     #pragma omp parallel for
     for (TreeNodeIndex i = 0; i < requestCounts.size(); ++i)
@@ -67,9 +77,27 @@ void countRequestParticles(gsl::span<const KeyType> leaves, gsl::span<const unsi
     }
 }
 
+/*! @brief exchange particle counts with specified peer ranks
+ *
+ * @tparam KeyType                  32- or 64-bit unsigned integer
+ * @param[in]  peerRanks            list of peer rank IDs
+ * @param[in]  exchangeIndices      contains one range of indices of @p localLeaves to request counts
+ *                                  for from each peer rank, length = same as @p peerRanks
+ * @param[in]  localLeaves          cornerstone SFC key sequence of the locally (focused) tree
+ *                                  of the executing rank
+ * @param[out] localCounts          particle counts associated with @p localLeaves
+ *                                  length(localCounts) = length(localLeaves) - 1
+ * @param[-]   queryLeafBuffer      temp buffer for MPI p2p, length = same as @p localLeaves
+ * @param[-]   queryCountBuffer     temp buffer for MPI p2p, length = same as @p localCounts
+ *
+ * Procedure on each rank:
+ *  1. Send out the SFC keys for which it wants to get particle counts to peer ranks
+ *  2. receive SFC keys from other peer ranks, count particles and send back the counts as answer
+ *  3. receive answer with the counts for the requested keys
+ */
 template<class KeyType>
 void exchangePeerCounts(gsl::span<const int> peerRanks, gsl::span<const pair<TreeNodeIndex>> exchangeIndices,
-                        gsl::span<const KeyType> focusLeaves, gsl::span<unsigned> focusCounts,
+                        gsl::span<const KeyType> localLeaves, gsl::span<unsigned> localCounts,
                         gsl::span<KeyType> queryLeafBuffer, gsl::span<unsigned> queryCountBuffer)
 
 {
@@ -79,7 +107,7 @@ void exchangePeerCounts(gsl::span<const int> peerRanks, gsl::span<const pair<Tre
         int destinationRank = peerRanks[rankIndex];
         // +1 to include the upper key boundary for the last node
         TreeNodeIndex sendCount = exchangeIndices[rankIndex][1] - exchangeIndices[rankIndex][0] + 1;
-        mpiSendAsync(focusLeaves.data() + exchangeIndices[rankIndex][0], sendCount, destinationRank, 0, sendRequests);
+        mpiSendAsync(localLeaves.data() + exchangeIndices[rankIndex][0], sendCount, destinationRank, 0, sendRequests);
     }
 
     size_t numMessages = peerRanks.size();
@@ -95,7 +123,7 @@ void exchangePeerCounts(gsl::span<const int> peerRanks, gsl::span<const pair<Tre
         // compute particle counts for the received node structure.
         // The number of nodes to count is one less the number of received SFC keys
         TreeNodeIndex numNodes = numKeys - 1;
-        countRequestParticles<KeyType>(focusLeaves, focusCounts, queryLeafBuffer.first(numKeys), queryCountBuffer.first(numNodes));
+        countRequestParticles<KeyType>(localLeaves, localCounts, queryLeafBuffer.first(numKeys), queryCountBuffer.first(numNodes));
 
         // send back answer with the counts for the requested nodes
         //mpiSendAsync(queryCountBuffer.data(), numNodes, receiveRank, 1, sendRequests);
@@ -115,8 +143,8 @@ void exchangePeerCounts(gsl::span<const int> peerRanks, gsl::span<const pair<Tre
 
         TreeNodeIndex receiveCount = exchangeIndices[receiveRankIndex][1] - exchangeIndices[receiveRankIndex][0];
         std::transform(queryCountBuffer.begin(), queryCountBuffer.begin() + receiveCount,
-                       focusCounts.begin() + exchangeIndices[receiveRankIndex][0],
-                       focusCounts.begin() + exchangeIndices[receiveRankIndex][0], std::plus<unsigned>{});
+                       localCounts.begin() + exchangeIndices[receiveRankIndex][0],
+                       localCounts.begin() + exchangeIndices[receiveRankIndex][0], std::plus<unsigned>{});
 
         numMessages--;
     }
