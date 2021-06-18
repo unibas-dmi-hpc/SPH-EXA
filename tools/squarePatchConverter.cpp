36c4e7d8118ee231c9f62ca8b3ddf1f0fb5387c0
Sebastian Keller <sebastian.f.keller@gmail.com>
2021-06-18 18:27:38 +0200

    clang-format on main repo

--- tools/squarePatchConverter.cpp
+++ tools/squarePatchConverter.cpp
@@ -1,32 +1,33 @@
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
-#include <fstream> 
+#include <fstream>
 #include <iostream>
 #include <numeric>
 int main()
 {
     const int n = 1000000;
 
-
     std::vector<double> x(n), y(n), z(n), vx(n), vy(n), vz(n), p_0(n), h(n), volume(n), m(n);
-    
+
     printf("Opening the file\n");
-    FILE *f = fopen("squarepatch3D_1M", "r");
-    
-    if(f)
+    FILE* f = fopen("squarepatch3D_1M", "r");
+
+    if (f)
     {
-        for(int i=0; i<n; i++)
-        {   
-            fscanf(f, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", &x[i], &y[i], &z[i], &vx[i], &vy[i], &vz[i], &p_0[i], &h[i], &volume[i], &m[i]);
-            // printf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", x[i], y[i], z[i], vx[i], vy[i], vz[i], p_0[i], h[i], volume[i], m[i]);
+        for (int i = 0; i < n; i++)
+        {
+            fscanf(f, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", &x[i], &y[i], &z[i], &vx[i], &vy[i], &vz[i], &p_0[i],
+                   &h[i], &volume[i], &m[i]);
+            // printf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", x[i], y[i], z[i], vx[i], vy[i], vz[i], p_0[i], h[i],
+            // volume[i], m[i]);
         }
 
         fclose(f);
 
         std::ofstream ofs("squarepatch3D_1M.bin", std::ofstream::out | std::ofstream::binary);
-        
-        if(ofs)
+
+        if (ofs)
         {
             ofs.write(reinterpret_cast<const char*>(x.data()), x.size() * sizeof(double));
             ofs.write(reinterpret_cast<const char*>(y.data()), y.size() * sizeof(double));
