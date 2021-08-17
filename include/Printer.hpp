9971b89a6807a146ea7fe4a00d607d877e486647
Sebastian Keller <sebastian.f.keller@gmail.com>
2021-08-17 15:28:33 +0200

    added memory stats to printer

--- include/Printer.hpp
+++ include/Printer.hpp
@@ -5,24 +5,18 @@
 namespace sphexa
 {
 
-template <typename Dataset>
+template<typename Dataset>
 class Printer
 {
 public:
-    Printer(const Dataset &d)
+    Printer(const Dataset& d)
         : d(d)
     {
     }
-    /*
-        void printRadiusAndGravityForce(const std::vector<int> &clist, std::ostream &out)
-        {
-            out << std::sqrt(d.x[i] * d.x[i] + d.y[i] * d.y[i] + d.z[i] * d.z[i]) << ' ' << d.x[i] << ' ' << d.y[i] << ' ' << d.z[i] << ' '
-                << d.fx[i] << ' ' << d.fy[i] << ' ' << d.fz[i] << std::endl;
-        }
-    */
-    void printTree(GravityOctree<double> &octree, [[maybe_unused]] std::ostream &out) { octree.print(); }
 
-    void printConstants(size_t iteration, size_t nntot, std::ostream &out) const
+    void printTree(GravityOctree<double>& octree, [[maybe_unused]] std::ostream& out) { octree.print(); }
+
+    void printConstants(size_t iteration, size_t nntot, std::ostream& out) const
     {
         if (d.rank == 0)
         {
@@ -38,31 +32,46 @@ public:
         }
     }
 
-    void printCheck(const size_t particleCount, const size_t nodeCount, const size_t haloCount, const size_t totalNeighbors,
-                    std::ostream &out) const
+    void printCheck(const size_t particleCount, const size_t nodeCount, const size_t haloCount,
+                    const size_t totalNeighbors, std::ostream& out) const
     {
-        out << "### Check ### Global Tree Nodes: " << nodeCount << ", Particles: " << particleCount << ", Halos: " << haloCount
-            << std::endl;
-        out << "### Check ### Computational domain: " << d.bbox.xmin << " " << d.bbox.xmax << " " << d.bbox.ymin << " " << d.bbox.ymax
-            << " " << d.bbox.zmin << " " << d.bbox.zmax << std::endl;
-        out << "### Check ### Total Neighbors: " << totalNeighbors << ", Avg neighbor count per particle: " << totalNeighbors / d.n
-            << std::endl;
+        out << "### Check ### Global Tree Nodes: " << nodeCount << ", Particles: " << particleCount
+            << ", Halos: " << haloCount << std::endl;
+        out << "### Check ### Computational domain: " << d.bbox.xmin << " " << d.bbox.xmax << " " << d.bbox.ymin << " "
+            << d.bbox.ymax << " " << d.bbox.zmin << " " << d.bbox.zmax << std::endl;
+        out << "### Check ### Total Neighbors: " << totalNeighbors
+            << ", Avg neighbor count per particle: " << totalNeighbors / d.n << std::endl;
         out << "### Check ### Total time: " << d.ttot << ", current time-step: " << d.minDt << std::endl;
         out << "### Check ### Total energy: " << d.etot << ", (internal: " << d.eint << ", cinetic: " << d.ecin;
 #ifdef GRAVITY
         const std::streamsize defaultPrecision = std::cout.precision();
         const std::streamsize gravityPrecision = 15;
-        out << ", gravitational: " << std::setprecision(gravityPrecision) << d.egrav << std::setprecision(defaultPrecision);
+        out << ", gravitational: " << std::setprecision(gravityPrecision) << d.egrav
+            << std::setprecision(defaultPrecision);
 #endif
         out << ")" << std::endl;
     }
 
-    void printTotalIterationTime(const float duration, std::ostream &out) const
+    void printTotalIterationTime(const float duration, std::ostream& out) const
     {
         out << "=== Total time for iteration(" << d.iteration << ") " << duration << "s" << std::endl << std::endl;
     }
 
+    void printMemory(size_t halos, std::ostream& out) const
+    {
+        size_t mem = d.x.capacity();
+        MPI_Allreduce(MPI_IN_PLACE, &mem, 1, MPI_UNSIGNED_LONG, MPI_MAX, MPI_COMM_WORLD);
+
+        MPI_Allreduce(MPI_IN_PLACE, &halos, 1, MPI_UNSIGNED_LONG, MPI_MAX, MPI_COMM_WORLD);
+
+        if (d.rank == 0)
+        {
+            out << "### Check ### Array sizes: " << mem << " Max halos: " << halos << std::endl;
+        }
+    }
+
 private:
-    const Dataset &d;
+    const Dataset& d;
 };
+
 } // namespace sphexa
