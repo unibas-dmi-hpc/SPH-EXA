e093b9da87c77fee43a3ca0c3277342d6c6a2914
Sebastian Keller <sebastian.f.keller@gmail.com>
2021-10-27 15:27:13 +0200

    removed hi^3 from j-loop in IAD

--- include/sph/kernel/computeIAD.hpp
+++ include/sph/kernel/computeIAD.hpp
@@ -21,40 +21,45 @@ void IADJLoop(int pi, T sincIndex, T K, int ngmax, const BBox<T>& bbox, const in
 
     T tau11 = 0.0, tau12 = 0.0, tau13 = 0.0, tau22 = 0.0, tau23 = 0.0, tau33 = 0.0;
 
+    T hi    = h[i];
+    T hiInv = 1.0 / hi;
+
     for (int pj = 0; pj < nn; ++pj)
     {
         const int j = neighbors[pi * ngmax + pj];
-
-        // later can be stored into an array per particle
-        const T dist = distancePBC(bbox, h[i], x[i], y[i], z[i], x[j], y[j], z[j]); // store the distance from each neighbor
-        // calculate the v as ratio between the distance and the smoothing length
-        const T vloc = dist / h[i];
-        const T w = K * ::sphexa::math::pow(lt::wharmonic_lt_with_derivative(wh, whd, vloc), (int)sincIndex);
-        const T W = w / (h[i] * h[i] * h[i]);
 
         T r_ijx = (x[i] - x[j]);
         T r_ijy = (y[i] - y[j]);
         T r_ijz = (z[i] - z[j]);
 
-        applyPBC(bbox, 2.0 * h[i], r_ijx, r_ijy, r_ijz);
+        applyPBC(bbox, 2.0 * hi, r_ijx, r_ijy, r_ijz);
 
-        tau11 += r_ijx * r_ijx * m[j] / ro[j] * W;
-        tau12 += r_ijx * r_ijy * m[j] / ro[j] * W;
-        tau13 += r_ijx * r_ijz * m[j] / ro[j] * W;
-        tau22 += r_ijy * r_ijy * m[j] / ro[j] * W;
-        tau23 += r_ijy * r_ijz * m[j] / ro[j] * W;
-        tau33 += r_ijz * r_ijz * m[j] / ro[j] * W;
+        T dist = std::sqrt(r_ijx * r_ijx + r_ijy * r_ijy + r_ijz * r_ijz);
+
+        // calculate the v as ratio between the distance and the smoothing length
+        T vloc = dist * hiInv;
+        T w = K * ::sphexa::math::pow(lt::wharmonic_lt_with_derivative(wh, whd, vloc), (int)sincIndex);
+
+        tau11 += r_ijx * r_ijx * m[j] / ro[j] * w;
+        tau12 += r_ijx * r_ijy * m[j] / ro[j] * w;
+        tau13 += r_ijx * r_ijz * m[j] / ro[j] * w;
+        tau22 += r_ijy * r_ijy * m[j] / ro[j] * w;
+        tau23 += r_ijy * r_ijz * m[j] / ro[j] * w;
+        tau33 += r_ijz * r_ijz * m[j] / ro[j] * w;
     }
 
-    const T det =
-        tau11 * tau22 * tau33 + 2.0 * tau12 * tau23 * tau13 - tau11 * tau23 * tau23 - tau22 * tau13 * tau13 - tau33 * tau12 * tau12;
+    T det = tau11 * tau22 * tau33 + 2.0 * tau12 * tau23 * tau13 - tau11 * tau23 * tau23 - tau22 * tau13 * tau13 -
+            tau33 * tau12 * tau12;
 
-    c11[i] = (tau22 * tau33 - tau23 * tau23) / det;
-    c12[i] = (tau13 * tau23 - tau33 * tau12) / det;
-    c13[i] = (tau12 * tau23 - tau22 * tau13) / det;
-    c22[i] = (tau11 * tau33 - tau13 * tau13) / det;
-    c23[i] = (tau13 * tau12 - tau11 * tau23) / det;
-    c33[i] = (tau11 * tau22 - tau12 * tau12) / det;
+    T hi3 = hi * hi * hi;
+    T factor = hi3 / det;
+
+    c11[i] = (tau22 * tau33 - tau23 * tau23) * factor;
+    c12[i] = (tau13 * tau23 - tau33 * tau12) * factor;
+    c13[i] = (tau12 * tau23 - tau22 * tau13) * factor;
+    c22[i] = (tau11 * tau33 - tau13 * tau13) * factor;
+    c23[i] = (tau13 * tau12 - tau11 * tau23) * factor;
+    c33[i] = (tau11 * tau22 - tau12 * tau12) * factor;
 }
 
 } // namespace kernels
