7d475cd62298b58266ce43b405d810312373cc81
Sebastian Keller <keller@cscs.ch>
2021-10-29 14:02:12 +0200

    removed clist from cuda density kernel

--- include/sph/cuda/cudaDensity.cu
+++ include/sph/cuda/cudaDensity.cu
@@ -14,25 +14,30 @@ namespace sph
 namespace cuda
 {
 
-template<class T/*, class KeyType*/>
-__global__ void density(int n, T sincIndex, T K, int ngmax, cstone::Box<T> box, const int* clist,
-                        const int* neighbors, const int* neighborsCount,
-                        //const KeyType* particleKeys, int numKeys,
+template<class T, class KeyType>
+__global__ void density(T sincIndex, T K, int ngmax, cstone::Box<T> box,
+                        int firstParticle, int lastParticle, int numParticles, const KeyType* particleKeys,
+                        int* neighborsCount,
                         const T* x, const T* y, const T* z, const T* h, const T* m, const T* wh, const T* whd, T* ro)
 {
     unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid >= n) return;
+    unsigned i = tid + firstParticle;
+    if (i >= lastParticle) return;
 
-    //int nLoc[150];
-    //int nCount;
+    // need to hard-code ngmax stack allocation for now
+    assert(ngmax <= NGMAX && "ngmax too big, please increase NGMAX to desired value");
+    int neighbors[NGMAX];
+    int neighborsCount_;
 
-    int i = clist[tid];
+    // starting from CUDA 11.3, dynamic stack allocation is available with the following command
+    // int* neighbors = (int*)alloca(ngmax * sizeof(int));
 
-    //cstone::findNeighbors(i, x, y ,z, h, box, cstone::sfcKindPointer(particleKeys), nLoc, &nCount, numKeys, ngmax);
+    cstone::findNeighbors(
+        i, x, y, z, h, box, cstone::sfcKindPointer(particleKeys), neighbors, &neighborsCount_, numParticles, ngmax);
 
-    //ro[i] = sph::kernels::densityJLoop(i, sincIndex, K, box, nLoc, nCount, x, y, z, h, m, wh, whd);
-    ro[i] = sph::kernels::densityJLoop(
-        i, sincIndex, K, box, neighbors + ngmax * tid, neighborsCount[tid], x, y, z, h, m, wh, whd);
+    ro[i] = sph::kernels::densityJLoop(i, sincIndex, K, box, neighbors, neighborsCount_, x, y, z, h, m, wh, whd);
+
+    neighborsCount[tid] = neighborsCount_;
 }
 
 template<class Dataset>
@@ -40,9 +45,10 @@ void computeDensity(std::vector<Task>& taskList, Dataset& d, const cstone::Box<d
 {
     using T = typename Dataset::RealType;
 
-    size_t np = d.x.size();
-    size_t size_np_T = np * sizeof(T);
-    size_t size_np_CodeType = np * sizeof(typename Dataset::KeyType);
+    size_t numParticles = d.x.size();
+
+    size_t size_np_T = numParticles * sizeof(T);
+    size_t size_np_CodeType = numParticles * sizeof(typename Dataset::KeyType);
     T ngmax = taskList.empty() ? 0 : taskList.front().ngmax;
 
     auto largestChunkSize =
@@ -57,14 +63,14 @@ void computeDensity(std::vector<Task>& taskList, Dataset& d, const cstone::Box<d
 
     size_t ltsize = d.wh.size();
     size_t size_lt_T = ltsize * sizeof(T);
+    CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_wh, d.wh.data(), size_lt_T, cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_whd, d.whd.data(), size_lt_T, cudaMemcpyHostToDevice));
 
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_x, d.x.data(), size_np_T, cudaMemcpyHostToDevice));
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_y, d.y.data(), size_np_T, cudaMemcpyHostToDevice));
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_z, d.z.data(), size_np_T, cudaMemcpyHostToDevice));
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_h, d.h.data(), size_np_T, cudaMemcpyHostToDevice));
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_m, d.m.data(), size_np_T, cudaMemcpyHostToDevice));
-    CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_wh, d.wh.data(), size_lt_T, cudaMemcpyHostToDevice));
-    CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_whd, d.whd.data(), size_lt_T, cudaMemcpyHostToDevice));
 
     CHECK_CUDA_ERR(cudaMemcpy(d.devPtrs.d_codes, d.codes.data(), size_np_CodeType, cudaMemcpyHostToDevice));
 
@@ -75,32 +81,24 @@ void computeDensity(std::vector<Task>& taskList, Dataset& d, const cstone::Box<d
         int sIdx = i % NST;
         cudaStream_t stream = d.devPtrs.d_stream[sIdx].stream;
 
-        int* d_clist_use = d.devPtrs.d_stream[sIdx].d_clist;
-        int* d_neighbors_use = d.devPtrs.d_stream[sIdx].d_neighbors;
         int* d_neighborsCount_use = d.devPtrs.d_stream[sIdx].d_neighborsCount;
 
-        size_t n = t.clist.size();
-        size_t size_n_int = n * sizeof(int);
-
-        CHECK_CUDA_ERR(cudaMemcpyAsync(d_clist_use, t.clist.data(), size_n_int, cudaMemcpyHostToDevice, stream));
-
-        findNeighborsHilbertGpu(d.devPtrs.d_x, d.devPtrs.d_y, d.devPtrs.d_z, d.devPtrs.d_h,
-                                t.clist[0], t.clist[n - 1] + 1, np, box, d.devPtrs.d_codes,
-                                d_neighbors_use, d_neighborsCount_use, ngmax, stream);
-        CHECK_CUDA_ERR(cudaGetLastError());
+        unsigned firstParticle = t.clist.front();
+        unsigned lastParticle  = t.clist.back() + 1;
+        unsigned numParticlesCompute = lastParticle - firstParticle;
 
         unsigned numThreads = 256;
-        unsigned numBlocks  = (n + numThreads - 1) / numThreads;
+        unsigned numBlocks  = (numParticlesCompute + numThreads - 1) / numThreads;
 
         density<<<numBlocks, numThreads, 0, stream>>>(
-            n, d.sincIndex, d.K, t.ngmax, box, d_clist_use, d_neighbors_use, d_neighborsCount_use,
-            //d.devPtrs.d_codes, np,
+            d.sincIndex, d.K, t.ngmax, box,
+            firstParticle, lastParticle, numParticles, d.devPtrs.d_codes, d_neighborsCount_use,
             d.devPtrs.d_x, d.devPtrs.d_y, d.devPtrs.d_z, d.devPtrs.d_h, d.devPtrs.d_m, d.devPtrs.d_wh, d.devPtrs.d_whd,
             d.devPtrs.d_ro);
         CHECK_CUDA_ERR(cudaGetLastError());
 
         CHECK_CUDA_ERR(cudaMemcpyAsync(t.neighborsCount.data(), d_neighborsCount_use,
-                                       size_n_int, cudaMemcpyDeviceToHost, stream));
+                                       numParticlesCompute * sizeof(int), cudaMemcpyDeviceToHost, stream));
     }
 
     // Memcpy in default stream synchronizes all other streams
