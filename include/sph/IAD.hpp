87092b67b940b4cc781cb8b0b39cfdc6405e7383
Sebastian Keller <sebastian.f.keller@gmail.com>
2021-10-29 17:43:23 +0200

    remove all clist accesses

--- include/sph/IAD.hpp
+++ include/sph/IAD.hpp
@@ -18,9 +18,8 @@ namespace sph
 template <typename T, class Dataset>
 void computeIADImpl(const Task& t, Dataset& d, const cstone::Box<T>& box)
 {
-    size_t n = t.clist.size();
+    int numParticles = t.clist.size();
     size_t ngmax = t.ngmax;
-    const int* clist = t.clist.data();
     const int* neighbors = t.neighbors.data();
     const int* neighborsCount = t.neighborsCount.data();
 
@@ -73,9 +72,9 @@ void computeIADImpl(const Task& t, Dataset& d, const cstone::Box<T>& box)
 #else
 #pragma omp parallel for schedule(guided)
 #endif
-    for (size_t pi = 0; pi < n; ++pi)
+    for (size_t pi = 0; pi < numParticles; ++pi)
     {
-        int i = clist[pi];
+        int i = pi + t.clist.front();
         kernels::IADJLoop(i, sincIndex, K, box, neighbors + ngmax * pi, neighborsCount[pi],
                           x, y, z, h, m, ro, wh, whd, c11, c12, c13, c22, c23, c33);
     }
