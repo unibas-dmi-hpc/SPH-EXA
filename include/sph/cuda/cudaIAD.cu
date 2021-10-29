a7d207949ecacdb387e0e0e6cefd25a4530a77d8
Sebastian Keller <keller@cscs.ch>
2021-10-29 11:27:59 +0200

    fused neighbor-IAD kernel

--- include/sph/cuda/cudaIAD.cu
+++ include/sph/cuda/cudaIAD.cu
@@ -14,18 +14,35 @@ namespace sph
 namespace cuda
 {
 
-template<class T>
+template<class T, class KeyType>
 __global__ void computeIAD(int n, T sincIndex, T K, int ngmax, cstone::Box<T> box, const int* clist,
-                           const int* neighbors, const int* neighborsCount,
+                           //const int* neighbors, const int* neighborsCount,
+                           const KeyType* particleKeys, int numKeys,
                            const T* x, const T* y, const T* z, const T* h, const T* m, const T* ro,
                            const T* wh, const T* whd, T* c11, T* c12, T* c13, T* c22, T* c23, T* c33)
 {
     const unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
     if (tid >= n) return;
 
+    // need to hard-code ngmax stack allocation for now
+    assert(ngmax <= NGMAX && "ngmax too big, please adjust allocation size here to desired value of ngmax");
+    int neighbors[NGMAX];
+
+    // starting from CUDA 11.3, dynamic stack allocation is available with the following command
+    // int* neighbors = (int*)alloca(ngmax * sizeof(int));
+
+    int neighborsCount;
+
     int i = clist[tid];
-    sph::kernels::IADJLoop(i, sincIndex, K, box, neighbors + ngmax * tid, neighborsCount[tid],
-                           x, y, z, h, m, ro, wh, whd, c11, c12, c13, c22, c23, c33);
+
+    cstone::findNeighbors(
+        i, x, y, z, h, box, cstone::sfcKindPointer(particleKeys), neighbors, &neighborsCount, numKeys, ngmax);
+
+    sph::kernels::IADJLoop(
+        i, sincIndex, K, box, neighbors, neighborsCount, x, y, z, h, m, ro, wh, whd, c11, c12, c13, c22, c23, c33);
+
+    // sph::kernels::IADJLoop(i, sincIndex, K, box, neighbors + ngmax * tid, neighborsCount[tid],
+    //                        x, y, z, h, m, ro, wh, whd, c11, c12, c13, c22, c23, c33);
 }
 
 template <class Dataset>
@@ -35,8 +52,6 @@ void computeIAD(const std::vector<Task>& taskList, Dataset& d, const cstone::Box
     size_t np = d.x.size();
     size_t size_np_T = np * sizeof(T);
     T ngmax = taskList.empty() ? 0 : taskList.front().ngmax;
-
-    size_t ltsize = d.wh.size();
 
     auto largestChunkSize =
         std::max_element(taskList.cbegin(), taskList.cend(),
@@ -58,24 +73,26 @@ void computeIAD(const std::vector<Task>& taskList, Dataset& d, const cstone::Box
         cudaStream_t stream = d.devPtrs.d_stream[sIdx].stream;
 
         int *d_clist_use = d.devPtrs.d_stream[sIdx].d_clist;
-        int *d_neighbors_use = d.devPtrs.d_stream[sIdx].d_neighbors;
-        int *d_neighborsCount_use = d.devPtrs.d_stream[sIdx].d_neighborsCount;
+        //int *d_neighbors_use = d.devPtrs.d_stream[sIdx].d_neighbors;
+        //int *d_neighborsCount_use = d.devPtrs.d_stream[sIdx].d_neighborsCount;
 
         size_t n = t.clist.size();
         size_t size_n_int = n * sizeof(int);
 
         CHECK_CUDA_ERR(cudaMemcpyAsync(d_clist_use, t.clist.data(), size_n_int, cudaMemcpyHostToDevice, stream));
 
-        findNeighborsHilbertGpu(d.devPtrs.d_x, d.devPtrs.d_y, d.devPtrs.d_z, d.devPtrs.d_h, t.clist[0],
-                                t.clist[n - 1] + 1, np, box, d.devPtrs.d_codes, d_neighbors_use,
-                                d_neighborsCount_use, ngmax, stream);
-        CHECK_CUDA_ERR(cudaGetLastError());
+        //findNeighborsHilbertGpu(d.devPtrs.d_x, d.devPtrs.d_y, d.devPtrs.d_z, d.devPtrs.d_h, t.clist[0],
+        //                        t.clist[n - 1] + 1, np, box, d.devPtrs.d_codes, d_neighbors_use,
+        //                        d_neighborsCount_use, ngmax, stream);
+        //CHECK_CUDA_ERR(cudaGetLastError());
 
         unsigned numThreads = 256;
         unsigned numBlocks  = (n + numThreads - 1) / numThreads;
 
         computeIAD<<<numBlocks, numThreads, 0, stream>>>(
-            n, d.sincIndex, d.K, ngmax, box, d_clist_use, d_neighbors_use, d_neighborsCount_use,
+            n, d.sincIndex, d.K, ngmax, box, d_clist_use,
+            //d_neighbors_use, d_neighborsCount_use,
+            d.devPtrs.d_codes, np,
             d.devPtrs.d_x, d.devPtrs.d_y, d.devPtrs.d_z, d.devPtrs.d_h, d.devPtrs.d_m, d.devPtrs.d_ro,
             d.devPtrs.d_wh, d.devPtrs.d_whd,
             d.devPtrs.d_c11, d.devPtrs.d_c12, d.devPtrs.d_c13, d.devPtrs.d_c22, d.devPtrs.d_c23, d.devPtrs.d_c33);
