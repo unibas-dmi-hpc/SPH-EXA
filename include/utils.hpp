bf4f0bbf415592c55dccbbe69612bad95da1e7ab
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-03-04 14:30:23 +0100

    fixed mpi init

--- include/utils.hpp
+++ include/utils.hpp
@@ -14,11 +14,11 @@ auto initMpi()
 #ifdef USE_MPI
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
     if (rank == 0)
     {
         int mpi_version, mpi_subversion;
         MPI_Get_version(&mpi_version, &mpi_subversion);
-        MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
 #ifdef _OPENMP
         printf("# %d MPI-%d.%d process(es) with %d OpenMP-%u thread(s)/process\n",
                numRanks,
