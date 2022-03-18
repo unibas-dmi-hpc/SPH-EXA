46be1c18f5acf70af581f8e20f471f3ba0c54fc4
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-03-18 13:39:43 +0100

    made cstone cmake-framework independent again

---
+++ domain/cmake/cstone_add_performance_test.cmake
@@ -0,0 +1,17 @@
+include(cstone_add_test)
+
+function(cstone_add_performance_test sourcename testname ranks)
+    add_executable(${testname} ${sourcename})
+    target_include_directories(${testname} PRIVATE ${PROJECT_SOURCE_DIR}/include)
+    target_include_directories(${testname} PRIVATE ${PROJECT_SOURCE_DIR}/test)
+    target_link_libraries(${testname} PRIVATE OpenMP::OpenMP_CXX)
+    install(TARGETS ${testname} RUNTIME DESTINATION ${CMAKE_INSTALL_SBINDIR}/performance)
+endfunction()
+
+function(cstone_add_cuda_performance_test sourcename objectname testname ranks)
+    add_executable(${testname} ${objectname} ${sourcename})
+    target_include_directories(${testname} PRIVATE ${PROJECT_SOURCE_DIR}/include)
+    target_include_directories(${testname} PRIVATE ${PROJECT_SOURCE_DIR}/test)
+    target_link_libraries(${testname} PRIVATE ${CUDA_RUNTIME_LIBRARY} OpenMP::OpenMP_CXX)
+    install(TARGETS ${testname} RUNTIME DESTINATION ${CMAKE_INSTALL_SBINDIR}/performance)
+endfunction()
