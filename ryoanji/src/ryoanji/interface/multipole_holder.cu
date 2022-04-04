7630d995cb5ea65120679fdc8d258ff822ad703c
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-04-04 11:57:45 +0200

    added ryoanji::multipole_holder

---
+++ ryoanji/src/ryoanji/interface/multipole_holder.cu
@@ -0,0 +1,147 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! @file
+ * @brief  Interface for calculation of multipole moments
+ *
+ * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ */
+
+#include <thrust/device_vector.h>
+
+#include "multipole_holder.cuh"
+#include "ryoanji/nbody/gpu_config.h"
+#include "ryoanji/nbody/upwardpass.cuh"
+#include "ryoanji/nbody/upsweep_cpu.hpp"
+
+namespace ryoanji
+{
+
+template<class Tc, class Tm, class Tf, class KeyType, class MType>
+class MultipoleHolder<Tc, Tm, Tf, KeyType, MType>::Impl
+{
+public:
+    Impl() {}
+
+    void compute(const Tc* x, const Tc* y, const Tc* z, const Tm* m, const cstone::Octree<KeyType>& globalOctree,
+                 const cstone::FocusedOctree<KeyType, Tf>& focusTree, const cstone::LocalIndex* layout,
+                 MType* multipoles)
+    {
+        constexpr int numThreads = UpsweepConfig::numThreads;
+        const cstone::Octree<KeyType>& octree = focusTree.octree();
+
+        //auto centers       = focusTree.expansionCenters();
+        auto globalCenters = focusTree.globalExpansionCenters();
+
+        TreeNodeIndex numLeaves = focusTree.octree().numLeafNodes();
+
+        // H2D leafToInternal, layout, centers
+
+        computeLeafMultipoles<<<(numLeaves - 1) / numThreads + 1, numThreads>>>(x,
+                                                                                y,
+                                                                                z,
+                                                                                m,
+                                                                                rawPtr(leafToInternal_.data()),
+                                                                                numLeaves,
+                                                                                rawPtr(layout_.data()),
+                                                                                rawPtr(centers_.data()),
+                                                                                rawPtr(multipoles_.data()));
+
+        //! first upsweep with local data
+        int  numLevels  = 21;
+        auto levelRange = octree.levelRange();
+        for (int level = numLevels - 1; level >= 1; level--)
+        {
+            int numCellsLevel = levelRange[level + 1] - levelRange[level];
+            int numBlocks     = (numCellsLevel - 1) / numThreads + 1;
+            upsweepMultipoles<<<numBlocks, numThreads>>>(levelRange[level + 1],
+                                                         levelRange[level],
+                                                         rawPtr(childOffsets_.data()),
+                                                         rawPtr(centers_.data()),
+                                                         multipoles);
+        }
+
+        // D2H multipoles
+
+        auto ryUpsweep = [](auto levelRange, auto childOffsets, auto M, auto centers)
+        { upsweepMultipoles(levelRange, childOffsets, centers, M); };
+
+        gsl::span multipoleSpan{multipoles, size_t(octree.numTreeNodes())};
+        cstone::globalFocusExchange(globalOctree, focusTree, multipoleSpan, ryUpsweep, globalCenters.data());
+
+        focusTree.peerExchange(multipoleSpan, static_cast<int>(cstone::P2pTags::focusPeerCenters) + 1);
+
+        // H2D multipoles
+
+        //! second upsweep with leaf data from peer and global ranks in place
+        for (int level = numLevels - 1; level >= 1; level--)
+        {
+            int numCellsLevel = levelRange[level + 1] - levelRange[level];
+            int numBlocks     = (numCellsLevel - 1) / numThreads + 1;
+            upsweepMultipoles<<<numBlocks, numThreads>>>(levelRange[level + 1],
+                                                         levelRange[level],
+                                                         rawPtr(childOffsets_.data()),
+                                                         rawPtr(centers_.data()),
+                                                         rawPtr(multipoles_.data()));
+        }
+    }
+
+private:
+    thrust::device_vector<TreeNodeIndex> leafToInternal_;
+    thrust::device_vector<TreeNodeIndex> childOffsets_;
+
+    thrust::device_vector<LocalIndex> layout_;
+
+    thrust::device_vector<Vec4<Tf>> centers_;
+    thrust::device_vector<MType>    multipoles_;
+};
+
+template<class Tc, class Tm, class Tf, class KeyType, class MType>
+MultipoleHolder<Tc, Tm, Tf, KeyType, MType>::MultipoleHolder() : impl_(new Impl())
+{
+}
+
+template<class Tc, class Tm, class Tf, class KeyType, class MType>
+MultipoleHolder<Tc, Tm, Tf, KeyType, MType>::~MultipoleHolder() = default;
+
+template<class Tc, class Tm, class Tf, class KeyType, class MType>
+void MultipoleHolder<Tc, Tm, Tf, KeyType, MType>::compute(const Tc* x, const Tc* y, const Tc* z, const Tm* m,
+                                                          const cstone::Octree<KeyType>&            globalTree,
+                                                          const cstone::FocusedOctree<KeyType, Tf>& focusTree,
+                                                          const LocalIndex* layout, MType* multipoles)
+{
+    impl_->compute(x, y, z, m, globalTree, focusTree, layout, multipoles);
+}
+
+template class MultipoleHolder<double, double, double, uint64_t, SphericalMultipole<double, 4>>;
+template class MultipoleHolder<double, float, double, uint64_t, SphericalMultipole<double, 4>>;
+template class MultipoleHolder<double, float, double, uint64_t, SphericalMultipole<float, 4>>;
+template class MultipoleHolder<float, float, float, uint64_t, SphericalMultipole<float, 4>>;
+template class MultipoleHolder<double, double, double, uint32_t, SphericalMultipole<double, 4>>;
+template class MultipoleHolder<double, float, double, uint32_t, SphericalMultipole<double, 4>>;
+template class MultipoleHolder<double, float, double, uint32_t, SphericalMultipole<float, 4>>;
+template class MultipoleHolder<float, float, float, uint32_t, SphericalMultipole<float, 4>>;
+
+} // namespace ryoanji
