16e5a2d8d108c167a5fafa90b463bad1f3870290
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-05-05 15:53:29 +0200

    initialize rho0 with self-contribution

--- sph/include/sph/kernel_ve/rho_zero_kern.hpp
+++ sph/include/sph/kernel_ve/rho_zero_kern.hpp
@@ -51,12 +51,14 @@ CUDA_DEVICE_HOST_FUN inline void rho0JLoop(int i, T sincIndex, T K, const cstone
     T yi = y[i];
     T zi = z[i];
     T hi = h[i];
+    T mi = m[i];
 
     T hInv  = 1.0 / hi;
     T h3Inv = hInv * hInv * hInv;
 
-    T rho0i  = 0.0;
-    T wrho0i = 0.0;
+    // initialize with self-contribution
+    T rho0i  = mi;
+    T wrho0i = -T(3) * mi;
     for (int pj = 0; pj < neighborsCount; ++pj)
     {
         int j     = neighbors[pj];
@@ -64,15 +66,14 @@ CUDA_DEVICE_HOST_FUN inline void rho0JLoop(int i, T sincIndex, T K, const cstone
         T   vloc  = dist * hInv;
         T   w     = ::sphexa::math::pow(lt::wharmonic_lt_with_derivative(wh, whd, vloc), sincIndex);
         T   dw    = wharmonic_derivative(vloc, w) * sincIndex;
-        T   dterh = -(3.0 * w + vloc * dw);
+        T   dterh = -(T(3) * w + vloc * dw);
 
         rho0i += w * m[j];
         wrho0i += dterh * m[j];
     }
 
-    // XXXX Define new arrays rho0, wrho0, xmass for all particles
-    rho0[i]  = K * (rho0i + m[i]) * h3Inv;
-    wrho0[i] = K * (wrho0i - 3.0 * m[i]) * h3Inv * hInv;
+    rho0[i]  = rho0i * K * h3Inv;
+    wrho0[i] = wrho0i * K * h3Inv * hInv;
 }
 
 } // namespace kernels
