62670c52b1843b2b4270805f5bb24d78b4f04900
luke_sch02 <lukas.schmidt@gmx.ch>
2022-05-13 10:38:09 +0200

    adressed requested changes

--- main/src/observables/gravitational_waves.hpp
+++ main/src/observables/gravitational_waves.hpp
@@ -23,68 +23,19 @@
 * SOFTWARE.
 */
 
-/*! @file calculate gravitational waves observable
+/*! @file output time, energies and the gravitational radiation as well as the quadrupole moment
  *
  *  @author Lukas Schmidt
  */
 
 #include <array>
 #include "mpi.h"
-
 #include "iobservables.hpp"
 #include "io/ifile_writer.hpp"
+#include "grav_waves_calculations.hpp"
 
 namespace sphexa
 {
-
-//!@brief calculates the second derivative of the quadpole momentum
-template<class T, class Dataset>
-T d2QuadpoleMomentum(size_t begin, size_t end, int pos1, int pos2, Dataset& d)
-{
-    T* x        = d.x.data();
-    T* y        = d.y.data();
-    T* z        = d.z.data();
-    T* vx       = d.vx.data();
-    T* vy       = d.vy.data();
-    T* vz       = d.vz.data();
-    T* gradP_x  = d.grad_P_x.data();
-    T* gradP_y  = d.grad_P_y.data();
-    T* gradP_z  = d.grad_P_z.data();
-    T* m        = d.m.data();
-
-    T out = 0.0;
-
-    std::array<T*, 3> pos = {x, y, z};
-    std::array<T*, 3> vel = {vx, vy, vz};
-    std::array<T*, 3> gradP = {gradP_x, gradP_y, gradP_z};
-
-    if(pos1 == pos2)
-    {
-
-#pragma omp parallel for reduction(+ : out)
-        for(size_t i = begin; i < end; i++)
-        {
-            T acc1 = -gradP[pos1][i];
-            T factor1 = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
-            T factor2 = x[i] * (-gradP_x[i]) + y[i] * (-gradP_y[i]) + z[i] * (-gradP_z[i]);
-
-            out += (3.0 * (vel[pos1][i] * vel[pos1][i] + pos[pos1][i] * acc1) - factor1 - factor2) * m[i];
-
-        }
-        return out * 2.0 / 3.0;
-    }
-    else
-    {
-#pragma omp parallel for reduction(+ : out)
-        for(size_t i = begin; i < end; i++)
-        {
-            T acc1 = -gradP[pos1][i];
-            T acc2 = -gradP[pos2][i];
-            out += (2.0 * vel[pos1][i] * vel[pos2][i] + acc1 * pos[pos2][i] + pos[pos1][i] * acc2) * m[i];
-        }
-        return out;
-    }
-}
 
 /*!@brief calculates the gravitational waves observable
  *
@@ -95,71 +46,50 @@ T d2QuadpoleMomentum(size_t begin, size_t end, int pos1, int pos2, Dataset& d)
  * @param last          first locally assigned particle index of buffers in @p d
  * @param viewTheta     viewing angle to determine ...
  * @param viewPhi       viewing angle to determine ...
- * @return              array containing the observables and the second derivative of the quadpole momentum
+ * @return              array containing the observables and the second derivative of the quadpole momentum:
+ *                      {httplus, httcross, ixx, iyy, izz, ixy, ixz, iyz}
  */
 template<class T, class Dataset>
 std::array<T, 8> gravRad(Dataset& d,size_t first, size_t last, T viewTheta, T viewPhi)
 {
-    T dot2ibartt;
-    T dot2ibarpp;
-    T dot2ibartp;
 
-    T g = 6.6726e-8;    //Gravitational constant
-    T c = 2.997924562e10;   //Speed of light
-    T gwunits = g / std::pow(c, 4) / 3.08568025e22; //Gravitational Waves unit at 10kpc
+    std::array<T, 6> d2Qxx_local;
 
-    std::array<T, 6> d2Local;
+    d2Qxx_local[0] = d2QuadpoleMomentum<T>(first, last, 0, 0,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //ixx
+    d2Qxx_local[1] = d2QuadpoleMomentum<T>(first, last, 1, 1,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //iyy
+    d2Qxx_local[2] = d2QuadpoleMomentum<T>(first, last, 2, 2,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //izz
 
-    d2Local[0] = d2QuadpoleMomentum<T>(first, last, 0, 0, d); //ixx
-    d2Local[1] = d2QuadpoleMomentum<T>(first, last, 1, 1, d); //iyy
-    d2Local[2] = d2QuadpoleMomentum<T>(first, last, 2, 2, d); //izz
-
-    d2Local[3] = d2QuadpoleMomentum<T>(first, last, 0, 1, d); //ixy
-    d2Local[4] = d2QuadpoleMomentum<T>(first, last, 0, 2, d); //ixz
-    d2Local[5] = d2QuadpoleMomentum<T>(first, last, 1, 2, d); //iyz
+    d2Qxx_local[3] = d2QuadpoleMomentum<T>(first, last, 0, 1,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //ixy
+    d2Qxx_local[4] = d2QuadpoleMomentum<T>(first, last, 0, 2,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //ixz
+    d2Qxx_local[5] = d2QuadpoleMomentum<T>(first, last, 1, 2,
+                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
+                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data()); //iyz
 
 
     int rootRank = 0;
-    std::array<T, 6> d2Global;
-    MPI_Reduce(d2Local.data(), d2Global.data(),6, MpiType<T>{}, MPI_SUM, rootRank, MPI_COMM_WORLD);
+    std::array<T, 6> d2Qxx_global;
+    MPI_Reduce(d2Qxx_local.data(), d2Qxx_global.data(),6, MpiType<T>{}, MPI_SUM, rootRank, MPI_COMM_WORLD);
 
-    if (viewTheta == 0.0 && viewPhi == 0.0)
-    {
-        dot2ibartt = d2Global[0];
-        dot2ibarpp = d2Global[1];
-        dot2ibartp = d2Global[3];
-    }
-    else
-    {
-        T sin2t = std::sin(2.0 * viewTheta);
-        T sin2p = std::sin(2.0 * viewPhi);
-        T cos2p = std::cos(2.0 * viewPhi);
-        T sint  = std::sin(viewTheta);
-        T sinp  = std::sin(viewPhi);
-        T cost  = std::cos(viewTheta);
-        T cosp  = std::cos(viewPhi);
-        T sqsint = sint * sint;
-        T sqsinp = sinp * sinp;
-        T sqcost = cost * cost;
-        T sqcosp = cosp * cosp;
+    T httplus;
+    T httcross;
 
-        dot2ibartt = (d2Global[0] * sqcosp + d2Global[1] * sqsinp + d2Global[3] * sin2p) * sqcost + d2Global[2] * sqsint
-                        - (d2Global[4] * cosp + d2Global[5] * sinp) * sin2t;
+    computeHtt(d2Qxx_global, viewTheta, viewPhi, &httplus, &httcross);
 
-        dot2ibarpp = d2Global[0] * sqsinp + d2Global[1] * sqcosp - d2Global[3] * sin2p;
-
-        dot2ibartp = 0.5 * (d2Global[1] - d2Global[0]) * cost * sin2p + d2Global[3] * cost * cos2p
-                        + (d2Global[4] * sinp - d2Global[5] * cosp) * sint;
-    }
-
-    T httplus = (dot2ibartt - dot2ibarpp) * gwunits;
-    T httcross = 2.0 * dot2ibartp * gwunits;
-
-    return {httplus, httcross, d2Global[0], d2Global[1], d2Global[2], d2Global[3], d2Global[4], d2Global[5]};
+    return {httplus, httcross, d2Qxx_global[0], d2Qxx_global[1], d2Qxx_global[2], d2Qxx_global[3], d2Qxx_global[4], d2Qxx_global[5]};
 }
 
 
-//! @brief Observables that includes times, energies and gravitational wave something //TODO
+//! @brief Observables that includes times, energies, gravitational radiation and the second derivative of the quadrupole moment
 template<class Dataset>
 class GravWaves : public IObservables<Dataset>
 {
--- main/test/observables/gravitational_waves.cpp
+++ main/test/observables/gravitational_waves.cpp
@@ -28,28 +28,14 @@
  * @author Lukas Schmidt
  */
 
-#include <algorithm>
 #include "gtest/gtest.h"
-#include "cstone/domain/domain.hpp"
-#include "observables/gravitational_waves.hpp"
-#include "sph/particles_data.hpp"
+#include "observables/grav_waves_calculations.hpp"
 
 using namespace sphexa;
-using AccType = cstone::CpuTag;
 using T = double;
-using KeyType = uint64_t;
-using Dataset = ParticlesData<T, KeyType, AccType>;
 
-
-
-TEST(observables, grav)
+TEST(grav_observable, quadpoleMomentum)
 {
-    MPI_Init(NULL, NULL);
-    int rank = 0, numRanks = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
-
-    Dataset d;
 
     std::vector<T> x = {123.0,   234.0,  -345.0, 456.0,  -567.0};
     std::vector<T> y = {-678.0, 789.0,  -890.0, -901.0, 112.0};
@@ -59,35 +45,43 @@ TEST(observables, grav)
     std::vector<T> vy = {-778.0,    489.0e-1,   -810.0,     900.0,      122.0};
     std::vector<T> vz = {-722.0e2,  -234.0,     -541.0e2,   705.0,      232.0e-1};
 
-    std::vector<T> gradP_x = {-123.0e-3, 334.0,     345.0,      136.0e-3,  -167.0};
-    std::vector<T> gradP_y = {-678.0e-3, -789.0,    -891.0e-3,  -901.0,    -132.0e-3};
-    std::vector<T> gradP_z = {-122.0e-3, 333.0e-3,  543.0e-3,   -765.0,    214.0};
+    std::vector<T> ax = {123.0e-3, -334.0,     -345.0,      -136.0e-3,  167.0};
+    std::vector<T> ay = {678.0e-3, 789.0,    891.0e-3,  901.0,    132.0e-3};
+    std::vector<T> az = {122.0e-3, -333.0e-3,  -543.0e-3,   765.0,    -214.0};
     std::vector<T> m = {1.0e5,  1.0e5,  1.0e5,  1.0e5,  1.0e5,};
 
-    d.x = x;
-    d.y = y;
-    d.z = z;
-    d.vx = vx;
-    d.vy = vy;
-    d.vz = vz;
-    d.grad_P_x = gradP_x;
-    d.grad_P_y = gradP_y;
-    d.grad_P_z = gradP_z;
-    d.m = m;
+    T ixx = d2QuadpoleMomentum<T>(0, 5, 0, 0, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                               ax.data(), ay.data(), az.data(), m.data());
+    T iyy = d2QuadpoleMomentum<T>(0, 5, 1, 1, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                                  ax.data(), ay.data(), az.data(), m.data());
+    T izz = d2QuadpoleMomentum<T>(0, 5, 2, 2, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                                  ax.data(), ay.data(), az.data(), m.data());
+    T ixy = d2QuadpoleMomentum<T>(0, 5, 0, 1, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                                  ax.data(), ay.data(), az.data(), m.data());
+    T ixz = d2QuadpoleMomentum<T>(0, 5, 0, 2, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                                  ax.data(), ay.data(), az.data(), m.data());
+    T iyz = d2QuadpoleMomentum<T>(0, 5, 1, 2, x.data(), y.data(), z.data(), vx.data(), vy.data(), vz.data(),
+                          ax.data(), ay.data(), az.data(), m.data());
 
+
+    EXPECT_EQ(ixx, -2.89095364643866687e14);   //ixx
+    EXPECT_EQ(iyy, -6.69346245332466625e14);   //iyy
+    EXPECT_EQ(izz, 9.58441609976333125e14);    //izz
+    EXPECT_EQ(ixy, -6.17629053030000000e12);   //ixy
+    EXPECT_EQ(ixz, -3.74431432361500000e14);   //ixz
+    EXPECT_EQ(iyz, 2.01029844058000000e13);    //iyz
+
+}
+TEST(grav_observable, httcalc)
+{
     T viewTheta = 0.545;
-    T viewPhi   = 1.421;
+    T viewPhi = 1.421;
+    T httplus;
+    T httcross;
+    std::array<T, 6> quadpole = {-2.89095364643866687e14, -6.69346245332466625e14, 9.58441609976333125e14,
+                                 -6.17629053030000000e12, -3.74431432361500000e14, 2.01029844058000000e13};
+    sphexa::computeHtt(quadpole, viewTheta, viewPhi, &httplus, &httcross);
 
-    std::array<T, 8> test = gravRad(d, 0, 5 , viewTheta, viewPhi);
-
-    EXPECT_NEAR(test[0], 2.69459728766579961e-58, 1e-72); //httplus
-    EXPECT_EQ(test[1], -1.26588487735439141e-57);//httcross
-
-    EXPECT_EQ(test[2], -2.89095364643866687e14);   //ixx
-    EXPECT_EQ(test[3], -6.69346245332466625e14);   //iyy
-    EXPECT_EQ(test[4], 9.58441609976333125e14);    //izz
-    EXPECT_EQ(test[5], -6.17629053030000000e12);   //ixy
-    EXPECT_EQ(test[6], -3.74431432361500000e14);   //ixz
-    EXPECT_EQ(test[7], 2.01029844058000000e13);    //iyz
-
+    EXPECT_NEAR(2.69459728766579961E-58, httplus, 1e-72);
+    EXPECT_NEAR(-1.26588487735439141E-57, httcross, 1e-72);
 }
