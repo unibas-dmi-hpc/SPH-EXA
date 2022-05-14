ec2c911fdf05fff7903735c43a8c9511108ea538
luke_sch02 <65285757+lks1248@users.noreply.github.com>
2022-05-14 12:43:06 +0200

    added Gravitational Waves observable (#251)
    * added Gravitational Waves observable

--- main/src/observables/factory.hpp
+++ main/src/observables/factory.hpp
@@ -37,6 +37,7 @@
 #include "iobservables.hpp"
 #include "time_energy_growth.hpp"
 #include "time_energies.hpp"
+#include "gravitational_waves.hpp"
 
 namespace sphexa
 {
@@ -94,6 +95,8 @@ std::unique_ptr<IObservables<Dataset>> observablesFactory(const std::string& tes
 {
 #ifdef SPH_EXA_HAVE_H5PART
     std::string khGrowthRate = "KelvinHelmholtzGrowthRate";
+    std::string gravWaves = "observeGravWaves";
+
     if (haveH5Attribute(testCase, khGrowthRate, H5PART_INT64))
     {
         h5part_int64_t attrValue;
@@ -103,6 +106,16 @@ std::unique_ptr<IObservables<Dataset>> observablesFactory(const std::string& tes
         H5PartCloseFile(h5_file);
 
         if (attrValue) { return std::make_unique<TimeEnergyGrowth<Dataset>>(constantsFile); }
+    }
+
+    if(haveH5Attribute(testCase, gravWaves, H5PART_FLOAT64))
+    {
+        double attrValue[3];
+        H5PartFile*    h5_file = nullptr;
+        h5_file                = H5PartOpenFile(testCase.c_str(), H5PART_READ);
+        H5PartReadFileAttrib(h5_file, gravWaves.c_str(), attrValue);
+        H5PartCloseFile(h5_file);
+        if(attrValue[0] != 0) { return std::make_unique<GravWaves<Dataset>>(constantsFile, attrValue[1], attrValue[2]); }
     }
 #endif
 
---
+++ main/src/observables/grav_waves_calculations.hpp
@@ -0,0 +1,105 @@
+/*
+* MIT License
+*
+* Copyright (c) 2021 CSCS, ETH Zurich
+*               2021 University of Basel
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
+
+/*! @file calculations for the gravitational waves observable
+ *          References: Centrella & McMillan ApJ, 416 (1993), R. M. Cabezon Phd Thesis (2010)
+ *
+ *  @author Lukas Schmidt
+ */
+
+#include <cmath>
+
+namespace sphexa
+{
+
+//! @brief calculates the two polarization modes of the gravitational waves for a given quadrupole momentum
+template<class T>
+void computeHtt(std::array<T, 6> quadpoleMomentum, T theta, T phi, T* httplus, T* httcross)
+{
+    T g       = 6.6726e-8;                          // Gravitational constant
+    T c       = 2.997924562e10;                     // Speed of light
+    T gwunits = g / std::pow(c, 4) / 3.08568025e22; // Gravitational Waves unit at 10kpc
+
+    T sin2t  = std::sin(2.0 * theta);
+    T sin2p  = std::sin(2.0 * phi);
+    T cos2p  = std::cos(2.0 * phi);
+    T sint   = std::sin(theta);
+    T sinp   = std::sin(phi);
+    T cost   = std::cos(theta);
+    T cosp   = std::cos(phi);
+    T sqsint = sint * sint;
+    T sqsinp = sinp * sinp;
+    T sqcost = cost * cost;
+    T sqcosp = cosp * cosp;
+
+    T dot2ibartt =
+        (quadpoleMomentum[0] * sqcosp + quadpoleMomentum[1] * sqsinp + quadpoleMomentum[3] * sin2p) * sqcost +
+        quadpoleMomentum[2] * sqsint - (quadpoleMomentum[4] * cosp + quadpoleMomentum[5] * sinp) * sin2t;
+
+    T dot2ibarpp = quadpoleMomentum[0] * sqsinp + quadpoleMomentum[1] * sqcosp - quadpoleMomentum[3] * sin2p;
+
+    T dot2ibartp = 0.5 * (quadpoleMomentum[1] - quadpoleMomentum[0]) * cost * sin2p +
+                   quadpoleMomentum[3] * cost * cos2p +
+                   (quadpoleMomentum[4] * sinp - quadpoleMomentum[5] * cosp) * sint;
+
+    *httplus  = (dot2ibartt - dot2ibarpp) * gwunits;
+    *httcross = 2.0 * dot2ibartp * gwunits;
+}
+
+//!@brief calculates the second derivative of the quadrupole momentum
+template<class T>
+T d2QuadpoleMomentum(size_t begin, size_t end, int dim1, int dim2, const T* x, const T* y, const T* z, const T* vx,
+                     const T* vy, const T* vz, const T* ax, const T* ay, const T* az, const T* m)
+{
+    T out = 0.0;
+
+    std::array<const T*, 3> coords = {x, y, z};
+    std::array<const T*, 3> vel    = {vx, vy, vz};
+    std::array<const T*, 3> acc  = {ax, ay, az};
+
+    if (dim1 == dim2)
+    {
+
+#pragma omp parallel for reduction(+ : out)
+        for (size_t i = begin; i < end; i++)
+        {
+            T scalv2        = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
+            T coordDotAccel = x[i] * ax[i] + y[i] * ay[i] + z[i] * az[i];
+
+            out += (3.0 * (vel[dim1][i] * vel[dim1][i] + coords[dim1][i] * acc[dim1][i]) - scalv2 - coordDotAccel) * m[i];
+        }
+        return out * 2.0 / 3.0;
+    }
+    else
+    {
+#pragma omp parallel for reduction(+ : out)
+        for (size_t i = begin; i < end; i++)
+        {
+            out += (2.0 * vel[dim1][i] * vel[dim2][i] + acc[dim1][i] * coords[dim2][i] + coords[dim1][i] * acc[dim2][i]) * m[i];
+        }
+        return out;
+    }
+}
+}
