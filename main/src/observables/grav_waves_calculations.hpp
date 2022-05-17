1984d0a4d1d8f145e9b355f2c85694ac48cc2d49
luke_sch02 <lukas.schmidt@gmx.ch>
2022-05-17 12:36:33 +0200

    small adjustments for uniformity and readability

--- main/src/observables/grav_waves_calculations.hpp
+++ main/src/observables/grav_waves_calculations.hpp
@@ -1,26 +1,26 @@
 /*
-* MIT License
-*
-* Copyright (c) 2021 CSCS, ETH Zurich
-*               2021 University of Basel
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /*! @file calculations for the gravitational waves observable
@@ -33,6 +33,18 @@
 
 namespace sphexa
 {
+struct QIdx
+{
+    enum IndexLabels
+    {
+        xx = 0,
+        yy = 1,
+        zz = 2,
+        xy = 3,
+        xz = 4,
+        yz = 5
+    };
+};
 
 //! @brief calculates the two polarization modes of the gravitational waves for a given quadrupole momentum
 template<class T>
@@ -54,15 +66,18 @@ void computeHtt(std::array<T, 6> quadpoleMomentum, T theta, T phi, T* httplus, T
     T sqcost = cost * cost;
     T sqcosp = cosp * cosp;
 
-    T dot2ibartt =
-        (quadpoleMomentum[0] * sqcosp + quadpoleMomentum[1] * sqsinp + quadpoleMomentum[3] * sin2p) * sqcost +
-        quadpoleMomentum[2] * sqsint - (quadpoleMomentum[4] * cosp + quadpoleMomentum[5] * sinp) * sin2t;
+    T dot2ibartt = (quadpoleMomentum[QIdx::xx] * sqcosp + quadpoleMomentum[QIdx::yy] * sqsinp +
+                    quadpoleMomentum[QIdx::xy] * sin2p) *
+                       sqcost +
+                   quadpoleMomentum[QIdx::zz] * sqsint -
+                   (quadpoleMomentum[QIdx::xz] * cosp + quadpoleMomentum[QIdx::yz] * sinp) * sin2t;
 
-    T dot2ibarpp = quadpoleMomentum[0] * sqsinp + quadpoleMomentum[1] * sqcosp - quadpoleMomentum[3] * sin2p;
+    T dot2ibarpp =
+        quadpoleMomentum[QIdx::xx] * sqsinp + quadpoleMomentum[QIdx::yy] * sqcosp - quadpoleMomentum[QIdx::xy] * sin2p;
 
-    T dot2ibartp = 0.5 * (quadpoleMomentum[1] - quadpoleMomentum[0]) * cost * sin2p +
-                   quadpoleMomentum[3] * cost * cos2p +
-                   (quadpoleMomentum[4] * sinp - quadpoleMomentum[5] * cosp) * sint;
+    T dot2ibartp = 0.5 * (quadpoleMomentum[QIdx::yy] - quadpoleMomentum[QIdx::xx]) * cost * sin2p +
+                   quadpoleMomentum[QIdx::xy] * cost * cos2p +
+                   (quadpoleMomentum[QIdx::xz] * sinp - quadpoleMomentum[QIdx::yz] * cosp) * sint;
 
     *httplus  = (dot2ibartt - dot2ibarpp) * gwunits;
     *httcross = 2.0 * dot2ibartp * gwunits;
@@ -77,7 +92,7 @@ T d2QuadpoleMomentum(size_t begin, size_t end, int dim1, int dim2, const T* x, c
 
     std::array<const T*, 3> coords = {x, y, z};
     std::array<const T*, 3> vel    = {vx, vy, vz};
-    std::array<const T*, 3> acc  = {ax, ay, az};
+    std::array<const T*, 3> acc    = {ax, ay, az};
 
     if (dim1 == dim2)
     {
@@ -88,7 +103,8 @@ T d2QuadpoleMomentum(size_t begin, size_t end, int dim1, int dim2, const T* x, c
             T scalv2        = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
             T coordDotAccel = x[i] * ax[i] + y[i] * ay[i] + z[i] * az[i];
 
-            out += (3.0 * (vel[dim1][i] * vel[dim1][i] + coords[dim1][i] * acc[dim1][i]) - scalv2 - coordDotAccel) * m[i];
+            out +=
+                (3.0 * (vel[dim1][i] * vel[dim1][i] + coords[dim1][i] * acc[dim1][i]) - scalv2 - coordDotAccel) * m[i];
         }
         return out * 2.0 / 3.0;
     }
@@ -97,9 +113,11 @@ T d2QuadpoleMomentum(size_t begin, size_t end, int dim1, int dim2, const T* x, c
 #pragma omp parallel for reduction(+ : out)
         for (size_t i = begin; i < end; i++)
         {
-            out += (2.0 * vel[dim1][i] * vel[dim2][i] + acc[dim1][i] * coords[dim2][i] + coords[dim1][i] * acc[dim2][i]) * m[i];
+            out +=
+                (2.0 * vel[dim1][i] * vel[dim2][i] + acc[dim1][i] * coords[dim2][i] + coords[dim1][i] * acc[dim2][i]) *
+                m[i];
         }
         return out;
     }
 }
-}
+} // namespace sphexa
--- main/src/observables/gravitational_waves.hpp
+++ main/src/observables/gravitational_waves.hpp
@@ -1,27 +1,27 @@
 /*
-* MIT License
-*
-* Copyright (c) 2021 CSCS, ETH Zurich
-*               2021 University of Basel
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /*! @file output time, energies and the gravitational radiation as well as the quadrupole moment
  *
@@ -50,20 +50,8 @@ namespace sphexa
  *                      {httplus, httcross, ixx, iyy, izz, ixy, ixz, iyz}
  */
 template<class T, class Dataset>
-std::array<T, 8> gravRad(Dataset& d,size_t first, size_t last, T viewTheta, T viewPhi)
+auto gravRad(Dataset& d, size_t first, size_t last, T viewTheta, T viewPhi)
 {
-    struct Q
-    {
-        enum IndexLabels
-        {
-            xx = 0,
-            yy = 1,
-            zz = 2,
-            xy = 3,
-            xz = 4,
-            yz = 5
-        };
-    };
 
     struct Dim
     {
@@ -77,69 +65,151 @@ std::array<T, 8> gravRad(Dataset& d,size_t first, size_t last, T viewTheta, T vi
 
     std::array<T, 6> d2Q_local;
 
-    d2Q_local[Q::xx] = d2QuadpoleMomentum<T>(first, last, Dim::x, Dim::x,
-                                       d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                            d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
-    d2Q_local[Q::yy] = d2QuadpoleMomentum<T>(first, last, Dim::y, Dim::y,
-                                                           d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                                           d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
-    d2Q_local[Q::zz] = d2QuadpoleMomentum<T>(first, last, Dim::z, Dim::z,
-                                                           d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                                           d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
-    d2Q_local[Q::xy] = d2QuadpoleMomentum<T>(first, last, Dim::x, Dim::y,
-                                                           d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                                           d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
-    d2Q_local[Q::xz] = d2QuadpoleMomentum<T>(first, last, Dim::x, Dim::z,
-                                                           d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                                           d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
-    d2Q_local[Q::yz] = d2QuadpoleMomentum<T>(first, last, Dim::y, Dim::z,
-                                                           d.x.data(), d.y.data(), d.z.data(), d.vx.data(), d.vy.data(), d.vz.data(),
-                                                           d.ax.data(), d.ay.data(), d.az.data(), d.m.data());
+    d2Q_local[QIdx::xx] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::x,
+                                                Dim::x,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
+    d2Q_local[QIdx::yy] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::y,
+                                                Dim::y,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
+    d2Q_local[QIdx::zz] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::z,
+                                                Dim::z,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
+    d2Q_local[QIdx::xy] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::x,
+                                                Dim::y,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
+    d2Q_local[QIdx::xz] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::x,
+                                                Dim::z,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
+    d2Q_local[QIdx::yz] = d2QuadpoleMomentum<T>(first,
+                                                last,
+                                                Dim::y,
+                                                Dim::z,
+                                                d.x.data(),
+                                                d.y.data(),
+                                                d.z.data(),
+                                                d.vx.data(),
+                                                d.vy.data(),
+                                                d.vz.data(),
+                                                d.ax.data(),
+                                                d.ay.data(),
+                                                d.az.data(),
+                                                d.m.data());
 
-    int rootRank = 0;
-    std::array<T, 6> d2Qxx_global;
-    MPI_Reduce(d2Q_local.data(), d2Qxx_global.data(),6, MpiType<T>{}, MPI_SUM, rootRank, MPI_COMM_WORLD);
+    int              rootRank = 0;
+    std::array<T, 6> d2Q_global;
+    MPI_Reduce(d2Q_local.data(), d2Q_global.data(), 6, MpiType<T>{}, MPI_SUM, rootRank, MPI_COMM_WORLD);
 
     T httplus;
     T httcross;
 
-    computeHtt(d2Qxx_global, viewTheta, viewPhi, &httplus, &httcross);
+    computeHtt(d2Q_global, viewTheta, viewPhi, &httplus, &httcross);
 
-    return {httplus, httcross, d2Qxx_global[0], d2Qxx_global[1], d2Qxx_global[2], d2Qxx_global[3], d2Qxx_global[4], d2Qxx_global[5]};
+    return std::make_tuple(
+        httplus, httcross, d2Q_global[0], d2Q_global[1], d2Q_global[2], d2Q_global[3], d2Q_global[4], d2Q_global[5]);
 }
 
-
-//! @brief Observables that includes times, energies, gravitational radiation and the second derivative of the quadrupole moment
+//! @brief Observables that includes times, energies, gravitational radiation and the second derivative of the
+//! quadrupole moment
 template<class Dataset>
 class GravWaves : public IObservables<Dataset>
 {
     using T = typename Dataset::RealType;
     std::ofstream& constantsFile;
-    T viewTheta;
-    T viewPhi;
+    T              viewTheta;
+    T              viewPhi;
 
 public:
     GravWaves(std::ofstream& constPath, T theta, T phi)
-        : constantsFile(constPath), viewTheta(theta), viewPhi(phi)
+        : constantsFile(constPath)
+        , viewTheta(theta)
+        , viewPhi(phi)
     {
     }
-
-
 
     void computeAndWrite(Dataset& d, size_t firstIndex, size_t lastIndex, cstone::Box<T>& box)
     {
 
-        std::array<T, 8> out = gravRad(d, firstIndex, lastIndex, viewTheta, viewPhi);
+        auto [httplus, httcross, d2xx, d2yy, d2zz, d2xy, d2xz, d2yz] =
+            gravRad(d, firstIndex, lastIndex, viewTheta, viewPhi);
 
         int rank;
         MPI_Comm_rank(d.comm, &rank);
 
         if (rank == 0)
         {
-            fileutils::writeColumns(
-                constantsFile, ' ', d.iteration, d.ttot, d.minDt, d.etot, d.ecin, d.eint, d.egrav,
-                out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7]);
+            fileutils::writeColumns(constantsFile,
+                                    ' ',
+                                    d.iteration,
+                                    d.ttot,
+                                    d.minDt,
+                                    d.etot,
+                                    d.ecin,
+                                    d.eint,
+                                    d.egrav,
+                                    httplus,
+                                    httcross,
+                                    d2xx,
+                                    d2yy,
+                                    d2zz,
+                                    d2xy,
+                                    d2xz,
+                                    d2yz);
         }
     }
 };
-}
+} // namespace sphexa
