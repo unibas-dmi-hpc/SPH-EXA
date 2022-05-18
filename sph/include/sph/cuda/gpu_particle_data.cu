1fcd23ef75115c3051bcf997c96940ed61f6a08d
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-05-18 17:13:01 +0200

    clang-format

--- sph/include/sph/cuda/gpu_particle_data.cu
+++ sph/include/sph/cuda/gpu_particle_data.cu
@@ -45,18 +45,12 @@ void DeviceParticlesData<T, KeyType>::resize(size_t size)
     auto deallocateVector = [size](auto* devVectorPtr)
     {
         using DevVector = std::decay_t<decltype(*devVectorPtr)>;
-        if (devVectorPtr->capacity() < size)
-        {
-            *devVectorPtr = DevVector{};
-        }
+        if (devVectorPtr->capacity() < size) { *devVectorPtr = DevVector{}; }
     };
 
     for (size_t i = 0; i < data_.size(); ++i)
     {
-        if (this->isAllocated(i))
-        {
-            std::visit(deallocateVector, data_[i]);
-        }
+        if (this->isAllocated(i)) { std::visit(deallocateVector, data_[i]); }
     }
 
     for (size_t i = 0; i < data_.size(); ++i)
