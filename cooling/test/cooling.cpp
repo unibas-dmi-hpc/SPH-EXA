d486dea24a07c583e947084c8c81a5b70b659fe3
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-06-07 13:32:14 +0200

    preliminary cooling interface cleaning

--- cooling/test/cooling.cpp
+++ cooling/test/cooling.cpp
@@ -34,10 +34,8 @@
 #include "gtest/gtest.h"
 #include "cooling.hpp"
 
-
 TEST(cooling_grackle, test1a)
 {
-
     grackle_options options;
     options.with_radiative_cooling = 1;
     options.primordial_chemistry   = 3;
@@ -46,82 +44,81 @@ TEST(cooling_grackle, test1a)
     options.UVbackground           = 1;
 
     // WTF ?!
-    //initGrackle("/Users/noah/Documents/Changa/grackle/grackle/grackle_data_files/input/CloudyData_UVB=HM2012.h5",
+    // initGrackle("/Users/noah/Documents/Changa/grackle/grackle/grackle_data_files/input/CloudyData_UVB=HM2012.h5",
     //            options);
 
     using Real = double;
 
-    double   dt          = 3.15e7 * 1e6; // grackle_units.time_units;
+    double dt = 3.15e7 * 1e6; // grackle_units.time_units;
 
     constexpr gr_float tiny_number = 1.e-20;
-    constexpr Real mh 1.67262171e-24
-    constexpr Real kboltz 1.3806504e-16
+    constexpr Real     mh 1.67262171e-24;
+    constexpr Real     kboltz 1.3806504e-16;
 
-    d.dt  = std::vector<Real>{dt};
-    d.x   = std::vector<Real>{0.};
-    d.y   = std::vector<Real>{0.};
-    d.z   = std::vector<Real>{0.};
-    d.rho = std::vector<Real>{1.0};
-    double temperature_units =
-        mh * pow(code_units_simulation.a_units * code_units_simulation.length_units / code_units_simulation.time_units, 2.) / kboltz;
-    d.u              = std::vector<Real>{1000. / temperature_units};
-    d.HI_fraction    = std::vector<Real>{0.76};
-    d.HII_fraction   = std::vector<Real>{tiny_number};
-    d.HM_fraction    = std::vector<Real>{tiny_number};
-    d.HeI_fraction   = std::vector<Real>{0.24};
-    d.HeII_fraction  = std::vector<Real>{tiny_number};
-    d.HeIII_fraction = std::vector<Real>{tiny_number};
-    d.H2I_fraction   = std::vector<Real>{tiny_number};
-    d.H2II_fraction  = std::vector<Real>{tiny_number};
-    d.DI_fraction    = std::vector<Real>{2.0 * 3.4e-5};
-    d.DII_fraction   = std::vector<Real>{tiny_number};
-    d.HDI_fraction   = std::vector<Real>{tiny_number};
-    d.e_fraction     = std::vector<Real>{tiny_number};
-    d.metal_fraction = std::vector<Real>{0.01295};
-    d.volumetric_heating_rate = std::vector<Real>{0.};
-    d.specific_heating_rate = std::vector<Real>{0.};
-    d.RT_heating_rate = std::vector<Real>{0.};
-    d.RT_HI_ionization_rate = std::vector<Real>{0.};
-    d.RT_HeI_ionization_rate = std::vector<Real>{0.};
-    d.RT_HeII_ionization_rate = std::vector<Real>{0.};
-    d.RT_H2_dissociation_rate = std::vector<Real>{0.};
-    d.H2_self_shielding_length = std::vector<Real>{0.};
-    d.n              = 1;
+    auto dt  = std::vector<Real>{dt};
+    auto rho = std::vector<Real>{1.0};
+    Real temperature_units =
+        mh *
+        pow(code_units_simulation.a_units * code_units_simulation.length_units / code_units_simulation.time_units, 2.) /
+        kboltz;
+    auto u                        = std::vector<Real>{1000. / temperature_units};
+    auto HI_fraction              = std::vector<Real>{0.76};
+    auto HII_fraction             = std::vector<Real>{tiny_number};
+    auto HM_fraction              = std::vector<Real>{tiny_number};
+    auto HeI_fraction             = std::vector<Real>{0.24};
+    auto HeII_fraction            = std::vector<Real>{tiny_number};
+    auto HeIII_fraction           = std::vector<Real>{tiny_number};
+    auto H2I_fraction             = std::vector<Real>{tiny_number};
+    auto H2II_fraction            = std::vector<Real>{tiny_number};
+    auto DI_fraction              = std::vector<Real>{2.0 * 3.4e-5};
+    auto DII_fraction             = std::vector<Real>{tiny_number};
+    auto HDI_fraction             = std::vector<Real>{tiny_number};
+    auto e_fraction               = std::vector<Real>{tiny_number};
+    auto metal_fraction           = std::vector<Real>{0.01295};
+    auto volumetric_heating_rate  = std::vector<Real>{0.};
+    auto specific_heating_rate    = std::vector<Real>{0.};
+    auto RT_heating_rate          = std::vector<Real>{0.};
+    auto RT_HI_ionization_rate    = std::vector<Real>{0.};
+    auto RT_HeI_ionization_rate   = std::vector<Real>{0.};
+    auto RT_HeII_ionization_rate  = std::vector<Real>{0.};
+    auto RT_H2_dissociation_rate  = std::vector<Real>{0.};
+    auto H2_self_shielding_length = std::vector<Real>{0.};
+
+    size_t numParticles = 1;
+
+    std::cout << HI_fraction[0] << std::endl;
+    std::cout << HeI_fraction[0] << std::endl;
+    std::cout << metal_fraction[0] << std::endl;
+
+    cool_particle(dt[0],
+                  rho[0],
+                  u[0],
+                  HI_fraction[0],
+                  HII_fraction[0],
+                  HM_fraction[0],
+                  HeI_fraction[0],
+                  HeII_fraction[0],
+                  HeIII_fraction[0],
+                  H2I_fraction[0],
+                  H2II_fraction[0],
+                  DI_fraction[0],
+                  DII_fraction[0],
+                  HDI_fraction[0],
+                  e_fraction[0],
+                  metal_fraction[0],
+                  volumetric_heating_rate[0],
+                  specific_heating_rate[0],
+                  RT_heating_rate[0],
+                  RT_HI_ionization_rate[0],
+                  RT_HeI_ionization_rate[0],
+                  RT_HeII_ionization_rate[0],
+                  RT_H2_dissociation_rate[0],
+                  H2_self_shielding_length[0]);
 
     std::cout << d.HI_fraction[0] << std::endl;
-    std::cout << d.HeI_fraction[0] << std::endl;
-    std::cout << d.metal_fraction[0] << std::endl;
 
-    cool_particle<Real, Dataset>(d.dt[0],
-                                 d.rho[0],
-                                 d.u[0],
-                                 d.HI_fraction[0],
-                                 d.HII_fraction[0],
-                                 d.HM_fraction[0],
-                                 d.HeI_fraction[0],
-                                 d.HeII_fraction[0],
-                                 d.HeIII_fraction[0],
-                                 d.H2I_fraction[0],
-                                 d.H2II_fraction[0],
-                                 d.DI_fraction[0],
-                                 d.DII_fraction[0],
-                                 d.HDI_fraction[0],
-                                 d.e_fraction[0],
-                                 d.metal_fraction[0],
-                                 d.volumetric_heating_rate[0],
-                                 d.specific_heating_rate[0],
-                                 d.RT_heating_rate[0],
-                                 d.RT_HI_ionization_rate[0],
-                                 d.RT_HeI_ionization_rate[0],
-                                 d.RT_HeII_ionization_rate[0],
-                                 d.RT_H2_dissociation_rate[0],
-                                 d.H2_self_shielding_length[0]
-    );
-
-    std::cout << d.HI_fraction[0] << std::endl;
-
-    EXPECT_NEAR(d.HI_fraction[0], 0.630705, 1e-6);
-    EXPECT_NEAR(d.u[0], 2.95159e+35, 1e30);
+    EXPECT_NEAR(HI_fraction[0], 0.630705, 1e-6);
+    EXPECT_NEAR(u[0], 2.95159e+35, 1e30);
 
     cleanGrackle();
 }
