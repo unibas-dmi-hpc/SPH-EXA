41eccc4bb88498d513938ee79cb11a53b780f177
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-06-07 14:01:59 +0200

    re-added grackle submodule

---
+++ extern/grackle/grackle_deps/src/clib/auto_show_version.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void auto_show_version(FILE *fp) {
+   fprintf (fp,"\n");
+   fprintf (fp,"The Grackle Version xxx\n");
+   fprintf (fp,"Git Branch   master\n");
+   fprintf (fp,"Git Revision xxx\n");
+   fprintf (fp,"\n");
+}
