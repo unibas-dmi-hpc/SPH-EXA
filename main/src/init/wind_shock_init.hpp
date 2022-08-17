0f9dee7b7178a8edea471e772a036d7848d35e00
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-08-17 08:58:23 +0200

    experimental rectangular box

--- main/src/init/wind_shock_init.hpp
+++ main/src/init/wind_shock_init.hpp
@@ -79,14 +79,14 @@ void initWindShockFields(Dataset& d, const std::map<std::string, double>& consta
 
     T k = 150. / r;
 
+    util::array<T, 3> blobCenter{r, r, r};
+
 #pragma omp parallel for schedule(static)
     for (size_t i = 0; i < d.x.size(); i++)
     {
-        T xi = std::abs(d.x[i]);
-        T yi = std::abs(d.y[i]);
-        T zi = std::abs(d.z[i]);
+        util::array<T, 3> X{d.x[i], d.y[i], d.z[i]};
 
-        T rPos = std::sqrt((xi * xi) + (yi * yi) + (zi * zi));
+        T rPos = std::sqrt(norm2(X - blobCenter));
 
         if (rPos > rSphere + epsilon)
         {
@@ -137,96 +137,6 @@ std::map<std::string, double> WindShockConstants()
             {"epsilon", 0.}};
 }
 
-/*! @brief compute the shift factor towards the center for point X in a capped pyramid
- *
- * @tparam T      float or double
- * @param  rPos   point radius < rExt
- * @param  rInt   half cube length of the internal high-density cube
- * @param  s      compression radius used to create the high-density cube, in [rInt, rExt]
- * @param  rExt   half cube length of the external low-density cube
- * @return        factor in [0:1]
- */
-template<class T>
-T sphereStretch(T rPos, T rInt, T s, T rExt)
-{
-    assert(rInt < s && s < rExt && rPos <= rExt);
-
-    T expo      = 0.71;
-    T ratio     = (rExt - rInt) / std::pow(rExt - s, expo);
-    T newRadius = rInt + std::pow(rPos - s, expo) * ratio;
-
-    return newRadius / rPos;
-}
-
-/*! returns a value in [rInt:rExt]
- *
- * @tparam T         float or double
- * @param  rInt      inner sphere half side
- * @param  rExt      outer sphere half side
- * @param  rhoRatio  the desired density ratio between inner and outer
- * @return           value s, such that if [-s, s]^3 gets contracted into the inner sphere
- *                   and [s:rExt, s:rExt]^3 is expanded into the resulting empty area,
- *                   the inner and outer spheres will have a density ratio of @p rhoRatio
- *
- * Derivation:
- *      internal density: rho_int = rho_0 * (s / rInt)^3
- *
- *      external density: rho_ext = rho_0  * (2rExt)^3 - (4/3*pi)*(s)^3
- *                                           -----------------------------
- *                                           (2rExt)^3 - (4/3*pi)*(rInt)^3
- *
- * The return value is the solution of rho_int / rho_ext == rhoRatio for s
- */
-template<class T>
-T WindShockcomputeStretchFactor(T rInt, T rExt, T rhoRatio)
-{
-    T factor = (4. / 3.) * M_PI;
-    T hc     = factor * rInt * rInt * rInt;
-    T rc     = 8. * rExt * rExt * rExt;
-    T s      = std::cbrt(rhoRatio * (hc / factor) * rc / (rc - hc + rhoRatio * hc));
-    assert(rInt < s && s < rExt);
-    return s;
-}
-
-template<class T>
-size_t WindShockcompressCenterSphere(gsl::span<T> x, gsl::span<T> y, gsl::span<T> z, T rInt, T s, T rExt, T epsilon)
-{
-    size_t sum = 0;
-
-#pragma omp parallel for reduction(+ : sum) schedule(static)
-    for (size_t i = 0; i < x.size(); i++)
-    {
-        T rPos = std::sqrt((x[i] * x[i]) + (y[i] * y[i]) + (z[i] * z[i]));
-
-        if (rPos - s > epsilon)
-        {
-            // Only streech particles inside the rExt radius sphere
-            if (rPos <= rExt)
-            {
-                T scaleFactor = sphereStretch(rPos, rInt, s, rExt);
-
-                x[i] *= scaleFactor;
-                y[i] *= scaleFactor;
-                z[i] *= scaleFactor;
-            }
-        }
-        else
-        {
-            // particle in inner high-density region get contracted towards the center
-            x[i] *= rInt / s;
-            y[i] *= rInt / s;
-            z[i] *= rInt / s;
-
-            sum++;
-        }
-    }
-
-    std::cout << "rExt=" << rExt << ", s=" << s << ", rInt=" << rInt << "; Particles in High density region: " << sum
-              << std::endl;
-
-    return sum;
-}
-
 template<class Dataset>
 class WindShockGlass : public ISimInitializer<Dataset>
 {
@@ -258,17 +168,46 @@ public:
         size_t multiplicity  = std::rint(cbrtNumPart / std::cbrt(blockSize));
         d.numParticlesGlobal = multiplicity * multiplicity * multiplicity * blockSize;
 
-        cstone::Box<T> globalBox(-r, r, true);
-        auto [keyStart, keyEnd] = partitionRange(cstone::nodeRange<KeyType>(0), rank, numRanks);
-        assembleCube<T>(keyStart, keyEnd, globalBox, multiplicity, xBlock, yBlock, zBlock, d.x, d.y, d.z);
+        cstone::Box<T> globalBox(0, 4 * r, 0, 2 * r, 0, 2 * r, true);
+        cstone::Box<T> boxA(0, 2 * r, 0, 2 * r, 0, 2 * r, true);
+        cstone::Box<T> boxB(2 * r, 4 * r, 0, 2 * r, 0, 2 * r, true);
 
-        T      s                    = WindShockcomputeStretchFactor(rSphere, r, rhoInt / rhoExt);
-        size_t numParticlesInternal = WindShockcompressCenterSphere<T>(d.x, d.y, d.z, rSphere, s, r, epsilon);
+        auto [keyStart, keyEnd] = partitionRange(cstone::nodeRange<KeyType>(0), rank, numRanks);
+        assembleCube<T>(keyStart, keyEnd, boxA, multiplicity, xBlock, yBlock, zBlock, d.x, d.y, d.z);
+        assembleCube<T>(keyStart, keyEnd, boxB, multiplicity, xBlock, yBlock, zBlock, d.x, d.y, d.z);
+
+        auto cutSphereOut = [r, rSphere](auto x, auto y, auto z)
+        {
+            using T_ = decltype(x);
+            util::array<T_, 3> X{x, y, z};
+            util::array<T_, 3> center{r, r, r};
+            return std::sqrt(norm2(X - center)) > rSphere;
+        };
+        selectParticles(d.x, d.y, d.z, cutSphereOut);
+
+        // create the high-density blob
+        std::vector<T> xBlob, yBlob, zBlob;
+        cstone::Box<T> boxS(r - rSphere, r + rSphere);
+        assembleCube<T>(keyStart, keyEnd, boxS, multiplicity, xBlock, yBlock, zBlock, xBlob, yBlob, zBlob);
+        auto keepSphere = [r, rSphere](auto x, auto y, auto z)
+        {
+            using T_ = decltype(x);
+            util::array<T_, 3> X{x, y, z};
+            util::array<T_, 3> center{r, r, r};
+            return std::sqrt(norm2(X - center)) < rSphere;
+        };
+        selectParticles(xBlob, yBlob, zBlob, keepSphere);
+        std::copy(xBlob.begin(), xBlob.end(), std::back_inserter(d.x));
+        std::copy(yBlob.begin(), yBlob.end(), std::back_inserter(d.y));
+        std::copy(zBlob.begin(), zBlob.end(), std::back_inserter(d.z));
 
         // Calculate particle mass with the internal sphere
-        T innerSide   = rSphere;
-        T innerVolume = (4. / 3.) * M_PI * innerSide * innerSide * innerSide;
-        T massPart    = innerVolume * rhoInt / numParticlesInternal;
+        // T innerSide   = rSphere;
+        // T innerVolume = (4. / 3.) * M_PI * innerSide * innerSide * innerSide;
+
+        size_t numParticlesInternal = blockSize * std::pow(multiplicity, 3);
+        // T massPart    = innerVolume * rhoInt / numParticlesInternal;
+        T massPart = globalBox.lx() * globalBox.ly() * globalBox.lz() * rhoExt / (2 * numParticlesInternal);
 
         // Initialize Wind shock domain variables
         d.resize(d.x.size());
