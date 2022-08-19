b08cd4b17f45d22fd430eb24481f850f8aede03b
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-08-19 15:32:19 +0200

    added separate compilation unit for cstone tree on gpu

--- domain/include/cstone/tree/octree.cuh
+++ domain/include/cstone/tree/octree.cuh
@@ -33,74 +33,13 @@
 
 #pragma once
 
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-#include <vector>
-#include <tuple>
-
-#include <thrust/device_vector.h>
+#include <limits>
 
 #include "cstone/cuda/cuda_utils.cuh"
-#include "cstone/cuda/errorcheck.cuh"
-#include "cstone/util/util.hpp"
 #include "octree.hpp"
 
 namespace cstone
 {
-
-//! @brief see computeNodeCounts
-template<class KeyType>
-__global__ void computeNodeCountsKernel(const KeyType* tree,
-                                        unsigned* counts,
-                                        TreeNodeIndex nNodes,
-                                        const KeyType* codesStart,
-                                        const KeyType* codesEnd,
-                                        unsigned maxCount)
-{
-    unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < nNodes) { counts[tid] = calculateNodeCount(tree[tid], tree[tid + 1], codesStart, codesEnd, maxCount); }
-}
-
-//! @brief see updateNodeCounts
-template<class KeyType>
-__global__ void updateNodeCountsKernel(const KeyType* tree,
-                                       unsigned* counts,
-                                       TreeNodeIndex numNodes,
-                                       const KeyType* codesStart,
-                                       const KeyType* codesEnd,
-                                       unsigned maxCount)
-{
-    unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < numNodes)
-    {
-        unsigned firstGuess     = counts[tid];
-        TreeNodeIndex secondIdx = (tid + 1 < numNodes - 1) ? tid + 1 : numNodes - 1;
-        unsigned secondGuess    = counts[secondIdx];
-
-        counts[tid] = updateNodeCount(tid, tree, firstGuess, secondGuess, codesStart, codesEnd, maxCount);
-    }
-}
-
-//! @brief used to communicate required node search range for computeNodeCountsKernel back to host
-__device__ TreeNodeIndex populatedNodes[2];
-
-//! @brief compute first and last non-empty nodes in the tree
-template<class KeyType>
-__global__ void
-findPopulatedNodes(const KeyType* tree, TreeNodeIndex nNodes, const KeyType* codesStart, const KeyType* codesEnd)
-{
-    if (threadIdx.x == 0 && codesStart != codesEnd)
-    {
-        populatedNodes[0] = stl::upper_bound(tree, tree + nNodes, *codesStart) - tree - 1;
-        populatedNodes[1] = stl::upper_bound(tree, tree + nNodes, *(codesEnd - 1)) - tree;
-    }
-    else
-    {
-        populatedNodes[0] = nNodes;
-        populatedNodes[1] = nNodes;
-    }
-}
 
 /*! @brief count number of particles in each octree node
  *
@@ -115,88 +54,14 @@ findPopulatedNodes(const KeyType* tree, TreeNodeIndex nNodes, const KeyType* cod
  *                          to prevent overflow in MPI_Allreduce
  */
 template<class KeyType>
-void computeNodeCountsGpu(const KeyType* tree,
-                          unsigned* counts,
-                          TreeNodeIndex nNodes,
-                          const KeyType* codesStart,
-                          const KeyType* codesEnd,
-                          unsigned maxCount,
-                          bool useCountsAsGuess = false)
-{
-    TreeNodeIndex popNodes[2];
+extern void computeNodeCountsGpu(const KeyType* tree,
+                                 unsigned* counts,
+                                 TreeNodeIndex nNodes,
+                                 const KeyType* codesStart,
+                                 const KeyType* codesEnd,
+                                 unsigned maxCount,
+                                 bool useCountsAsGuess = false);
 
-    findPopulatedNodes<<<1, 1>>>(tree, nNodes, codesStart, codesEnd);
-    checkGpuErrors(cudaMemcpyFromSymbol(popNodes, populatedNodes, 2 * sizeof(TreeNodeIndex)));
-
-    checkGpuErrors(cudaMemset(counts, 0, popNodes[0] * sizeof(unsigned)));
-    checkGpuErrors(cudaMemset(counts + popNodes[1], 0, (nNodes - popNodes[1]) * sizeof(unsigned)));
-
-    constexpr unsigned nThreads = 256;
-    if (useCountsAsGuess)
-    {
-        thrust::exclusive_scan(thrust::device, counts + popNodes[0], counts + popNodes[1], counts + popNodes[0]);
-        updateNodeCountsKernel<<<iceil(popNodes[1] - popNodes[0], nThreads), nThreads>>>(
-            tree + popNodes[0], counts + popNodes[0], popNodes[1] - popNodes[0], codesStart, codesEnd, maxCount);
-    }
-    else
-    {
-        computeNodeCountsKernel<<<iceil(popNodes[1] - popNodes[0], nThreads), nThreads>>>(
-            tree + popNodes[0], counts + popNodes[0], popNodes[1] - popNodes[0], codesStart, codesEnd, maxCount);
-    }
-}
-
-//! @brief this symbol is used to keep track of octree structure changes and detect convergence
-__device__ int rebalanceChangeCounter;
-
-/*! @brief Compute split or fuse decision for each octree node in parallel
- *
- * @tparam KeyType         32- or 64-bit unsigned integer type
- * @param[in] tree         octree nodes given as Morton codes of length @a numNodes
- *                         needs to satisfy the octree invariants
- * @param[in] counts       output particle counts per node, length = @a numNodes
- * @param[in] numNodes     number of nodes in tree
- * @param[in] bucketSize   maximum particle count per (leaf) node and
- *                         minimum particle count (strictly >) for (implicit) internal nodes
- * @param[out] nodeOps     stores rebalance decision result for each node, length = @a numNodes
- * @param[out] converged   stores 0 upon return if converged, a non-zero positive integer otherwise.
- *                         The storage location is accessed concurrently and cuda-memcheck might detect
- *                         a data race, but this is irrelevant for correctness.
- *
- * For each node i in the tree, in nodeOps[i], stores
- *  - 0 if to be merged
- *  - 1 if unchanged,
- *  - 8 if to be split.
- */
-template<class KeyType>
-__global__ void rebalanceDecisionKernel(
-    const KeyType* tree, const unsigned* counts, TreeNodeIndex numNodes, unsigned bucketSize, TreeNodeIndex* nodeOps)
-{
-    unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < numNodes)
-    {
-        int decision = calculateNodeOp(tree, tid, counts, bucketSize);
-        if (decision != 1) { rebalanceChangeCounter = 1; }
-        nodeOps[tid] = decision;
-    }
-}
-
-/*! @brief construct new nodes in the balanced tree
- *
- * @tparam KeyType         32- or 64-bit unsigned integer type
- * @param[in]  oldTree     old cornerstone octree, length = numOldNodes + 1
- * @param[in]  nodeOps     transformation codes for old tree, length = numOldNodes + 1
- * @param[in]  numOldNodes number of nodes in @a oldTree
- * @param[out] newTree     the rebalanced tree, length = nodeOps[numOldNodes] + 1
- */
-template<class KeyType>
-__global__ void
-processNodes(const KeyType* oldTree, const TreeNodeIndex* nodeOps, TreeNodeIndex numOldNodes, KeyType* newTree)
-{
-    unsigned tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < numOldNodes) { processNode(tid, oldTree, nodeOps, newTree); }
-}
-
-__global__ void resetRebalanceCounter() { rebalanceChangeCounter = 0; }
 
 /*! @brief split or fuse octree nodes based on node counts relative to bucketSize
  *
@@ -209,39 +74,15 @@ __global__ void resetRebalanceCounter() { rebalanceChangeCounter = 0; }
  * @return                 number of nodes in the future rebalanced tree
  */
 template<class KeyType>
-TreeNodeIndex computeNodeOpsGpu(
-    const KeyType* tree, TreeNodeIndex numNodes, const unsigned* counts, unsigned bucketSize, TreeNodeIndex* nodeOps)
-{
-    resetRebalanceCounter<<<1, 1>>>();
-
-    constexpr unsigned nThreads = 512;
-    rebalanceDecisionKernel<<<iceil(numNodes, nThreads), nThreads>>>(tree, counts, numNodes, bucketSize, nodeOps);
-
-    size_t nodeOpsSize = numNodes + 1;
-    thrust::exclusive_scan(thrust::device, nodeOps, nodeOps + nodeOpsSize, nodeOps);
-
-    TreeNodeIndex newNumNodes;
-    thrust::copy_n(thrust::device_pointer_cast(nodeOps) + nodeOpsSize - 1, 1, &newNumNodes);
-
-    return newNumNodes;
-}
+extern TreeNodeIndex computeNodeOpsGpu(
+    const KeyType* tree, TreeNodeIndex numNodes, const unsigned* counts, unsigned bucketSize, TreeNodeIndex* nodeOps);
 
 template<class KeyType>
-bool rebalanceTreeGpu(const KeyType* tree,
-                      TreeNodeIndex numNodes,
-                      TreeNodeIndex newNumNodes,
-                      const TreeNodeIndex* nodeOps,
-                      KeyType* newTree)
-{
-    constexpr unsigned nThreads = 512;
-    processNodes<<<iceil(numNodes, nThreads), nThreads>>>(tree, nodeOps, numNodes, newTree);
-    thrust::fill_n(thrust::device_pointer_cast(newTree + newNumNodes), 1, nodeRange<KeyType>(0));
-
-    int changeCounter;
-    checkGpuErrors(cudaMemcpyFromSymbol(&changeCounter, rebalanceChangeCounter, sizeof(int)));
-
-    return changeCounter == 0;
-}
+extern bool rebalanceTreeGpu(const KeyType* tree,
+                             TreeNodeIndex numNodes,
+                             TreeNodeIndex newNumNodes,
+                             const TreeNodeIndex* nodeOps,
+                             KeyType* newTree);
 
 /*! @brief update the octree with a single rebalance/count step
  *
@@ -256,14 +97,14 @@ bool rebalanceTreeGpu(const KeyType* tree,
  * @param[in]    maxCount    if actual node counts are higher, they will be capped to @p maxCount
  * @return                   true if converged, false otherwise
  */
-template<class KeyType>
+template<class KeyType, class DevKeyVec, class DevCountVec, class DevIdxVec>
 bool updateOctreeGpu(const KeyType* firstKey,
                      const KeyType* lastKey,
                      unsigned bucketSize,
-                     thrust::device_vector<KeyType>& tree,
-                     thrust::device_vector<unsigned>& counts,
-                     thrust::device_vector<KeyType>& tmpTree,
-                     thrust::device_vector<TreeNodeIndex>& workArray,
+                     DevKeyVec& tree,
+                     DevCountVec& counts,
+                     DevKeyVec& tmpTree,
+                     DevIdxVec& workArray,
                      unsigned maxCount = std::numeric_limits<unsigned>::max())
 {
     workArray.resize(tree.size());
