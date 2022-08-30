a64858216629737d0aa2c9c15de1d0222f314c28
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-08-30 11:04:11 +0200

    added GPU integrator

--- sph/include/sph/positions.hpp
+++ sph/include/sph/positions.hpp
@@ -32,9 +32,9 @@
 
 #pragma once
 
-#include <vector>
-#include <cmath>
-#include <tuple>
+#include "cstone/sfc/box.hpp"
+#include "cstone/util/array.hpp"
+#include "cstone/util/tuple.hpp"
 
 namespace sph
 {
@@ -47,19 +47,19 @@ HOST_DEVICE_FUN bool fbcCheck(Tc coord, Th h, Tc top, Tc bottom, bool fbc)
 }
 
 //! @brief update the energy according to Adams-Bashforth (2nd order)
-template<class T1, class T2>
-HOST_DEVICE_FUN T2 energyUpdate(T1 dt, T1 dt_m1, T2 du, T2 du_m1)
+template<class T>
+HOST_DEVICE_FUN double energyUpdate(double dt, double dt_m1, T du, T du_m1)
 {
-    T1 deltaA = 0.5 * dt * dt / dt_m1;
-    T1 deltaB = dt + deltaA;
+    double deltaA = 0.5 * dt * dt / dt_m1;
+    double deltaB = dt + deltaA;
 
     return du * deltaB - du_m1 * deltaA;
 }
 
 //! @brief Update positions according to Press (2nd order)
-template<class T, class Tc, class Ta, class Tm1>
-HOST_DEVICE_FUN auto positionUpdate(T dt, T dt_m1, cstone::Vec3<Tc> X, cstone::Vec3<Ta> A, cstone::Vec3<Tm1> X_m1,
-                                    const cstone::Box<Tc>& box)
+template<class T>
+HOST_DEVICE_FUN auto positionUpdate(T dt, T dt_m1, cstone::Vec3<T> X, cstone::Vec3<T> A, cstone::Vec3<T> X_m1,
+                                    const cstone::Box<T>& box)
 {
     T deltaA = dt + T(0.5) * dt_m1;
     T deltaB = T(0.5) * (dt + dt_m1);
@@ -74,30 +74,14 @@ HOST_DEVICE_FUN auto positionUpdate(T dt, T dt_m1, cstone::Vec3<Tc> X, cstone::V
     X_m1 += Xpbc - X;
     X = Xpbc;
 
-    return util::tuple<cstone::Vec3<Tc>, cstone::Vec3<Tc>, cstone::Vec3<Tm1>>{X, V, X_m1};
+    return util::tuple<cstone::Vec3<T>, cstone::Vec3<T>, cstone::Vec3<T>>{X, V, X_m1};
 }
 
 template<class T, class Dataset>
 void computePositions(size_t startIndex, size_t endIndex, Dataset& d, const cstone::Box<T>& box)
 {
-    using Vec3T = cstone::Vec3<T>;
-    T dt        = d.minDt;
-    T dt_m1     = d.minDt_m1;
-
-    const auto* du = d.du.data();
-    const auto* h  = d.h.data();
-
-    auto* x     = d.x.data();
-    auto* y     = d.y.data();
-    auto* z     = d.z.data();
-    auto* vx    = d.vx.data();
-    auto* vy    = d.vy.data();
-    auto* vz    = d.vz.data();
-    auto* x_m1  = d.x_m1.data();
-    auto* y_m1  = d.y_m1.data();
-    auto* z_m1  = d.z_m1.data();
-    auto* u     = d.u.data();
-    auto* du_m1 = d.du_m1.data();
+    double dt    = d.minDt;
+    double dt_m1 = d.minDt_m1;
 
     bool fbcX = (box.boundaryX() == cstone::BoundaryType::fixed);
     bool fbcY = (box.boundaryY() == cstone::BoundaryType::fixed);
@@ -108,30 +92,29 @@ void computePositions(size_t startIndex, size_t endIndex, Dataset& d, const csto
 #pragma omp parallel for schedule(static)
     for (size_t i = startIndex; i < endIndex; i++)
     {
-        if (anyFBC && vx[i] == T(0) && vy[i] == T(0) && vz[i] == T(0))
+        if (anyFBC && d.vx[i] == T(0) && d.vy[i] == T(0) && d.vz[i] == T(0))
         {
-            if (fbcCheck(x[i], h[i], box.xmax(), box.xmin(), fbcX) ||
-                fbcCheck(y[i], h[i], box.ymax(), box.ymin(), fbcY) ||
-                fbcCheck(z[i], h[i], box.zmax(), box.zmin(), fbcZ))
+            if (fbcCheck(d.x[i], d.h[i], box.xmax(), box.xmin(), fbcX) ||
+                fbcCheck(d.y[i], d.h[i], box.ymax(), box.ymin(), fbcY) ||
+                fbcCheck(d.z[i], d.h[i], box.zmax(), box.zmin(), fbcZ))
             {
                 continue;
             }
         }
 
-        Vec3T A{d.ax[i], d.ay[i], d.az[i]};
-        Vec3T X{x[i], y[i], z[i]};
-        Vec3T X_m1{x_m1[i], y_m1[i], z_m1[i]};
-        Vec3T V;
+        cstone::Vec3<T> A{d.ax[i], d.ay[i], d.az[i]};
+        cstone::Vec3<T> X{d.x[i], d.y[i], d.z[i]};
+        cstone::Vec3<T> X_m1{d.x_m1[i], d.y_m1[i], d.z_m1[i]};
+        cstone::Vec3<T> V;
         util::tie(X, V, X_m1) = positionUpdate(dt, dt_m1, X, A, X_m1, box);
 
-        util::tie(x[i], y[i], z[i])          = util::tie(X[0], X[1], X[2]);
-        util::tie(x_m1[i], y_m1[i], z_m1[i]) = util::tie(X_m1[0], X_m1[1], X_m1[2]);
-        util::tie(vx[i], vy[i], vz[i])       = util::tie(V[0], V[1], V[2]);
+        util::tie(d.x[i], d.y[i], d.z[i])          = util::tie(X[0], X[1], X[2]);
+        util::tie(d.x_m1[i], d.y_m1[i], d.z_m1[i]) = util::tie(X_m1[0], X_m1[1], X_m1[2]);
+        util::tie(d.vx[i], d.vy[i], d.vz[i])       = util::tie(V[0], V[1], V[2]);
 
-        u[i] += energyUpdate(dt, dt_m1, du[i], du_m1[i]);
-        du_m1[i] = du[i];
+        d.u[i] += energyUpdate(dt, dt_m1, d.du[i], d.du_m1[i]);
+        d.du_m1[i] = d.du[i];
     }
 }
 
 } // namespace sph
-
--- sph/include/sph/sph_gpu.hpp
+++ sph/include/sph/sph_gpu.hpp
@@ -41,5 +41,9 @@ template<class Tu, class Tm, class Thydro>
 extern void computeEOS(size_t, size_t, Tu, const Tu*, const Tm*, const Thydro*, const Thydro*, const Thydro*, Thydro*,
                        Thydro*);
 
+template<class Tc, class Tv, class Tm1, class Tu, class Thydro>
+extern void computePositionsGpu(size_t, size_t, double, double, Tc* x, Tc* y, Tc* z, Tv* vx, Tv* vy, Tv* vz, Tm1* x_m1,
+                                Tm1* y_m1, Tm1* z_m1, Tu* u, Tm1* du, Tm1* du_m1, Thydro* h, const cstone::Box<Tc>&);
+
 } // namespace cuda
 } // namespace sph
