2a9c64890d8a14bb73debf7a65de43f22cc3c9cf
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-10-04 11:25:08 +0200

    removed unused field accessors

--- sph/include/sph/data_util.hpp
+++ sph/include/sph/data_util.hpp
@@ -70,18 +70,6 @@ constexpr size_t getFieldIndex(std::string_view field, const Array& fieldNames)
     return fieldNames.size();
 }
 
-//! @brief translate string fields to an array with the field indices
-template<class Array1, class Array2>
-constexpr auto fieldNamesToIndices(const Array1& queries, const Array2& fieldNames)
-{
-    typename util::SwapArg<Array1, size_t>::type ret;
-    for (size_t i = 0; i < queries.size(); ++i)
-    {
-        ret[i] = getFieldIndex(queries[i], fieldNames);
-    }
-    return ret;
-}
-
 /*! @brief Look up indices of a (runtime-variable) number of field names
  *
  * @tparam     Array
@@ -99,20 +87,6 @@ std::vector<int> fieldStringsToInt(const std::vector<std::string>& subsetNames, 
         if (subsetIndices[i] == allNames.size()) { throw std::runtime_error("Field not found: " + subsetNames[i]); }
     }
     return subsetIndices;
-}
-
-template<auto Indices, class Tuple, size_t... Is>
-auto accessFields_helper(Tuple&& tuple, std::index_sequence<Is...>)
-{
-    return std::tie(std::get<Indices[Is]>(std::forward<Tuple>(tuple))...);
-}
-
-//! @brief return a tuple of references to the elements of @p tuple specified by compile-time Indices
-template<auto Indices, class Tuple>
-auto accessFields(Tuple&& tuple)
-{
-    constexpr size_t numIndices = Indices.size();
-    return accessFields_helper<Indices>(std::forward<Tuple>(tuple), std::make_index_sequence<numIndices>{});
 }
 
 } // namespace sphexa
