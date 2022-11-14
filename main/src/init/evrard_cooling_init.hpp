808986fc834c853c55d3e316ec53f67c33cf2846
Sebastian Keller <sebastian.f.keller@gmail.com>
2022-11-14 14:07:33 +0100

    fix formatting

--- main/src/init/evrard_cooling_init.hpp
+++ main/src/init/evrard_cooling_init.hpp
@@ -1,47 +1,65 @@
-//
-// Created by Noah Kubli on 26.10.22.
-//
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 CSCS, ETH Zurich, University of Zurich, University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-#ifndef SPHEXA_EVRARD_COOLING_INIT_HPP
-#define SPHEXA_EVRARD_COOLING_INIT_HPP
+/*! @file
+ * @brief Unit tests for ParticlesData
+ *
+ * @author Noah Kubli <noah.kubli@uzh.ch>
+ */
+
+#pragma once
 
 #include "evrard_init.hpp"
 #include "cooling/cooling.hpp"
-
 
 template<class Dataset>
 class EvrardGlassSphereCooling : public sphexa::EvrardGlassSphere<Dataset>
 {
     const float ms_sim = 1e16;
     const float kp_sim = 46400.;
+
 public:
-    EvrardGlassSphereCooling(std::string initBlock):
-    sphexa::EvrardGlassSphere<Dataset>(initBlock) {}
+    EvrardGlassSphereCooling(std::string initBlock)
+        : sphexa::EvrardGlassSphere<Dataset>(initBlock)
+    {
+    }
     cstone::Box<typename Dataset::RealType> init(int rank, int numRanks, size_t cbrtNumPart,
                                                  Dataset& simData) const override
     {
-
         chemistry_data grackleData = simData.chem.cooling_data.getDefaultChemistryData();
 
-        grackleData.use_grackle = 1;
+        grackleData.use_grackle            = 1;
         grackleData.with_radiative_cooling = 1;
-        grackleData.primordial_chemistry = 0;
-        grackleData.dust_chemistry = 0;
-        grackleData.metal_cooling = 0;
-        grackleData.UVbackground = 0;
+        grackleData.primordial_chemistry   = 0;
+        grackleData.dust_chemistry         = 0;
+        grackleData.metal_cooling          = 0;
+        grackleData.UVbackground           = 0;
 
-        simData.chem.cooling_data.init(ms_sim,
-                                       kp_sim,
-                                       0,
-                                       grackleData,
-                                       std::nullopt,
-                                       std::nullopt);
+        simData.chem.cooling_data.init(ms_sim, kp_sim, 0, grackleData, std::nullopt, std::nullopt);
 
         auto box = sphexa::EvrardGlassSphere<Dataset>::init(rank, numRanks, cbrtNumPart, simData);
         cooling::initGrackleData(simData.chem, simData.hydro.x.size());
         return box;
     }
 };
-
-
-#endif //SPHEXA_EVRARD_COOLING_INIT_HPP
