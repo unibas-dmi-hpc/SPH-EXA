90d6093c6360398f52ca0c88229724321ac35fea
Sebastian Keller <sebastian.f.keller@gmail.com>
2023-05-31 15:12:59 +0200

    compute IAD-tau determinants with normalization factors

--- sph/include/sph/hydro_std/iad_kern.hpp
+++ sph/include/sph/hydro_std/iad_kern.hpp
@@ -50,12 +50,24 @@ HOST_DEVICE_FUN inline void IADJLoopSTD(cstone::LocalIndex i, T sincIndex, T K, 
         tau33 += rz * rz * mj_roj_w;
     }
 
+    auto getExp    = [](T val) { return (val == T(0) ? 0 : std::ilogb(val)); };
+    int  tauExpSum = getExp(tau11) + getExp(tau12) + getExp(tau13) + getExp(tau22) + getExp(tau23) + getExp(tau33);
+    // normalize with 2^-averageTauExponent, ldexp(a, b) == a * 2^b
+    T normalization = std::ldexp(T(1), -tauExpSum / 6);
+
+    tau11 *= normalization;
+    tau12 *= normalization;
+    tau13 *= normalization;
+    tau22 *= normalization;
+    tau23 *= normalization;
+    tau33 *= normalization;
+
     T det = tau11 * tau22 * tau33 + T(2) * tau12 * tau23 * tau13 - tau11 * tau23 * tau23 - tau22 * tau13 * tau13 -
             tau33 * tau12 * tau12;
 
     // Note normalization factor: cij have units of 1/tau because det is proportional to tau^3 so we have to
-    // divide by K/h^3. det can be zero due to numerical underflow
-    T factor = (det == T(0.0)) ? T(0.0) : (hi * hi * hi) / (det * K);
+    // divide by K/h^3.
+    T factor = normalization * (hi * hi * hi) / (det * K);
 
     c11[i] = (tau22 * tau33 - tau23 * tau23) * factor;
     c12[i] = (tau13 * tau23 - tau33 * tau12) * factor;
