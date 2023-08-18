57bd520de613e5e5b22e54ccb802ceacbdfe0c52
nknk567 <noah.kubli@uzh.ch>
2023-08-18 12:38:42 +0200

    fixed Grackle Cmake

--- extern/grackle/grackle_deps/auto_show_version.c
+++ extern/grackle/grackle_deps/auto_show_version.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "grackle_float.h"
 #include "grackle_types.h"
-
+#if !defined(GRACKLE_FLOAT_4) && !defined(GRACKLE_FLOAT_8)
+#error "error"
+#endif
 grackle_version get_grackle_version() {
    	grackle_version out;
 	out.version = "grackle_version";
