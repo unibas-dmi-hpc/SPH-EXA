d6d3a422cdf4224300482694ad94eb6d33042a81
nknk567 <noah.kubli@uzh.ch>
2023-08-21 16:21:46 +0200

    Removed field alpha in Cooling propagator

--- main/src/propagator/std_hydro_grackle.hpp
+++ main/src/propagator/std_hydro_grackle.hpp
@@ -65,7 +65,7 @@ class HydroGrackleProp final : public HydroProp<DomainType, DataType>
      *
      * x, y, z, h and m are automatically considered conserved and must not be specified in this list
      */
-    using ConservedFields = FieldList<"u", "vx", "vy", "vz", "x_m1", "y_m1", "z_m1", "du_m1", "alpha">;
+    using ConservedFields = FieldList<"u", "vx", "vy", "vz", "x_m1", "y_m1", "z_m1", "du_m1">;
 
     //! @brief the list of dependent particle fields, these may be used as scratch space during domain sync
     using DependentFields =
