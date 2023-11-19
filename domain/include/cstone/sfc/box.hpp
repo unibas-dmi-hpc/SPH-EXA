cb7cc6754b81b1df529a341f39123a3a08cb2f70
luke_sch02 <lukas.schmidt@gmx.ch>
2023-11-19 20:24:53 +0100

    FBC: added generation of boundary particles

--- domain/include/cstone/sfc/box.hpp
+++ domain/include/cstone/sfc/box.hpp
@@ -113,11 +113,12 @@ class Box
 {
 
 public:
-    HOST_DEVICE_FUN constexpr Box(T xyzMin, T xyzMax, BoundaryType b = BoundaryType::open)
+    HOST_DEVICE_FUN constexpr Box(T xyzMin, T xyzMax, BoundaryType b = BoundaryType::open, int fbcThickness = 8)
         : limits{xyzMin, xyzMax, xyzMin, xyzMax, xyzMin, xyzMax}
         , lengths_{xyzMax - xyzMin, xyzMax - xyzMin, xyzMax - xyzMin}
         , inverseLengths_{T(1.) / (xyzMax - xyzMin), T(1.) / (xyzMax - xyzMin), T(1.) / (xyzMax - xyzMin)}
         , boundaries{b, b, b}
+        , fbcThickness_{fbcThickness}
     {
     }
 
@@ -127,13 +128,15 @@ public:
                                   T ymax,
                                   T zmin,
                                   T zmax,
-                                  BoundaryType bx = BoundaryType::open,
-                                  BoundaryType by = BoundaryType::open,
-                                  BoundaryType bz = BoundaryType::open)
+                                  BoundaryType bx  = BoundaryType::open,
+                                  BoundaryType by  = BoundaryType::open,
+                                  BoundaryType bz  = BoundaryType::open,
+                                  int fbcThickness = 8)
         : limits{xmin, xmax, ymin, ymax, zmin, zmax}
         , lengths_{xmax - xmin, ymax - ymin, zmax - zmin}
         , inverseLengths_{T(1.) / (xmax - xmin), T(1.) / (ymax - ymin), T(1.) / (zmax - zmin)}
         , boundaries{bx, by, bz}
+        , fbcThickness_{fbcThickness}
     {
     }
 
@@ -164,14 +167,20 @@ public:
     //! @brief return the longes coordinate range in any dimension
     HOST_DEVICE_FUN constexpr T maxExtent() const { return stl::max(stl::max(lengths_[0], lengths_[1]), lengths_[2]); }
 
+    //! @brief return the thickness of added boundary particle layer
+    HOST_DEVICE_FUN constexpr int fbcThickness() const { return fbcThickness_; }
+
     template<class Archive>
     void loadOrStore(Archive* ar)
     {
         ar->stepAttribute("box", limits, 6);
         ar->stepAttribute("boundaryType", (char*)boundaries, 3);
+        bool anyFbc = boundaries[0] == BoundaryType::fixed || boundaries[1] == BoundaryType::fixed ||
+                      boundaries[3] == BoundaryType::fixed;
+        if (anyFbc) { ar->stepAttribute("fbcThickness_", &fbcThickness_, 1); }
 
         *this = Box<T>(limits[0], limits[1], limits[2], limits[3], limits[4], limits[5], boundaries[0], boundaries[1],
-                       boundaries[2]);
+                       boundaries[2], fbcThickness_);
     }
 
 private:
@@ -181,13 +190,14 @@ private:
         return a.limits[0] == b.limits[0] && a.limits[1] == b.limits[1] && a.limits[2] == b.limits[2] &&
                a.limits[3] == b.limits[3] && a.limits[4] == b.limits[4] && a.limits[5] == b.limits[5] &&
                a.boundaries[0] == b.boundaries[0] && a.boundaries[1] == b.boundaries[1] &&
-               a.boundaries[2] == b.boundaries[2];
+               a.boundaries[2] == b.boundaries[2] && a.fbcThickness_ = b.fbcThickness_;
     }
 
     T limits[6];
     T lengths_[3];
     T inverseLengths_[3];
     BoundaryType boundaries[3];
+    int fbcThickness_;
 };
 
 //! @brief Compute the shortest periodic distance dX = A - B between two points,
