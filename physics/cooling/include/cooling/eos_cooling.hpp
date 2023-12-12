6acfd6ab591c540afca1b60df7990664442a3147
nknk567 <noah.kubli@uzh.ch>
2023-12-12 15:31:21 +0100

    x

--- physics/cooling/include/cooling/eos_cooling.hpp
+++ physics/cooling/include/cooling/eos_cooling.hpp
@@ -13,7 +13,7 @@ auto coolingTimestep(size_t first, size_t last, Dataset& d, Cooler& cooler, Chem
 {
     using T             = typename Dataset::RealType;
     using CoolingFields = typename Cooler::CoolingFields;
-    return INFINITY;
+    return std::numeric_limits<double>::infinity();
     //T minCt = cooler.min_cooling_time(d.rho, d.u, cstone::getPointers(get<CoolingFields>(chem), 0), first, last);
    // return minCt;
     /*std::vector<double> cooling_times(last - first);
