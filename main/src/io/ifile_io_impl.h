f86a7003f863fa4a202508767305652d2abd0b5e
yzhu-ub <yiqing.zhu@unibas.ch>
2023-12-25 18:25:02 +0100

    reformat

--- main/src/io/ifile_io_impl.h
+++ main/src/io/ifile_io_impl.h
@@ -11,10 +11,10 @@ namespace sphexa
 std::unique_ptr<IFileWriter> makeAsciiWriter(MPI_Comm comm);
 std::unique_ptr<IFileWriter> makeH5PartWriter(MPI_Comm comm);
 std::unique_ptr<IFileWriter> makeADIOSWriter(MPI_Comm comm, const std::string& compressionMethod,
-                                               const std::string& compressionParam = "");
+                                             const std::string& compressionParam = "");
 
 std::unique_ptr<IFileReader> makeH5PartReader(MPI_Comm comm);
 std::unique_ptr<IFileReader> makeADIOSReader(MPI_Comm comm, const std::string& compressionMethod,
-                                               const std::string& compressionParam = "");
+                                             const std::string& compressionParam = "");
 
 } // namespace sphexa
