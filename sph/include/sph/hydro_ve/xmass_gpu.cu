92b116d10659c9dce66bbd07bc22fe2b0b09a18f
nknk567 <noah.kubli@uzh.ch>
2024-01-18 15:13:29 +0100

    fix convertXMassToDensity

--- sph/include/sph/hydro_ve/xmass_gpu.cu
+++ sph/include/sph/hydro_ve/xmass_gpu.cu
@@ -133,21 +133,6 @@ void computeXMass(size_t startIndex, size_t endIndex, Dataset& d, const cstone::
 
 template void computeXMass(size_t, size_t, sphexa::ParticlesData<cstone::GpuTag>& d,
                            const cstone::Box<SphTypes::CoordinateType>&);
-template<class Dataset>
-void computeTargetGroups(size_t startIndex, size_t endIndex, Dataset& d,
-                         const cstone::Box<typename Dataset::RealType>& box)
-{
-    thrust::device_vector<util::array<GpuConfig::ThreadMask, TravConfig::nwt>> S;
-
-    float tolFactor = 2.0f;
-    cstone::computeGroupSplits<TravConfig::targetSize>(startIndex, endIndex, rawPtr(d.devData.x), rawPtr(d.devData.y),
-                                                       rawPtr(d.devData.z), rawPtr(d.devData.h), d.treeView.leaves,
-                                                       d.treeView.tree.numLeafNodes, d.treeView.layout, box, tolFactor,
-                                                       S, d.devData.traversalStack, d.devData.targetGroups);
-}
-
-template void computeTargetGroups(size_t, size_t, sphexa::ParticlesData<cstone::GpuTag>& d,
-                                  const cstone::Box<SphTypes::CoordinateType>&);
 
 template<typename Trho, typename Tm>
 __global__ void cuda_convertXMassToDensity(size_t firstParticle, size_t lastParticle, Trho* rho, const Tm* m)
@@ -168,5 +153,21 @@ void convertXMassToDensity(size_t firstParticle, size_t lastParticle, Trho* rho,
 
 template void convertXMassToDensity(size_t, size_t, float*, const float*);
 } // namespace cuda
+
+template<class Dataset>
+void computeTargetGroups(size_t startIndex, size_t endIndex, Dataset& d,
+                         const cstone::Box<typename Dataset::RealType>& box)
+{
+    thrust::device_vector<util::array<GpuConfig::ThreadMask, TravConfig::nwt>> S;
+
+    float tolFactor = 2.0f;
+    cstone::computeGroupSplits<TravConfig::targetSize>(startIndex, endIndex, rawPtr(d.devData.x), rawPtr(d.devData.y),
+                                                       rawPtr(d.devData.z), rawPtr(d.devData.h), d.treeView.leaves,
+                                                       d.treeView.tree.numLeafNodes, d.treeView.layout, box, tolFactor,
+                                                       S, d.devData.traversalStack, d.devData.targetGroups);
+}
+
+template void computeTargetGroups(size_t, size_t, sphexa::ParticlesData<cstone::GpuTag>& d,
+                                  const cstone::Box<SphTypes::CoordinateType>&);
 
 } // namespace sph
