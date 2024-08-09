d124a519ddaff6b9e044ff5166bd251b6d196faf
Sebastian Keller <sebastian.f.keller@gmail.com>
2024-08-09 14:35:24 +0200

    added test for particleRemoval

--- domain/test/integration_mpi/domain_nranks.cpp
+++ domain/test/integration_mpi/domain_nranks.cpp
@@ -259,6 +259,73 @@ TEST(FocusDomain, assignmentShift)
     EXPECT_TRUE(std::count(property.begin(), property.end(), rank) == domain.nParticles());
 }
 
+TEST(FocusDomain, removeParticle)
+{
+    int rank = 0, numRanks = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+
+    using Real    = double;
+    using KeyType = unsigned;
+
+    Box<Real> box(0, 1);
+    LocalIndex numParticlesPerRank = 10;
+    unsigned bucketSize            = 1024;
+    unsigned bucketSizeFocus       = 8;
+    float theta                    = 0.5;
+
+    RandomCoordinates<Real, SfcKind<KeyType>> coordinates(numParticlesPerRank, box, rank);
+
+    std::vector<Real> x(coordinates.x().begin(), coordinates.x().end());
+    std::vector<Real> y(coordinates.y().begin(), coordinates.y().end());
+    std::vector<Real> z(coordinates.z().begin(), coordinates.z().end());
+    std::vector<Real> h(numParticlesPerRank, 0.1 / std::cbrt(numRanks));
+
+    std::vector<uint64_t> id(x.size());
+    std::iota(begin(id), end(id), 100 + uint64_t(rank * numParticlesPerRank));
+
+    Domain<KeyType, Real> domain(rank, numRanks, bucketSize, bucketSizeFocus, theta, box);
+
+    std::vector<KeyType> particleKeys(x.size());
+
+    std::vector<Real> s1, s2, s3;
+    domain.sync(particleKeys, x, y, z, h, std::tie(id), std::tie(s1, s2, s3));
+
+    // pick a particle to remove on each rank
+    LocalIndex removeIndex    = domain.startIndex() + numParticlesPerRank / 2;
+    particleKeys[removeIndex] = removeKey<KeyType>::value;
+    uint64_t removeID = id[removeIndex];
+
+    std::cout << "KEYS: ";
+    for (auto v : particleKeys) std::cout << std::oct << v << " "; std::cout << std::dec << std::endl;
+    std::cout << "id: ";
+    for (auto v : id) std::cout << v << " "; std::cout << std::endl;
+    std::cout << "removeID " << removeID << " removeIndex " << removeIndex << std::endl;
+
+    std::cout << domain.startIndex() << " " << domain.endIndex() << " " << particleKeys.size() << std::endl;
+
+    domain.sync(particleKeys, x, y, z, h, std::tie(id), std::tie(s1, s2, s3));
+
+    //std::cout << domain.startIndex() << " " << domain.endIndex() << " " << id.back() << " " << id[removeIndex] << std::endl;
+    std::cout << "KEYS: ";
+    for (auto v : particleKeys) std::cout << std::oct << v << " "; std::cout << std::dec << std::endl;
+    std::cout << "id: ";
+    for (auto v : id) std::cout << v << " "; std::cout << std::endl;
+
+    uint64_t numLocalParticles = domain.nParticles();
+    uint64_t numGlobalParticles;
+    MPI_Allreduce(&numLocalParticles, &numGlobalParticles, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, MPI_COMM_WORLD);
+    EXPECT_EQ(numGlobalParticles, numRanks * numParticlesPerRank - numRanks);
+
+    //// check that removed particles are gone by checking their IDs
+    //std::vector<uint64_t> removedIDs(numRanks);
+    //MPI_Allgather(&removeID, 1, MPI_UNSIGNED_LONG_LONG, removedIDs.data(), 1, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
+    //for (uint64_t rid : removedIDs)
+    //{
+    //    EXPECT_EQ(std::count(id.begin(), id.end(), rid), 0);
+    //}
+}
+
 TEST(FocusDomain, reapplySync)
 {
     int rank = 0, numRanks = 0;
