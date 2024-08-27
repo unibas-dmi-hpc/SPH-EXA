5cd73a79eec18a86c7509e51d369e3db0fd8f11f
Osman Seckin Simsek <osman.simsek@unibas.ch>
2024-08-27 15:46:39 +0200

    fix formatting

--- main/src/propagator/ve_hydro_bdt.hpp
+++ main/src/propagator/ve_hydro_bdt.hpp
@@ -399,7 +399,8 @@ public:
                                  d.outputFieldIndices.begin();
                     transferToHost(d, first, last, {d.fieldNames[fidx]});
                     std::visit([writer, c = column, key = namesDone[i]](auto field)
-                               { writer->writeField(key, field->data(), c); }, fieldPointers[fidx]);
+                               { writer->writeField(key, field->data(), c); },
+                               fieldPointers[fidx]);
                     indicesDone.erase(indicesDone.begin() + i);
                     namesDone.erase(namesDone.begin() + i);
                 }
